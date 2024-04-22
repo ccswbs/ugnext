8e9879b67cb04b8c49c136ce222278054e8724d8
Raza Mahmood <razammahmood@gmail.com>
2024-04-22 11:15:47 -0400

    switch .jsx to .js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import Layout from '@/components/layout';
 
 export default function Home() {
-  return (
-    <Layout>
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<Layout>
+			<Head>
+				<title>Next.js Starter!</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-
-    </Layout>
-  )
+			<p className="description">
+				Get started by editing <code>pages/index.js</code>
+			</p>
+		</Layout>
+	);
 }
