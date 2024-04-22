7f8210533ac7010dfecc98d66059c4f722918c22
Raza Mahmood <razammahmood@gmail.com>
2024-04-22 11:10:19 -0400

    add all needed modules, finally loading without errors

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,4 +1,3 @@
-import { PreviewRibbon } from '@pantheon-systems/nextjs-kit';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import Head from 'next/head';
@@ -6,7 +5,6 @@ import Head from 'next/head';
 export default function Layout({ children, menu, preview = false }) {
 	return (
 		<div className="flex flex-col flex-1">
-			{preview && <PreviewRibbon />}
 			<Header menu={menu} />
 			<main className="mb-auto">{children}</main>
 			<Footer />
