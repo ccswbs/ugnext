50b41f8d5868055b5c66334aff43d4e261c58a18
Raza Mahmood <razammahmood@gmail.com>
2024-04-23 16:39:41 -0400

    add styles to Link and start using twMerge instead of classNames

--- components/link.js
+++ components/link.js
@@ -1,4 +1,5 @@
 import NextLink from 'next/link';
+import { twMerge } from 'tailwind-merge';
 
 const absoluteURLPattern = new RegExp('^(//|[a-z]+:)', 'i');
 
@@ -16,16 +17,17 @@ export const UnstyledLink = ({ href, children, ...rest }) => {
 	);
 };
 
-export default function Link({ href, children, ...rest }) {
-	const isAbsoluteOrHash = absoluteURLPattern.test(href);
+export default function Link({ href, color, children, ...rest }) {
+	const base = 'underline transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2';
 
-	return isAbsoluteOrHash ? (
-		<a {...rest} href={href}>
+	const colors = {
+		blue: 'text-blue-600 hover:text-blue-800 focus:text-blue-800 focus:ring-blue',
+		red: 'text-red hover:text-red-800 focus:text-red-800 focus:ring-red',
+	};
+
+	return (
+		<UnstyledLink {...rest} href={href} className={twMerge(base, colors[color] ?? colors['blue'], rest?.className)}>
 			{children}
-		</a>
-	) : (
-		<NextLink {...rest} href={href}>
-			{children}
-		</NextLink>
+		</UnstyledLink>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Layout from '@/components/layout';
 import Container from '@/components/container';
+import Link from '@/components/link';
 
 export default function Home() {
 	return (
 		<Layout title="Home">
 			<Container centered>
 				<h1>UGNext Home Page</h1>
+
+				<Link href="/test" color="blue" className="text-xl">Test Link</Link>
 			</Container>
 		</Layout>
 	);
