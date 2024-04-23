8f010bfadb205321673d22e1d725843dfc22c408
Raza Mahmood <razammahmood@gmail.com>
2024-04-23 17:15:05 -0400

    add heading component

---
+++ components/heading.js
@@ -0,0 +1,22 @@
+import { clamp } from '@/lib/math-utils';
+import { twMerge } from 'tailwind-merge';
+
+const Heading = ({ level, children, className, ...rest }) => {
+	const headingLevel = clamp(level || 1, 1, 6);
+	const Tag = `h${headingLevel}`;
+	const base = 'mb-4 mt-4 font-bold leading-tight';
+	const levels = {
+		1: 'text-5xl text-red',
+		2: 'text-4xl',
+		3: 'text-2xl',
+		4: 'text-xl',
+		5: 'text-lg',
+		6: 'text-lg',
+	};
+
+	return (
+		<Tag {...rest} className={twMerge(base, levels[headingLevel], className)}>
+			{children}
+		</Tag>
+	);
+};
