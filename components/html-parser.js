1a140aa12860d77df74e3fa2c5470f2495365ca5
Raza Mahmood <razammahmood@gmail.com>
2024-04-23 21:14:01 -0400

    adding instructions to html parser

--- components/html-parser.js
+++ components/html-parser.js
@@ -1,12 +1,24 @@
 import { Fragment, useEffect, useMemo, useState } from 'react';
 import { Parser, ProcessNodeDefinitions } from 'html-to-react';
 import Link from '@/components/link';
+import Heading from '@/components/heading';
 
 const parser = new Parser();
 const definitions = new ProcessNodeDefinitions();
 const defaultInstructions = [
 	{
-		shouldProcessNode: (node) => node?.tagName === 'a' && typeof node?.attribs?.href === 'string',
+		shouldProcessNode: (node) => {
+			const valid = new Set(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']);
+			return valid.has(node.tagName);
+		},
+		processNode: (node, children) => {
+			const level = /h(\d)/.exec(node.tagName)?.[1];
+			console.log(level)
+			return <Heading {...node.attribs} level={level}>{children}</Heading>;
+		},
+	},
+	{
+		shouldProcessNode: (node) => node.tagName === 'a' && typeof node.attribs?.href === 'string',
 		processNode: (node, children) => (
 			<Link {...node.attribs} href={node.attribs?.href ?? ''}>
 				{children}
@@ -28,6 +40,6 @@ const HtmlParser = ({ html, instructions }) => {
 	}, [html, instructions]);
 
 	return <>{parsed}</>;
-}
+};
 
 export default HtmlParser;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layout from '@/components/layout';
 import Container from '@/components/container';
 import Link, { UnstyledLink } from '@/components/link';
 import Button from '@/components/button';
+import HtmlParser from '@/components/html-parser';
 
 export default function Home() {
 	return (
@@ -12,6 +13,8 @@ export default function Home() {
 				<Link href="/test" color="blue" className="text-xl">Test Link</Link>
 
 				<Button href="/test" as={UnstyledLink} color="red">Test</Button>
+
+				<HtmlParser html="<h3>testing</h3>" />
 			</Container>
 		</Layout>
 	);
