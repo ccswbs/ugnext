a3ae47f9c58ba2aa8be30becd56c17846ed96277
Raza Mahmood <razammahmood@gmail.com>
2024-04-26 12:11:38 -0400

    add requirements home page

---
+++ pages/admission/requirements/undergraduate/index.jsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/layout';
+import Heading from '@/components/heading';
+
+export default function AdmissionRequirementsHome() {
+	return (
+		<Layout>
+			<Heading level={1}>Undergraduate Admission Requirements</Heading>
+		</Layout>
+	);
+}
