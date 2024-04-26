62b8078c6459a5e0b466a93e00db3552856d87da
Raza Mahmood <razammahmood@gmail.com>
2024-04-26 00:06:14 -0400

    list style change

--- components/list.js
+++ components/list.js
@@ -7,7 +7,7 @@ const List = ({ variant = 'unordered', children, className, ...rest }) => {
 	const itemClasses = twJoin(
 		'h-fit w-full',
 		variant !== 'ordered' &&
-			'relative pl-8 before:absolute before:left-[1rem] before:top-[0.35rem] before:block before:h-[1.8rem] before:w-[1.1rem] before:shrink-0 before:items-center before:justify-center before:text-yellow before:content-[url("/icons/chevron-right.svg")]',
+			'relative pl-8 before:absolute before:left-[.5rem] before:top-[0.35rem] before:block before:h-[1.8rem] before:w-[1.1rem] before:shrink-0 before:items-center before:justify-center before:text-yellow before:content-[url("/icons/chevron-right.svg")]',
 		variant === 'ordered' &&
 			'relative pl-8 before:absolute before:left-0 before:inline-flex before:h-6 before:w-6 before:shrink-0 before:items-center before:justify-center before:bg-yellow before:font-black before:text-black before:content-[counter(list-number)] before:[counter-increment:list-number]',
 	);
