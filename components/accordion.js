80d20d64b4b4502cdfd81af12a8a67dda34a8833
Raza Mahmood <razammahmood@gmail.com>
2024-04-25 21:12:41 -0400

    formatting

--- components/accordion.js
+++ components/accordion.js
@@ -1,32 +1,30 @@
 import { Disclosure } from '@headlessui/react';
 import AnimateHeight from 'react-animate-height';
 import { twJoin } from 'tailwind-merge';
-const Accordion = ({ text, children }) => (
-	<Disclosure>
-		{({ open }) => (
-			<>
-				<Disclosure.Button
-					className={twJoin(
-						'mb-1 inline-flex w-full cursor-pointer items-center justify-between p-2.5 px-5 text-xl transition-colors hover:bg-blue hover:text-white focus:bg-blue focus:text-white',
-						open ? 'bg-blue text-white' : 'bg-blue-50 text-blue',
-					)}
-				>
-					<span>{text}</span>
-					<i
-						className={twJoin(
-							'fa-regular text-yellow transition-transform duration-200',
-							open ? 'fa-circle-minus' : 'fa-circle-plus rotate-90',
-						)}
-					></i>
-				</Disclosure.Button>
-				<AnimateHeight height={open ? 'auto' : 0} duration={200} easing={'ease-in-out'}>
-					<Disclosure.Panel static className="border-l-4 border-yellow py-2 pl-3">
-						{children}
-					</Disclosure.Panel>
-				</AnimateHeight>
-			</>
-		)}
-	</Disclosure>
-);
+const Accordion = ({ text, children }) => {
+	const buttonClasses = twJoin(
+		'mb-1 inline-flex w-full cursor-pointer items-center justify-between p-2.5 px-5 text-xl transition-colors hover:bg-blue hover:text-white focus:bg-blue focus:text-white',
+	);
+	const iconClasses = twJoin('fa-regular text-yellow transition-transform duration-200');
+	const panelClasses = twJoin('border-l-4 border-yellow py-2 pl-3');
+
+	return (
+		<Disclosure>
+			{({ open }) => (
+				<>
+					<Disclosure.Button className={twJoin(buttonClasses, open ? 'bg-blue text-white' : 'bg-blue-50 text-blue')}>
+						<span>{text}</span>
+						<i className={twJoin(iconClasses, open ? 'fa-circle-minus' : 'fa-circle-plus rotate-90')}></i>
+					</Disclosure.Button>
+					<AnimateHeight height={open ? 'auto' : 0} duration={200} easing={'ease-in-out'}>
+						<Disclosure.Panel static className={panelClasses}>
+							{children}
+						</Disclosure.Panel>
+					</AnimateHeight>
+				</>
+			)}
+		</Disclosure>
+	);
+};
 
 export default Accordion;
