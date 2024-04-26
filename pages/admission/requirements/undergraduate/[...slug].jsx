9a6c0d667bb700c3d4ad244dab4ae0f10377919b
Raza Mahmood <razammahmood@gmail.com>
2024-04-26 12:11:52 -0400

    add basic template page

---
+++ pages/admission/requirements/undergraduate/[...slug].jsx
@@ -0,0 +1,48 @@
+import { promises as fs } from 'fs';
+import YAML from 'yaml';
+import Layout from '@/components/layout';
+import { hierarchy, Requirement } from '@/lib/admission-requirements';
+import HtmlParser from '@/components/html-parser';
+
+export async function getStaticPaths() {
+	// For admission pages we don't want to prerender any pages, we will always generate them on demand.
+	return {
+		paths: [],
+		fallback: 'blocking',
+	};
+}
+
+export async function getStaticProps(context) {
+	const toFind = Requirement.parse(context?.params?.slug);
+
+	if (!toFind) {
+		return { notFound: true };
+	}
+
+	const data = await fs.readFile(process.cwd() + '/data/admission/admission-requirements.yml', 'utf8');
+	const requirements = YAML.parse(data);
+
+	const match = Requirement.findClosest(
+		toFind,
+		requirements.map((el) => new Requirement(el)),
+	);
+
+	if (!match) {
+		return { notFound: true };
+	}
+
+	return {
+		props: {
+			content: match?.content ?? '',
+			revalidate: 10, // Revalidate at most, every 10 seconds
+		},
+	};
+}
+
+export default function AdmissionRequirementsPage({ content }) {
+	return (
+		<Layout>
+			<HtmlParser html={content} />
+		</Layout>
+	);
+}
