e2971497a142908c63588352fbdf2e57d2c632d5
Raza Mahmood <razammahmood@gmail.com>
2024-04-25 23:19:56 -0400

    don't rely on fa web font

--- components/list.js
+++ components/list.js
@@ -7,7 +7,7 @@ const List = ({ variant = 'unordered', children, className, ...rest }) => {
 	const itemClasses = twJoin(
 		'h-fit w-full',
 		variant !== 'ordered' &&
-			'relative pl-8 before:absolute before:left-0 before:inline-flex before:h-6 before:w-6 before:shrink-0 before:items-center before:justify-center before:font-icon  before:font-black before:text-yellow before:content-chevron-right',
+			'relative pl-8 before:absolute before:left-[1rem] before:top-[0.35rem] before:block before:h-[1.8rem] before:w-[1.1rem] before:shrink-0 before:items-center before:justify-center before:text-yellow before:content-[url("/icons/chevron-right.svg")]',
 		variant === 'ordered' &&
 			'relative pl-8 before:absolute before:left-0 before:inline-flex before:h-6 before:w-6 before:shrink-0 before:items-center before:justify-center before:bg-yellow before:font-black before:text-black before:content-[counter(list-number)] before:[counter-increment:list-number]',
 	);
@@ -15,11 +15,23 @@ const List = ({ variant = 'unordered', children, className, ...rest }) => {
 	return (
 		<Tag {...rest} className={twMerge(classes, className)}>
 			{React.Children.toArray(children).map((child, index) => {
-				return child?.type === 'li' ? (
-					<li key={index} {...child?.props} className={twMerge(itemClasses, child?.props?.className)}>
-						{child?.props?.children}
-					</li>
-				) : (
+				if (child?.type?.name === 'List') {
+					return (
+						<li key={index} className={twMerge(itemClasses, 'before:content-none')}>
+							{child}
+						</li>
+					);
+				}
+
+				if (child?.type === 'li') {
+					return (
+						<li key={index} {...child?.props} className={twMerge(itemClasses, child?.props?.className)}>
+							{child?.props?.children}
+						</li>
+					);
+				}
+
+				return (
 					<li key={index} className={itemClasses}>
 						{child}
 					</li>
