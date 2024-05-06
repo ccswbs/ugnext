ce2256846ef6232e405d881dfebde38dfa5513a8
Raza Mahmood <razammahmood@gmail.com>
2024-05-06 16:15:56 -0400

    added ability for multi select

--- components/select.js
+++ components/select.js
@@ -5,7 +5,7 @@ import { twJoin } from 'tailwind-merge';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faCheck } from '@awesome.me/kit-7993323d0c/icons/classic/regular';
 
-const Select = ({ onChange, children }) => {
+const Select = ({ onChange, children, multiple = false, placeholder = 'Select a value...' }) => {
 	const options = useMemo(() => {
 		return React.Children.toArray(children)
 			.filter((child) => child?.type?.name === 'Option')
@@ -15,16 +15,21 @@ const Select = ({ onChange, children }) => {
 			}));
 	}, [children]);
 
-	const [selected, setSelected] = useState(options.find((option) => option?.selected) ?? options[0]);
+	const defaultSelected = multiple
+		? options.filter((option) => option?.selected) ?? []
+		: options.find((option) => option?.selected) ?? null;
+
+	const [selected, setSelected] = useState(defaultSelected);
 	const handleOnChange = (option) => {
-		setSelected(option);
+		setSelected(multiple ? [...option] : option);
 		onChange?.(option);
 	};
 
 	const containerClasses = twJoin('group relative');
 	const buttonClasses = twJoin(
-		'flex w-full items-center justify-between rounded-md border border-gray-300 px-3 py-2 shadow-sm group-focus-within:border-blue group-focus-within:outline-none ui-open:rounded-b-none',
+		'flex w-full items-center justify-between rounded-md border border-gray-300 px-4 py-2 shadow-sm group-focus-within:border-blue group-focus-within:outline-none ui-open:rounded-b-none',
 	);
+	const placeholderClasses = twJoin('text-gray-600');
 	const buttonIconClasses = twJoin('h-5 w-5 text-gray-400 transition-transform ui-open:rotate-180');
 	const optionsContainerClasses = twJoin(
 		'absolute z-10 w-full rounded-b-md border border-t-0 border-gray-300 bg-white shadow-md group-focus-within:border-blue group-focus-within:outline-none',
@@ -33,10 +38,19 @@ const Select = ({ onChange, children }) => {
 		'relative cursor-pointer select-none border-b border-gray-300 px-4 py-2 text-gray-900 transition-colors last:border-b-0 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none ui-active:bg-gray-100',
 	);
 
+	const buttonText = multiple ? selected[0]?.children : selected?.children;
+
 	return (
-		<Listbox value={selected} by="value" onChange={handleOnChange} as="div" className={containerClasses}>
+		<Listbox
+			value={selected}
+			by="value"
+			onChange={handleOnChange}
+			as="div"
+			className={containerClasses}
+			multiple={multiple}
+		>
 			<Listbox.Button className={buttonClasses}>
-				<span>{selected.children}</span>
+				{buttonText ? <span>{buttonText}</span> : <span className={placeholderClasses}>{placeholder}</span>}
 				<FontAwesomeIcon className={buttonIconClasses} icon={faChevronDown} />
 			</Listbox.Button>
 			<Transition
@@ -51,7 +65,7 @@ const Select = ({ onChange, children }) => {
 					{options.map((option, index) => (
 						<Listbox.Option className={optionClasses} key={option?.value} value={option} disabled={option?.disabled}>
 							{option?.children}
-							{selected?.value === option?.value && (
+							{(selected?.some?.((opt) => opt?.value === option?.value) || selected?.value === option?.value) && (
 								<span className="absolute inset-y-0 right-0 flex items-center pr-4 text-blue-600">
 									<FontAwesomeIcon icon={faCheck} className="h-5 w-5" />
 								</span>
