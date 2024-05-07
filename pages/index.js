4cbd246d35dead19700f1f53dcd1153378d4c73c
Raza Mahmood <razammahmood@gmail.com>
2024-05-07 14:48:27 -0400

    update home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import Layout from '@/components/layout';
-import Container from '@/components/container';
-import Link, { UnstyledLink } from '@/components/link';
-import Button from '@/components/button';
-import HtmlParser from '@/components/html-parser';
-import List from '@/components/list';
-import { Tabs, Tab } from '@/components/tabs';
-import Heading from '@/components/heading';
-import Accordion from '@/components/accordion';
-import Divider from '@/components/divider';
+import { Layout } from '@/components/layout';
+import { Container } from '@/components/container';
+import { Heading } from '@/components/heading';
+import { Divider } from '@/components/divider';
 
 export default function Home() {
 	return (
