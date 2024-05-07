c84d25fa89ea0ba1cdb74bcc732cff0f6ad9f61d
Raza Mahmood <razammahmood@gmail.com>
2024-05-07 16:26:04 -0400

    space

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { Layout } from '@/components/layout';
 import { Container } from '@/components/container';
 import { Heading } from '@/components/heading';
 import { Divider } from '@/components/divider';
-
 export default function Home() {
 	return (
 		<Layout title="Home">
