53e859ce104f6ea6710381b8ec3e543714b021cf
Raza Mahmood <razammahmood@gmail.com>
2024-05-07 09:12:13 -0400

    done text input maybe

--- components/text-input.js
+++ components/text-input.js
@@ -1,5 +1,19 @@
-const TextInput = () => {
+import { twMerge } from 'tailwind-merge';
 
-}
+const TextInput = ({ value, type = 'text', placeholder = '', onInput, className, ...rest }) => {
+	return (
+		<input
+			value={value}
+			type={type}
+			placeholder={placeholder}
+			onInput={onInput}
+			{...rest}
+			className={twMerge(
+				'w-full rounded-md border border-gray-300 px-4 py-2 transition-colors focus:border-blue focus:outline-none',
+				className,
+			)}
+		/>
+	);
+};
 
 export default TextInput;
