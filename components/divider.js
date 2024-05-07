fc5613c7fce45656975add00f3010557de99644c
Raza Mahmood <razammahmood@gmail.com>
2024-05-07 08:44:32 -0400

    fix divider

--- components/divider.js
+++ components/divider.js
@@ -1,5 +1,5 @@
 const Divider = () => {
-	return <hr className="bg-divider my-5 h-1 w-3/4 mx-auto" />;
+	return <hr className="bg-divider my-5 h-1 w-3/4 mx-auto border-0" />;
 };
 
 export default Divider;
