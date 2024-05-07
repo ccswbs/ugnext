1f6043af6a037c264a1feb6cea357bfa983f1571
Raza Mahmood <razammahmood@gmail.com>
2024-05-07 11:55:35 -0400

    add label and description for select

--- components/text-input.js
+++ components/text-input.js
@@ -1,18 +1,25 @@
 import { twMerge } from 'tailwind-merge';
+import { Description, Field, Input, Label } from '@headlessui/react';
 
-const TextInput = ({ value, type = 'text', placeholder = '', onInput, className, ...rest }) => {
+const TextInput = ({ value, type = 'text', placeholder = '', onInput, className, label, description, ...rest }) => {
 	return (
-		<input
-			value={value}
-			type={type}
-			placeholder={placeholder}
-			onInput={onInput}
-			{...rest}
-			className={twMerge(
-				'w-full rounded-md border border-gray-300 px-4 py-2 transition-colors focus:border-blue focus:outline-none',
-				className,
-			)}
-		/>
+		<Field className="flex flex-col gap-0.5">
+			{label && <Label>{label}</Label>}
+
+			<Input
+				value={value}
+				type={type}
+				placeholder={placeholder}
+				onInput={onInput}
+				{...rest}
+				className={twMerge(
+					'w-full rounded-md border border-gray-300 px-4 py-2 transition-colors focus:border-blue focus:outline-none',
+					className,
+				)}
+			/>
+
+			{description && <Description className="text-sm text-gray-500">{description}</Description>}
+		</Field>
 	);
 };
 
