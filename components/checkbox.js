0b42970b3f84d9572ee05de7a2d3e1091b7f4f87
Raza Mahmood <razammahmood@gmail.com>
2024-05-07 11:44:52 -0400

    add colors to checkbox

--- components/checkbox.js
+++ components/checkbox.js
@@ -2,22 +2,34 @@ import { Checkbox as HUICheckbox, Description, Field, Label } from '@headlessui/
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@awesome.me/kit-7993323d0c/icons/classic/regular';
+import { twJoin } from 'tailwind-merge';
 
-const Checkbox = ({ checked = false, label, description }) => {
+const Checkbox = ({ checked = false, label, description, color = 'red', disabled = false }) => {
 	const [enabled, setEnabled] = useState(checked);
+
+	const boxClasses = twJoin(
+		'group flex size-4 items-center justify-center rounded border bg-white p-3 transition-colors',
+		color === 'red' && 'focus-visible:ring-red ui-checked:bg-red',
+		color === 'yellow' && 'focus-visible:ring-yellow ui-checked:bg-yellow',
+		color === 'blue' && 'focus-visible:ring-blue ui-checked:bg-blue',
+		color === 'green' && 'focus-visible:ring-green ui-checked:bg-green',
+		color === 'grey' && 'focus-visible:ring-grey ui-checked:bg-grey',
+	);
+
+	const iconClasses = twJoin(
+		'h-5 w-5 opacity-0 transition-opacity group-data-[checked]:opacity-100',
+		color === 'red' && 'text-white',
+		color === 'yellow' && 'text-black',
+		color === 'blue' && 'text-white',
+		color === 'green' && 'text-white',
+		color === 'grey' && 'text-black',
+	);
 
 	return (
 		<Field>
 			<div className="flex items-center gap-2">
-				<HUICheckbox
-					checked={enabled}
-					onChange={setEnabled}
-					className="group flex size-4 items-center justify-center rounded border bg-white p-3 transition-colors ui-checked:bg-blue-500"
-				>
-					<FontAwesomeIcon
-						className="h-5 w-5 opacity-0 transition-opacity text-white group-data-[checked]:opacity-100"
-						icon={faCheck}
-					/>
+				<HUICheckbox checked={enabled} onChange={setEnabled} disabled={disabled} className={boxClasses}>
+					<FontAwesomeIcon className={iconClasses} icon={faCheck} />
 				</HUICheckbox>
 				{label && <Label>{label}</Label>}
 			</div>
