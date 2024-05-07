05c1b807964049e6c18e70749d53286529c99d91
Raza Mahmood <razammahmood@gmail.com>
2024-05-07 12:05:06 -0400

    add label and description for select

--- components/select.js
+++ components/select.js
@@ -1,11 +1,11 @@
 import React, { useMemo } from 'react';
 import { useState } from 'react';
-import { Listbox, Transition } from '@headlessui/react';
+import { Description, Field, Label, Listbox, Transition } from '@headlessui/react';
 import { twJoin } from 'tailwind-merge';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faCheck } from '@awesome.me/kit-7993323d0c/icons/classic/regular';
 
-const Select = ({ onChange, children, multiple = false, placeholder = 'Select a value...' }) => {
+const Select = ({ onChange, children, multiple = false, label, description, placeholder = 'Select a value...' }) => {
 	const options = useMemo(() => {
 		return React.Children.toArray(children)
 			.filter((child) => child?.type?.name === 'Option')
@@ -41,40 +41,46 @@ const Select = ({ onChange, children, multiple = false, placeholder = 'Select a 
 	const buttonText = multiple ? selected[0]?.children : selected?.children;
 
 	return (
-		<Listbox
-			value={selected}
-			by="value"
-			onChange={handleOnChange}
-			as="div"
-			className={containerClasses}
-			multiple={multiple}
-		>
-			<Listbox.Button className={buttonClasses}>
-				{buttonText ? <span>{buttonText}</span> : <span className={placeholderClasses}>{placeholder}</span>}
-				<FontAwesomeIcon className={buttonIconClasses} icon={faChevronDown} />
-			</Listbox.Button>
-			<Transition
-				enter="transition duration-100 ease-out"
-				enterFrom="opacity-0"
-				enterTo="opacity-100"
-				leave="transition duration-75 ease-out"
-				leaveFrom="opacity-100"
-				leaveTo="opacity-0"
+		<Field className="flex flex-col gap-0.5">
+			{label && <Label>{label}</Label>}
+
+			<Listbox
+				value={selected}
+				by="value"
+				onChange={handleOnChange}
+				as="div"
+				className={containerClasses}
+				multiple={multiple}
 			>
-				<Listbox.Options className={optionsContainerClasses}>
-					{options.map((option, index) => (
-						<Listbox.Option className={optionClasses} key={option?.value} value={option} disabled={option?.disabled}>
-							{option?.children}
-							{(selected?.some?.((opt) => opt?.value === option?.value) || selected?.value === option?.value) && (
-								<span className="absolute inset-y-0 right-0 flex items-center pr-4 text-blue-600">
-									<FontAwesomeIcon icon={faCheck} className="h-5 w-5" />
-								</span>
-							)}
-						</Listbox.Option>
-					))}
-				</Listbox.Options>
-			</Transition>
-		</Listbox>
+				<Listbox.Button className={buttonClasses}>
+					{buttonText ? <span>{buttonText}</span> : <span className={placeholderClasses}>{placeholder}</span>}
+					<FontAwesomeIcon className={buttonIconClasses} icon={faChevronDown} />
+				</Listbox.Button>
+				<Transition
+					enter="transition duration-100 ease-out"
+					enterFrom="opacity-0"
+					enterTo="opacity-100"
+					leave="transition duration-75 ease-out"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
+				>
+					<Listbox.Options className={optionsContainerClasses}>
+						{options.map((option, index) => (
+							<Listbox.Option className={optionClasses} key={option?.value} value={option} disabled={option?.disabled}>
+								{option?.children}
+								{(selected?.some?.((opt) => opt?.value === option?.value) || selected?.value === option?.value) && (
+									<span className="absolute inset-y-0 right-0 flex items-center pr-4 text-blue-600">
+										<FontAwesomeIcon icon={faCheck} className="h-5 w-5" />
+									</span>
+								)}
+							</Listbox.Option>
+						))}
+					</Listbox.Options>
+				</Transition>
+			</Listbox>
+
+			{description && <Description className="text-sm text-gray-500">{description}</Description>}
+		</Field>
 	);
 };
 
