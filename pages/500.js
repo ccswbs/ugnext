cf02fa1b5c6475d22819183bf8655ff492e65a1b
Raza Mahmood <razammahmood@gmail.com>
2024-05-07 13:34:26 -0400

    update imports

--- pages/500.js
+++ pages/500.js
@@ -1,5 +1,5 @@
-import Layout from '@/components/layout';
-import Container from '@/components/container';
+import { Layout } from '@/components/layout';
+import { Container } from '@/components/container';
 
 export default function ServerError() {
 	return (
