34edf5c5f6c9ec01e80521d9c615aa916552a7e6
Raza Mahmood <razammahmood@gmail.com>
2024-05-08 16:08:01 -0400

    add other pages

--- pages/programs/certificate-and-diploma.js
+++ pages/programs/certificate-and-diploma.js
@@ -1,14 +1,34 @@
-import React from "react";
-import { Container } from "@/components/container";
+import React from 'react';
+import { Container } from '@/components/container';
 import { Layout } from '@/components/layout';
 import { Heading } from '@/components/heading';
+import { join } from 'path';
+import { readYamlFile } from '@/lib/file-utils';
+import { ProgramSearch } from '@/components/programs/search';
 
-export default function ProgramsCertificateAndDiploma({ data, children }) {
-  return (
-    <Layout>
-      <Container centered>
-        <Heading level={1}>Certificate and Diploma Programs at the University of Guelph</Heading>
-      </Container>
-    </Layout>
-  );
+export async function getStaticProps() {
+	const path = join(process.cwd(), 'data', 'programs', 'certificate-and-diploma.yml');
+	const programs = await readYamlFile(path);
+
+	return {
+		props: {
+			programs: programs,
+		},
+		// Next.js will attempt to re-generate the page:
+		// - When a request comes in
+		// - At most once every 10 seconds
+		revalidate: 10, // In seconds
+	};
+}
+
+export default function ProgramsCertificateAndDiploma({ programs }) {
+	return (
+		<Layout>
+			<Container centered>
+				<Heading level={1}>Certificate and Diplomas at the University of Guelph</Heading>
+
+				<ProgramSearch programs={programs} />
+			</Container>
+		</Layout>
+	);
 }
