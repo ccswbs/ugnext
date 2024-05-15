e00ae173ac72a4cd46b14955327d8bcba37df3a2
Raza Mahmood <razammahmood@gmail.com>
2024-05-15 09:11:38 -0400

    get drupal graphql fetching setup (testing with spotlight)

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,79 @@ import { Layout } from '@/components/layout';
 import { Container } from '@/components/container';
 import { Heading } from '@/components/heading';
 import { Divider } from '@/components/divider';
-export default function Home() {
+import { graphql } from '@/lib/drupal';
+
+export async function getStaticProps() {
+	const { data } = await graphql(`
+		query {
+			hero: spotlightHero {
+				results {
+					... on NodeSpotlight {
+						title
+						caption
+						captionAlignment
+						image {
+							... on MediaImage {
+								id
+								name
+								image {
+									width
+									url
+									title
+									mime
+									height
+									alt
+								}
+							}
+						}
+						url {
+							title
+							url
+						}
+					}
+				}
+			}
+			cards: spotlightCards {
+				results {
+					... on NodeSpotlight {
+						id
+						title
+						thumbnailImageCropping
+						rank
+						image {
+							... on MediaImage {
+								id
+								name
+								image {
+									width
+									url
+									title
+									mime
+									height
+									alt
+								}
+							}
+						}
+						url {
+							title
+							url
+						}
+					}
+				}
+			}
+		}
+	`);
+
+	return {
+		props: {
+			data: data,
+		},
+	};
+}
+
+export default function Home(data) {
+	console.log(data);
+
 	return (
 		<Layout title="Home">
 			<Container centered>
