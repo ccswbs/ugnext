22f2ac6c9ca0bc2dc8082d3173bb91809774723c
Raza Mahmood <razammahmood@gmail.com>
2024-05-15 14:17:17 -0400

    formatting

--- pages/programs/graduate.js
+++ pages/programs/graduate.js
@@ -35,12 +35,16 @@ export default function ProgramsGraduate({ programs }) {
 					filterer={filter}
 					render={(program) => (
 						<UnstyledLink
-							className="group focus:visible:ring-offset-2 flex flex-col justify-center transition hover:scale-105 focus:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-light-blue"
+							className="focus:visible:ring-offset-2 group flex flex-col justify-center transition hover:scale-105 focus:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-light-blue"
 							href={program.url}
 							key={program.id + program.url}
 						>
-							<span className="bg-light-blue-50 p-5 transition-colors group-hover:bg-light-blue-100">{program.title}</span>
-							<span className="bg-light-blue-100 transition-colors px-5 py-2 border-t border-t-blue-200">{program?.degrees?.join(', ')}</span>
+							<span className="bg-light-blue-50 p-5 transition-colors group-hover:bg-light-blue-100">
+								{program.title}
+							</span>
+							<span className="border-t border-t-blue-200 bg-light-blue-100 px-5 py-2 transition-colors">
+								{program?.degrees?.join(', ')}
+							</span>
 						</UnstyledLink>
 					)}
 				>
