94dc30235740e34eaa5db23552896c3322959f6e
Raza Mahmood <razammahmood@gmail.com>
2024-05-15 14:57:11 -0400

    show types for undergraduate

--- pages/programs/undergraduate.js
+++ pages/programs/undergraduate.js
@@ -6,6 +6,8 @@ import { join } from 'path';
 import { readYamlFile } from '@/lib/file-utils';
 import { ProgramSearch } from '@/components/programs/search';
 import { ProgramNavigation } from '@/components/programs/navigation';
+import { UnstyledLink } from '@/components/link';
+import { toTitleCase } from '@/lib/string-utils';
 
 export async function getStaticProps() {
 	const path = join(process.cwd(), 'data', 'programs', 'undergraduate.yml');
@@ -23,10 +25,26 @@ export default function ProgramsUndergraduate({ programs }) {
 		<Layout title="Undergraduate Programs">
 			<Container centered>
 				<Heading level={1}>Undergraduate Programs at the University of Guelph</Heading>
-				
+
 				<ProgramNavigation />
 
-				<ProgramSearch programs={programs} />
+				<ProgramSearch
+					programs={programs}
+					render={(program) => (
+						<UnstyledLink
+							className="focus:visible:ring-offset-2 group flex flex-col justify-center transition hover:scale-105 focus:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-light-blue"
+							href={program.url}
+							key={program.id + program.url}
+						>
+							<span className="bg-light-blue-50 p-5 transition-colors group-hover:bg-light-blue-100">
+								{program.title}
+							</span>
+							<span className="border-t border-t-blue-200 bg-light-blue-100 px-5 py-2 transition-colors">
+								{program?.types?.map(type => toTitleCase(type)).join(', ')}
+							</span>
+						</UnstyledLink>
+					)}
+				/>
 			</Container>
 		</Layout>
 	);
