6d476bf8e5224d8a426a2da457885fe2bd27d23d
Raza Mahmood <razammahmood@gmail.com>
2024-05-15 11:16:56 -0400

    working on adding addition filter for graduate (need radio)

--- components/programs/search.js
+++ components/programs/search.js
@@ -5,13 +5,17 @@ import { useSearch } from '@/lib/use-search';
 import { useMemo, useState } from 'react';
 import { UnstyledLink } from '@/components/link';
 
-export const ProgramSearch = ({ programs, children, filterer, sidebar }) => {
+export const ProgramSearch = ({ programs, children, filterer }) => {
 	const [input, setInput] = useState('');
-	const types = Array.from(new Set(programs.flatMap((program) => program.types))).map((type) => ({
-		value: type,
-		label: type === 'co-op' ? 'Co-op' : toTitleCase(type?.replaceAll('-', ' ')),
-		selected: true,
-	}));
+	const types = useMemo(
+		() =>
+			Array.from(new Set(programs.flatMap((program) => program.types))).map((type) => ({
+				value: type,
+				label: type === 'co-op' ? 'Co-op' : toTitleCase(type?.replaceAll('-', ' ')),
+				selected: true,
+			})),
+		[programs],
+	);
 	const [selectedTypes, setSelectedTypes] = useState(types);
 
 	const matching = useSearch(programs, input);
@@ -19,6 +23,7 @@ export const ProgramSearch = ({ programs, children, filterer, sidebar }) => {
 		() =>
 			matching
 				?.filter((program) => selectedTypes.some((type) => program.types.includes(type.value)))
+				?.filter((program) => (typeof filterer === 'function' ? filterer(program) : () => true))
 				?.map((program) => (
 					<UnstyledLink
 						className="focus:visible:ring-offset-2 flex items-center bg-light-blue-50 p-5 transition hover:scale-105 hover:bg-light-blue-100 focus:scale-105 focus:bg-light-blue-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-light-blue"
@@ -28,30 +33,31 @@ export const ProgramSearch = ({ programs, children, filterer, sidebar }) => {
 						{program.title}
 					</UnstyledLink>
 				)),
-		[matching, selectedTypes],
+		[filterer, matching, selectedTypes],
 	);
 
 	return (
 		<>
-			<div className="flex flex-col gap-4 sm:flex-row">
-				<div className="flex-1">
-					<TextInput onInput={(value) => setInput(value)} label="What would you like to study?" />
+			<div className="flex flex-col gap-3">
+				<div className="flex flex-col gap-4 sm:flex-row">
+					<div className="flex-1">
+						<TextInput onInput={(value) => setInput(value)} label="What would you like to study?" />
+					</div>
+
+					<div className="sm:w-1/3 md:w-1/4">
+						<Select
+							multiple
+							options={types}
+							onChange={(options) => {
+								setSelectedTypes(options);
+							}}
+							label="Filter by type"
+						/>
+					</div>
 				</div>
 
-				<div className="sm:w-1/3 md:w-1/4">
-					<Select
-						multiple
-						options={types}
-						onChange={(options) => {
-							setSelectedTypes(options);
-						}}
-						label="Filter by type"
-					/>
-				</div>
+				<div className="flex flex-col justify-between">{children}</div>
 			</div>
-
-			<div className="flex flex-col justify-between">{children}</div>
-
 			<div className="mt-5 grid grid-cols-[repeat(auto-fit,_minmax(30rem,_1fr))] gap-5">{filtered}</div>
 		</>
 	);
--- pages/programs/graduate.js
+++ pages/programs/graduate.js
@@ -6,6 +6,7 @@ import { join } from 'path';
 import { readYamlFile } from '@/lib/file-utils';
 import { ProgramSearch } from '@/components/programs/search';
 import { ProgramNavigation } from '@/components/programs/navigation';
+import { Select } from '@/components/select';
 
 export async function getStaticProps() {
 	const path = join(process.cwd(), 'data', 'programs', 'graduate.yml');
@@ -26,7 +27,26 @@ export default function ProgramsGraduate({ programs }) {
 
 				<ProgramNavigation />
 
-				<ProgramSearch programs={programs} />
+				<ProgramSearch programs={programs}>
+					<div className="ml-auto sm:w-1/3 md:w-1/4">
+						<Select
+							multiple
+							options={[
+								{
+									value: 'phd',
+									label: 'PhD',
+									selected: true,
+								},
+								{
+									value: 'masters',
+									label: 'Masters',
+									selected: true,
+								},
+							]}
+							label="Filter by type"
+						/>
+					</div>
+				</ProgramSearch>
 			</Container>
 		</Layout>
 	);
