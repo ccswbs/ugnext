42f9185b212dde9380f4e8ee26aff0ae6cbc535a
Raza Mahmood <razammahmood@gmail.com>
2024-05-16 16:21:44 -0400

    add hero component

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { SpotlightCards } from '@/components/home/spotlight-cards';
 import { TagLine } from '@/components/home/tag-line';
 import { Rankings } from '@/components/home/rankings';
 import { ThreeCampuses } from '@/components/home/three-campuses';
+import { Hero } from '@/components/hero';
 
 export async function getStaticProps() {
 	const { data } = await graphql(`
@@ -14,6 +15,7 @@ export async function getStaticProps() {
 				results {
 					... on NodeSpotlight {
 						title
+						thumbnailImageCropping
 						caption
 						captionAlignment
 						image {
@@ -51,7 +53,6 @@ export async function getStaticProps() {
 							}
 						}
 						url {
-							title
 							url
 						}
 					}
@@ -71,6 +72,19 @@ export async function getStaticProps() {
 export default function Home({ cards, hero }) {
 	return (
 		<Layout title="Home">
+			<Hero
+				title={hero.title}
+				caption={hero.caption}
+				href={hero.url.url}
+				button={hero.url.title}
+				src={hero.image.image.url}
+				alt={hero.image.image.alt}
+				height={hero.image.image.height}
+				width={hero.image.image.width}
+				crop={hero.thumbnailImageCropping}
+				alignment={hero.captionAlignment}
+			/>
+
 			<Container centered>
 				<TagLine />
 
