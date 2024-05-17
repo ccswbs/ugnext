f0fef7121e172cfc186ffc07817b0d5b74f7c2df
Raza Mahmood <razammahmood@gmail.com>
2024-05-17 12:15:55 -0400

    formatting

--- components/link-carousel.js
+++ components/link-carousel.js
@@ -23,7 +23,7 @@ export const LinkCarousel = ({ links }) => {
 						leaveTo="opacity-0"
 					>
 						<Image
-							className="absolute left-0 top-0 h-full object-cover object-left hidden md:block"
+							className="absolute left-0 top-0 hidden h-full object-cover object-left md:block"
 							src={link.image.url}
 							alt={link.image?.alt}
 						/>
@@ -31,8 +31,11 @@ export const LinkCarousel = ({ links }) => {
 				))}
 			</div>
 
-			<div className="absolute bottom-0 left-0 z-10 w-full px-4 py-4 text-white hidden md:block">{activeLink?.image?.caption}</div>
-			<div className="absolute bottom-0 left-0 z-0 h-1/2 w-full bg-gradient-to-t from-black/60 to-black/0 hidden md:block"></div>
+			<div className="absolute bottom-0 left-0 z-10 hidden w-full px-4 py-4 text-white md:block">
+				{activeLink?.image?.caption}
+			</div>
+
+			<div className="absolute bottom-0 left-0 z-0 hidden h-1/2 w-full bg-gradient-to-t from-black/60 to-black/0 md:block"></div>
 
 			<div className="relative z-10 ml-auto flex w-full flex-col gap-2 md:w-1/3">
 				{links.map((link, index) => (
