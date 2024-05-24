ed87f1508238ca9973604b68e9a7d23d85f42639
Raza Mahmood <razammahmood@gmail.com>
2024-05-24 15:19:49 -0400

    fixing spotlight

--- pages/index.js
+++ pages/index.js
@@ -12,119 +12,141 @@ import { StudyHere } from '@/components/home/study-here';
 
 export async function getStaticProps(context) {
 	const isPreview = context?.preview || process.env.NODE_ENV !== 'production';
+	const status = isPreview ? null : true;
 
-	let result;
+	// Look through the latest revisions of spotlight items, filtered to only those that are rank 1 (and published if not in preview mode).
+	const hero = (
+		await graphql(
+			`
+				query SpotlightHero($status: Boolean = null) {
+					spotlightRevisions(pageSize: 1, filter: { rank: ["1"], status: $status }) {
+						results {
+							... on NodeSpotlight {
+								id
+								caption
+								captionAlignment
+								thumbnailImageCropping
+								title
+								url {
+									url
+									title
+								}
+								image {
+									... on MediaImage {
+										image {
+											height
+											alt
+											url
+											width
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			`,
+			{
+				status: status,
+			},
+		)
+	)?.data?.spotlightRevisions?.results?.[0];
 
-	if (isPreview) {
-		result = await graphql(`
-			query {
-				hero: previewSpotlightHero {
-					results {
-						... on NodeSpotlight {
-							title
-							thumbnailImageCropping
-							caption
-							captionAlignment
-							image {
-								... on MediaImage {
-									image {
-										width
-										url
-										height
-										alt
-									}
+	// For spotlight cards our query is a bit more involved. We look through the latest revisions (5 at once this time) like we did for the hero, but because we need 4 cards, and a single spotlight can have many revisions, we keep track of the ids of the spotlights we've seen and keep requesting more until we have 4 unique ids. With those ids. we can request the content separately.
+
+	const ids = new Set();
+	let isLastPage = false;
+	let page = 0;
+
+	outer: while (!isLastPage) {
+		const cards = (
+			await graphql(
+				`
+					query SpotlightCardIDs($status: Boolean = null, $page: Int = 0) {
+						spotlightRevisions(filter: { rank: [], status: $status }, page: $page, pageSize: 5) {
+							results {
+								... on NodeSpotlight {
+									id
 								}
-							}
-							url {
-								title
-								url
 							}
 						}
 					}
-				}
-				cards: previewSpotlightCards {
-					results {
-						... on NodeSpotlight {
-							id
-							title
-							thumbnailImageCropping
-							rank
-							image {
-								... on MediaImage {
-									image {
-										width
-										url
-										height
-										alt
-									}
-								}
-							}
-							url {
-								url
-							}
-						}
-					}
-				}
+				`,
+				{
+					status: status,
+					page: page,
+				},
+			)
+		)?.data?.spotlightRevisions?.results;
+
+		for (const card of cards) {
+			if (card.id === hero.id) {
+				continue;
 			}
-		`);
-	} else {
-		result = await graphql(`
-			query {
-				hero: spotlightHero {
-					results {
-						... on NodeSpotlight {
-							title
-							thumbnailImageCropping
-							caption
-							captionAlignment
-							image {
-								... on MediaImage {
-									image {
-										width
-										url
-										height
-										alt
-									}
-								}
-							}
-							url {
-								title
-								url
-							}
-						}
-					}
-				}
-				cards: spotlightCards {
-					results {
-						... on NodeSpotlight {
-							id
-							title
-							thumbnailImageCropping
-							rank
-							image {
-								... on MediaImage {
-									image {
-										width
-										url
-										height
-										alt
-									}
-								}
-							}
-							url {
-								url
-							}
-						}
-					}
-				}
+
+			if (ids.size === 4) {
+				break outer;
 			}
-		`);
+
+			ids.add(card.id);
+		}
+
+		// The page wasn't full, meaning we were on the last page.
+		if (cards.length < 5) {
+			isLastPage = true;
+		}
+
+		page++;
 	}
+
+	// Now that we have the ids, we can retrieve the content.
+	const cards = await Promise.all(
+		Array.from(ids).map(async (id) => {
+			const { data } = await graphql(
+				`
+					query SpotlightCard($status: Boolean = false, $id: String = "") {
+						spotlightRevisions(filter: { status: $status, id: $id }, pageSize: 1) {
+							results {
+								... on NodeSpotlight {
+									image {
+										... on MediaImage {
+											image {
+												alt
+												height
+												width
+												url
+											}
+										}
+									}
+									rank
+									title
+									thumbnailImageCropping
+									url {
+										url
+										title
+									}
+								}
+							}
+						}
+					}
+				`,
+				{
+					status: status,
+					id: id,
+				},
+			);
+
+			return {
+				id: id,
+				...(data.spotlightRevisions.results?.[0] ?? {}),
+			};
+		}),
+	);
 
 	return {
 		props: {
-			cards: result?.data?.cards?.results?.slice(0, 4) ?? [],
-			hero: result?.data?.hero?.results?.[0] ?? null,
+			cards: cards.sort((a, b) => a.rank - b.rank),
+			hero: hero,
 		},
 	};
 }
@@ -132,7 +154,7 @@ export async function getStaticProps(context) {
 export default function Home({ cards, hero }) {
 	return (
 		<Layout title="Home">
-			<SpotlightHero hero={hero} />
+			{hero && <SpotlightHero hero={hero} />}
 
 			<Container centered>
 				<TagLine />
