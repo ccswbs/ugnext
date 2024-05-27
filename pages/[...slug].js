5618a8d9fc0c6d299e6d5ee06c430fc5eaa1bee7
Raza Mahmood <razammahmood@gmail.com>
2024-05-27 16:45:19 -0400

    getting pages, need to prebuild

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -3,22 +3,28 @@ import { graphql } from '@/lib/drupal';
 import { Layout } from '@/components/layout';
 import { Container } from '@/components/container';
 import { Heading } from '@/components/heading';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@awesome.me/kit-7993323d0c/icons/classic/solid';
+import { twJoin } from 'tailwind-merge';
 
 export async function getStaticPaths() {
 	let paths = [];
 
 	return {
 		paths: paths,
-		fallback: 'blocking',
+		fallback: true,
 	};
 }
 
 export async function getStaticProps(context) {
+	const isPreview = context?.preview || process.env.NODE_ENV !== 'production';
+	const status = isPreview ? null : true;
+
 	// Try to get the ID of the page the user is requesting.
 	const id = (
 		await graphql(
 			`
-				query GetPage($url: String!) {
+				query GetPageID($url: String!) {
 					route(path: $url) {
 						... on RouteInternal {
 							entity {
@@ -43,15 +49,107 @@ export async function getStaticProps(context) {
 		};
 	}
 
+	// Now that we have the ID for the page we can request its content from its latest revision.
+	const page = (
+		await graphql(
+			`
+				query GetPage($id: String = "", $status: Boolean = false) {
+					contentRevisions(filter: { id: $id, status: $status, type: "page" }, pageSize: 1) {
+						results {
+							... on NodePage {
+								primaryNavigation {
+									... on TermPrimaryNavigation {
+										name
+									}
+								}
+								image {
+									... on MediaImage {
+										image {
+											alt
+											height
+											width
+											url
+										}
+									}
+								}
+								title
+							}
+						}
+					}
+				}
+			`,
+			{
+				id: id,
+				status: status,
+			},
+		)
+	)?.data?.contentRevisions?.results[0];
+
+	// Get the menu name if the page has one.
+	const menuName = page?.primaryNavigation?.name?.toUpperCase().replaceAll('-', '_');
+
+	delete page.primaryNavigation;
+
+	// We have to request the menu links separately.
+	if (menuName) {
+		// Since graphql doesn't allow recursive queries, we limit the depth of a menu to 6.
+		const menu = (
+			await graphql(
+				`
+					fragment MenuItemContent on MenuItem {
+						url
+						title
+					}
+
+					query GetMenu($menu: MenuAvailable!) {
+						menu(name: $menu) {
+							items {
+								...MenuItemContent
+								children {
+									...MenuItemContent
+									children {
+										...MenuItemContent
+										children {
+											...MenuItemContent
+											children {
+												...MenuItemContent
+												children {
+													...MenuItemContent
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				`,
+				{
+					menu: menuName,
+				},
+			)
+		)?.data?.menu?.items;
+
+		page.menu = menu;
+	}
+
 	return {
-		props: { data: {} },
+		props: { data: page },
 	};
 }
 
 export default function Page({ data }) {
+	const { isFallback } = useRouter();
+	console.log(data);
+
 	return (
 		<Layout>
-			<Container centered>
+			{isFallback && (
+				<div className="flex flex-1 w-full items-center justify-center">
+					<FontAwesomeIcon className="w-[5em] animate-spin" icon={faSpinner} />
+				</div>
+			)}
+			<Container className={twJoin(isFallback && 'hidden')} centered>
 				<Heading level={1}>{data?.title}</Heading>
 			</Container>
 		</Layout>
