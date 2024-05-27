5347aa9425d860b3f50727e104ff44356bfafaf7
Raza Mahmood <razammahmood@gmail.com>
2024-05-27 14:32:59 -0400

    working on using revisions

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,83 +1,59 @@
 import { useRouter } from 'next/router';
 import { graphql } from '@/lib/drupal';
+import { Layout } from '@/components/layout';
+import { Container } from '@/components/container';
+import { Heading } from '@/components/heading';
 
 export async function getStaticPaths() {
 	let paths = [];
-	let cursor = '';
-	let hasNextPage = true;
 
-	while (hasNextPage) {
-		const { data } = await graphql(
+	return {
+		paths: paths,
+		fallback: 'blocking',
+	};
+}
+
+export async function getStaticProps(context) {
+	// Try to get the ID of the page the user is requesting.
+	const id = (
+		await graphql(
 			`
-				query GetPages($cursor: Cursor) {
-					nodePages(first: 100, after: $cursor) {
-						edges {
-							node {
-								path
+				query GetPage($url: String!) {
+					route(path: $url) {
+						... on RouteInternal {
+							entity {
+								... on NodePage {
+									id
+								}
 							}
-						}
-						pageInfo {
-							hasNextPage
-							endCursor
 						}
 					}
 				}
 			`,
 			{
-				cursor: cursor,
+				url: '/' + context.params.slug.join('/'),
 			},
-		);
+		)
+	)?.data?.route?.entity?.id;
 
-		cursor = data?.nodePages?.pageInfo?.endCursor ?? '';
-		hasNextPage = data?.nodePages?.pageInfo?.hasNextPage ?? false;
-		paths = paths.concat(
-			data?.nodePages?.edges?.map(({ node }) => {
-				return {
-					params: {
-						slug: node.path?.split('/').filter(Boolean),
-					},
-				};
-			}),
-		);
+	// If we couldn't resolve an id, then that means this page doesn't exist on content hub, show a 404.
+	if (!id) {
+		return {
+			notFound: true,
+		};
 	}
 
 	return {
-		paths: paths,
-		fallback: false,
-	};
-}
-
-export async function getStaticProps(context) {
-	const { data, errors } = await graphql(
-		`
-			query GetPage($url: String!) {
-				route(path: $url) {
-					... on RouteInternal {
-						entity {
-							... on NodePage {
-								title
-							}
-						}
-					}
-				}
-			}
-		`,
-		{
-			url: '/' + context.params.slug.join('/'),
-		},
-	);
-
-	if (errors) {
-		throw new Error(errors);
-	}
-
-	return {
-		props: { data: data.route.entity },
+		props: { data: {} },
 	};
 }
 
 export default function Page({ data }) {
-	const router = useRouter();
-
-	return <p>Post: {JSON.stringify(data)}</p>;
+	return (
+		<Layout>
+			<Container centered>
+				<Heading level={1}>{data?.title}</Heading>
+			</Container>
+		</Layout>
+	);
 }
