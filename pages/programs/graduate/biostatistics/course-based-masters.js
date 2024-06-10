81a777b8364cfcae62ec90a520c0e51951f72e45
Miranda Mafe <miranda.mafe@gmail.com>
2024-06-10 17:01:46 -0400

    Add program data

--- pages/programs/graduate/biostatistics/course-based-masters.js
+++ pages/programs/graduate/biostatistics/course-based-masters.js
@@ -8,14 +8,13 @@ import { ButtonNavigation } from '@/components/programs/graduate/navigation-butt
 import { GraduateProgramSummary } from '@/components/programs/graduate/summary';
 import { GraduateProgramInfo } from '@/components/programs/graduate/information';
 
-
 export async function getStaticProps() {
-	const path = join(process.cwd(), 'data', 'programs', 'graduate.yml');
-	const programs = await readYamlFile(path);
+	const path = join(process.cwd(), 'data', 'programs', 'graduate','biostatistics.yml');
+	const program = await readYamlFile(path);
 
 	return {
 		props: {
-			programs: programs,
+			program: program,
 		},
 	};
 }
@@ -24,18 +23,20 @@ export async function getStaticProps() {
 @TODO: Send links for button navigation
 */
 
-export default function ProgramGraduate() {
+export default function ProgramGraduate({program}) {
 	return (
 		<Layout title="Graduate Program">
 			<GraduateProgramHero />
+
 			<Container centered>
-				<Heading level={1}>Biostatistics | Master of Science - Course-based (MSc)</Heading>
+				<Heading level={1}> {program.title} | Master of Science - Course-based (MSc)</Heading>
 				<ButtonNavigation label="Program Navigation" />
 
 				<div className="pt-1 grid xl:grid-cols-12 xl:gap-x-10">
 					<div className='xl:col-span-9'>
 						<GraduateProgramInfo />
 					</div>
+					
 					
 					<div className='xl:col-span-3'>
 						<GraduateProgramSummary />
