a41e80ae722f54bdb835ec61d5dc6f099ad2a666
Raza Mahmood <razammahmood@gmail.com>
2024-06-14 13:44:16 -0400

    extracting data logic

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,161 +1,17 @@
 import { useRouter } from 'next/router';
-import { graphql } from '@/lib/drupal';
 import { Layout } from '@/components/layout';
 import { Container } from '@/components/container';
 import { Heading } from '@/components/heading';
 import { twJoin } from 'tailwind-merge';
 import { Hero } from '@/components/hero';
+import { getPageContent, getPageID, getPageMenu, getPaths } from '@/lib/data/drupal/basic-pages';
 
 export async function getStaticPaths() {
-	// Here we can decide which pages get pre-rendered.
-	let paths = [];
-	let page = 0;
-	const pageSize = 100;
-	let hasNextPage = true;
-	const limit = 1500; // Add a limit to the number of pages we prebuild
-
-	while (hasNextPage && paths.length < limit) {
-		const results = (
-			await graphql(
-				`
-					query GetPages($page: Int = 0, $pageSize: Int = 100) {
-						content(filter: { type: "page", status: true }, page: $page, pageSize: $pageSize) {
-							results {
-								... on NodePage {
-									path
-								}
-							}
-						}
-					}
-				`,
-				{
-					page: page,
-					pageSize: pageSize,
-				},
-			)
-		)?.data?.content?.results;
-
-		results?.length === pageSize ? page++ : (hasNextPage = false);
-		paths = [...paths, ...results];
-	}
-
 	return {
-		paths: paths.map((node) => ({
-			params: {
-				slug: node?.path?.split('/').filter(Boolean),
-			},
-		})),
+		paths: await getPaths(),
 		fallback: true,
 	};
 }
-
-async function getPageID(url) {
-	const { data } = await graphql(
-		`
-			query GetPageID($url: String!) {
-				route(path: $url) {
-					... on RouteInternal {
-						entity {
-							... on NodePage {
-								id
-							}
-						}
-					}
-				}
-			}
-		`,
-		{
-			url: url,
-		},
-	);
-
-	return data?.route?.entity?.id;
-}
-
-async function getPageContent(id, status) {
-	const { data } = await graphql(
-		`
-			query GetPage($id: String = "", $status: Boolean = null) {
-				contentRevisions(filter: { id: $id, status: $status, type: "page" }, pageSize: 1) {
-					results {
-						... on NodePage {
-							primaryNavigation {
-								... on TermPrimaryNavigation {
-									name
-								}
-							}
-							image {
-								... on MediaImage {
-									image {
-										alt
-										height
-										width
-										url
-									}
-								}
-							}
-							title
-						}
-					}
-				}
-			}
-		`,
-		{
-			id: id,
-			status: status,
-		},
-	);
-
-	return data?.contentRevisions?.results[0];
-}
-
-async function getPageMenu(page) {
-	const name = page?.primaryNavigation?.name?.toUpperCase()?.replaceAll('-', '_');
-
-	if (!name) {
-		return null;
-	}
-
-	const { data } = await graphql(
-		`
-			fragment MenuItemContent on MenuItem {
-				url
-				title
-			}
-
-			query GetMenu($menu: MenuAvailable!) {
-				menu(name: $menu) {
-					items {
-						...MenuItemContent
-						children {
-							...MenuItemContent
-							children {
-								...MenuItemContent
-								children {
-									...MenuItemContent
-									children {
-										...MenuItemContent
-										children {
-											...MenuItemContent
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		`,
-		{
-			menu: name,
-		},
-	);
-
-	const menu = data?.menu?.items ?? null;
-
-	return data?.menu?.items ?? null;
-}
-
 export async function getStaticProps(context) {
 	const isPreview = context?.preview;
 	const status = isPreview ? null : true;
