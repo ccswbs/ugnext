3064b02f3759aa517e26ca10d9a1f9f81cf40172
Tanya Qureshi <tqureshi@uoguelph.ca>
2024-06-21 15:20:51 -0400

    Add ability to accept passed class

--- components/list.js
+++ components/list.js
@@ -24,20 +24,21 @@ export const List = ({ variant = 'unordered', children, className, ...rest }) =>
 	);
 };
 
-export const ListItem = ({ children }) => {
-	const { variant } = useContext(ListContext);
+export const ListItem = ({ className, children }) => {
+  const { variant } = useContext(ListContext);
 
-	return (
-		<li
-			className={twJoin(
-				'h-fit w-full has-[ol]:before:content-none has-[ul]:before:content-none',
-				variant !== 'ordered' &&
-					'relative pl-8 before:absolute before:left-[.5rem] before:top-[0.35rem] before:block before:h-[1.8rem] before:w-[1.1rem] before:shrink-0 before:items-center before:justify-center before:text-yellow before:content-[url("/icons/chevron-right.svg")]',
-				variant === 'ordered' &&
-					'relative pl-8 before:absolute before:left-0 before:inline-flex before:h-6 before:w-6 before:shrink-0 before:items-center before:justify-center before:bg-yellow before:font-black before:text-black before:content-[counter(list-number)] before:[counter-increment:list-number]',
-			)}
-		>
-			{children}
-		</li>
-	);
+  return (
+   <li
+    className={twMerge(
+     'h-fit w-full has-[ol]:before:content-none has-[ul]:before:content-none',
+     variant !== 'ordered' &&
+      'relative pl-8 before:absolute before:left-[.5rem] before:top-[0.35rem] before:block before:h-[1.8rem] before:w-[1.1rem] before:shrink-0 before:items-center before:justify-center before:text-yellow before:content-[url("/icons/chevron-right.svg")]',
+     variant === 'ordered' &&
+      'relative pl-8 before:absolute before:left-0 before:inline-flex before:h-6 before:w-6 before:shrink-0 before:items-center before:justify-center before:bg-yellow before:font-black before:text-black before:content-[counter(list-number)] before:[counter-increment:list-number]',
+     className
+    )}
+   >
+    {children}
+   </li>
+  );
 };
