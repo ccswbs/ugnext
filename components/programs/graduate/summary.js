320bcc1891d0cb0010f930f0c4e9fa91dbd8541f
Tanya Qureshi <tqureshi@uoguelph.ca>
2024-06-21 15:21:17 -0400

    Latest formatting changes

--- components/programs/graduate/summary.js
+++ components/programs/graduate/summary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Heading } from '@/components/heading';
+import { List, ListItem } from '@/components/list';
 
 export const GraduateProgramSummary = ({ data }) => {
   if (!data) {
@@ -9,13 +10,13 @@ export const GraduateProgramSummary = ({ data }) => {
   return (<>
     {/* <Heading level={2}>Program Summary</Heading> */}
     <div className="mx-auto p-5 bg-gray-900">      
-      <dl>
+      <dl className="!leading-loose">
+        <dt className="text-yellow-400 font-bold">Program type</dt>
+        <dd className="text-white">{data.program_type}</dd>
         <dt className="text-yellow-400 font-bold">Degree</dt>
         <dd className="text-white leading-relaxed">{data.degree_type} - {data.degree_name}</dd>
-
         <dt className="text-yellow-400 font-bold">Delivery</dt>
-        <dd className="text-white">{data.delivery_method}</dd>
-        
+        <dd className="text-white">{data.delivery_method}</dd>        
         <dt className="text-yellow-400 font-bold">Duration</dt>
         <dd className="text-white">
           {data.program_length}
@@ -25,31 +26,23 @@ export const GraduateProgramSummary = ({ data }) => {
               part-time is available
             </>
           )}
-        </dd>
-        
+        </dd>        
         <dt className="text-yellow-400 font-bold">Admission Average</dt>
         <dd className="text-white">{data.admission_average}</dd>
-
-        <dt className="text-yellow-400 font-bold">Program type</dt>
-        <dd className="text-white">{data.program_type}</dd>
       </dl>
-      <Heading level={3} className="text-yellow-400">Intake Terms</Heading>
-      <p className="text-white">Domestic: Summer, Fall, Winter</p>
-      <p className="text-white">International: Summer, Fall, Winter</p>
-      <Heading level={3} className="text-yellow-400">Intake Deadline</Heading>
+      <Heading level={3} className="text-xl text-yellow-400 mt-4 mb-2">Intake Terms &amp; Deadlines</Heading>
       {data.intake_term_domestic && <>
-      <Heading level={4} className="text-yellow-400">Domestic</Heading>
-      <ul>
-        {data.intake_term_domestic.map(term => (
-        <li key={term.name} className="text-white">
-          {term.deadline} ({term.name})
-        </li>
-        ))}
-      </ul>
-      </>}
-      
+      <Heading level={4} className="font-normal text-lg text-gray-400 mt-4 mb-2">Domestic</Heading>
+        <List>
+          {data.intake_term_domestic.map(term => (
+            <ListItem key={term.name} className="text-white">
+              {term.deadline} ({term.name})
+            </ListItem>
+          ))}
+        </List>
+      </>}      
       {data.intake_term_international && <>
-      <Heading level={4} className="text-yellow-400">International</Heading>
+      <Heading level={4} className="font-normal text-lg text-gray-400 mt-4 mb-2">International</Heading>
       <ul>
         {data.intake_term_international.map(term => (
         <li key={term.name} className="text-white">
