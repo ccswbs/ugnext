992189ccd233805077040918e475071aef9ba1bf
Raza Mahmood <razammahmood@gmail.com>
2024-06-27 00:03:59 -0400

    rendering links widget

--- components/widgets/widget-selector.js
+++ components/widgets/widget-selector.js
@@ -1,5 +1,6 @@
 import { GeneralText } from '@/components/widgets/general-text';
 import { Accordions } from '@/components/widgets/accordions';
+import { Links } from '@/components/widgets/links';
 
 export const WidgetSelector = ({ data }) => {
 	switch (data.__typename) {
@@ -7,6 +8,8 @@ export const WidgetSelector = ({ data }) => {
 			return <GeneralText data={data} />;
 		case 'ParagraphAccordionSection':
 			return <Accordions data={data} />;
+		case 'ParagraphLinksWidget':
+			return <Links data={data} />;
 		default:
 			return <span>{data.__typename} is not yet implemented</span>;
 	}
