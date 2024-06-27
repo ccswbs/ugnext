fc39c7769f4f3975bc5f9bbbbf3c4744198136e4
Raza Mahmood <razammahmood@gmail.com>
2024-06-27 14:18:05 -0400

    add tabs widget

--- components/widgets/widget-selector.js
+++ components/widgets/widget-selector.js
@@ -1,6 +1,7 @@
 import { GeneralText } from '@/components/widgets/general-text';
 import { Accordions } from '@/components/widgets/accordions';
 import { Links } from '@/components/widgets/links';
+import { Tabs } from '@/components/widgets/tabs';
 
 export const WidgetSelector = ({ data }) => {
 	switch (data.__typename) {
@@ -10,6 +11,8 @@ export const WidgetSelector = ({ data }) => {
 			return <Accordions data={data} />;
 		case 'ParagraphLinksWidget':
 			return <Links data={data} />;
+		case 'ParagraphSectionTab':
+			return <Tabs data={data} />;
 		default:
 			return <span>{data.__typename} is not yet implemented</span>;
 	}
