83de8665e39453f40e754dcefad0f9fdef4f1f03
Tanya Qureshi <tqureshi@uoguelph.ca>
2024-06-28 16:07:09 -0400

    Add fontAwesome import script

--- pages/_document.js
+++ pages/_document.js
@@ -2,37 +2,39 @@ import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 export default function Document() {
-	return (
-		<Html lang="en">
-			<Head>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap"
-					rel="stylesheet"
-				/>
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
 
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
 
-				<link
-					href="https://cdn.jsdelivr.net/npm/@uoguelph/web-components@1.x.x/dist/uofg-web-components/uofg-web-components.css"
-					rel="stylesheet"
-				/>
+        <link
+          href="https://cdn.jsdelivr.net/npm/@uoguelph/web-components@1.x.x/dist/uofg-web-components/uofg-web-components.css"
+          rel="stylesheet"
+        />
 
-				<Script
-					src="https://cdn.jsdelivr.net/npm/@uoguelph/web-components@1.2.3-rc.0/dist/uofg-web-components/uofg-header.esm.js"
-					type="module"
-					strategy="beforeInteractive"
-				/>
+        <Script
+          src="https://cdn.jsdelivr.net/npm/@uoguelph/web-components@1.2.3-rc.0/dist/uofg-web-components/uofg-header.esm.js"
+          type="module"
+          strategy="beforeInteractive"
+        />
 
-				<Script
-					src="https://cdn.jsdelivr.net/npm/@uoguelph/web-components@1.2.3-rc.0/dist/uofg-web-components/uofg-footer.esm.js"
-					type="module"
-					strategy="beforeInteractive"
-				/>
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+        <Script
+          src="https://cdn.jsdelivr.net/npm/@uoguelph/web-components@1.2.3-rc.0/dist/uofg-web-components/uofg-footer.esm.js"
+          type="module"
+          strategy="beforeInteractive"
+        />
+
+        <Script src="https://kit.fontawesome.com/7993323d0c.js" crossorigin="anonymous" strategy="lazyOnload" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
