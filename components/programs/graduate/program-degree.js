bf3f940f35409e81b2b9e14d4f195b3a650d08ec
An Duong <aduong01@uoguelph.ca>
2024-07-09 11:23:47 -0400

    add testimonial session, and add test breadcrumb to hold place for bradcrumb code to be added later

--- components/programs/graduate/program-degree.js
+++ components/programs/graduate/program-degree.js
@@ -7,20 +7,29 @@ import { GraduateProgramHero } from '@/components/programs/graduate/hero'
 import { GraduateProgramSummary } from '@/components/programs/graduate/summary';
 import { GraduateProgramInfo } from '@/components/programs/graduate/information';
 import { CollaborativeSpecializations } from '@/components/programs/graduate/sessions/collaborative-specializations';
+import { Testimonial } from '@/components/programs/graduate/sessions/testimonial';
 import { FundYourEducation } from '@/components/programs/graduate/sessions/fund-your-education';
 
 
 export const GraduateProgramDegreePage = ({ data, isFallback }) => (
-		<Layout title="Graduate Programs">
-			<GraduateProgramHero />			
+		<Layout title="Graduate Programs">			
+			<GraduateProgramHero />					
 			<Container className={twJoin(isFallback && 'hidden', 'md:px-20 max-w-1680 mx-auto')} centered>				
+				{/* add test breadcrumbs, will replace breadcrumbs code here once merge with basic page branch*/}
+				<div>
+					<p>
+						Academics &gt; Graduate &gt; Biostatistics &gt; Master's Degree(Thesis-Based)
+					</p>
+				</div>
 				<Heading level={1}> {data?.program_parent} | {data?.degree_name} - {toTitleCase(data?.program_type)} ({data?.degree_type})</Heading>
 				<div className='mx-auto mt-4 md:flex gap-6'>
 					<GraduateProgramInfo data={data} />					
 					<GraduateProgramSummary data={data} />					
 				</div>
+				<Testimonial data={data}/>
 				<CollaborativeSpecializations data={data}/>
 				<FundYourEducation />
+				
 			</Container>
 		</Layout>
 );
---
+++ components/programs/graduate/sessions/testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Heading } from '@/components/heading';
+import { Columns } from '@/components/columns';
+
+export const Testimonial = ({data}) => {	
+	return (
+		<div className="ug-testimonial text-center" >
+			<Heading className='' level={3}>Meet the Biostatistics Faculty</Heading>
+		</div>
+
+	)
+};
