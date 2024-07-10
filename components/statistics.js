5aa21212dba82d9c68feb3ac1eb34616112c6ad1
Raza Mahmood <razammahmood@gmail.com>
2024-07-10 12:34:21 -0400

    Add bg extension to gradient statistics using ResizeObserver

--- components/statistics.js
+++ components/statistics.js
@@ -1,4 +1,5 @@
 import { twJoin } from 'tailwind-merge';
+import useResizeObserver from 'use-resize-observer';
 
 export const Statistics = ({ data, variant = 'gradient-of-solid-colours' }) => {
 	const divisibleByTwo = data.length % 2 === 0;
@@ -28,6 +29,29 @@ export const Statistics = ({ data, variant = 'gradient-of-solid-colours' }) => {
 		),
 	];
 
+	const gradientClasses = twJoin(
+		'sm:before:content-[""]',
+		'before:absolute',
+		'before:top-0',
+		'before:right-full',
+		'before:w-[calc((100vw-var(--statistic-bg-width))/2)]',
+		'before:h-full',
+		'before:bg-inherit',
+		'before:z-[-1]',
+		'first:before:z-0',
+		'sm:after:content-[""]',
+		'after:absolute',
+		'after:top-0',
+		'after:left-full',
+		'after:w-[calc((100vw-var(--statistic-bg-width))/2)]',
+		'after:h-full',
+		'after:bg-inherit',
+		'after:z-[-1]',
+		'last:after:z-0',
+	);
+
+	const { ref, width } = useResizeObserver();
+
 	return (
 		<dl
 			className={twJoin(
@@ -37,13 +61,22 @@ export const Statistics = ({ data, variant = 'gradient-of-solid-colours' }) => {
 				divisibleByTwo && !divisibleByFour && 'grid grid-cols-1 sm:grid-cols-2',
 				variant !== 'gradient-of-solid-colours' && 'gap-4',
 			)}
+			style={variant === 'gradient-of-solid-colours' ? { '--statistic-bg-width': width + 'px' } : undefined}
+			ref={variant === 'gradient-of-solid-colours' ? ref : undefined}
 		>
 			{data.map((statistic, index) => {
 				const color = colors[index % colors.length];
 				const { represents, value } = statistic;
 
 				return (
-					<div key={index} className={twJoin('relative flex flex-1 flex-col justify-around gap-2 p-6 ', color)}>
+					<div
+						key={index}
+						className={twJoin(
+							'relative flex flex-1 flex-col justify-around gap-2 p-6',
+							variant === 'gradient-of-solid-colours' && gradientClasses,
+							color,
+						)}
+					>
 						<dt className="hyphens-auto break-words text-center text-xl font-bold leading-tight md:text-3xl">
 							{value}
 						</dt>
