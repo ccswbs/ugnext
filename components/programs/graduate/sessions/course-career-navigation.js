375e35eb835f5b96bc930915d2058cebcb0dce32
An Duong <aduong01@uoguelph.ca>
2024-07-12 10:57:33 -0400

    rename navigation-tab file name

--- components/programs/graduate/sessions/course-career-navigation.js
+++ components/programs/graduate/sessions/course-career-navigation.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { NavigationTab } from '@/components/programs/graduate/sessions/tabs/navigation-grad-program';
+import { NavigationTab } from '@/components/programs/graduate/sessions/tabs/navigation-tab';
 
 export const CourseCareerNavigation = ({ activeTab,setActiveTab }) => {
   
