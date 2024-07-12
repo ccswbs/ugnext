75ffa5f5f3acc685e9dee92a795a31735d22b392
Raza Mahmood <razammahmood@gmail.com>
2024-07-12 13:34:53 -0400

    Take section component from basic-pages

---
+++ components/section.js
@@ -0,0 +1,15 @@
+import { createContext } from 'react';
+import { twJoin } from 'tailwind-merge';
+
+export const SectionContext = createContext(null);
+
+export const Section = ({ primary, secondary, equal = false }) => (
+	<div className={twJoin('grid w-full grid-cols-1', equal ? 'md:grid-cols-[50%_50%]' : 'md:grid-cols-[75%_25%]')}>
+		<SectionContext.Provider value={{ column: 'primary', equal: equal }}>
+			<div>{primary}</div>
+		</SectionContext.Provider>
+		<SectionContext.Provider value={{ column: 'secondary', equal: equal }}>
+			<div>{secondary}</div>
+		</SectionContext.Provider>
+	</div>
+);
