203f7fa93174535d5c2e381b1559c90c95dce6a9
Raza Mahmood <razammahmood@gmail.com>
2024-07-12 14:46:21 -0400

    Refactor admission requirements directory structure

--- pages/admission/undergraduate/requirements/index.js
+++ pages/admission/undergraduate/requirements/index.js
@@ -1,6 +1,6 @@
 import { Layout } from '@/components/layout';
 import { Container } from '@/components/container';
-import { getLocations, getStudentTypes } from '@/data/yaml/admission-requirements';
+import { getLocations, getStudentTypes } from '@/data/yaml/admission/undergraduate/requirements';
 import { Select } from '@/components/select';
 import { Heading } from '@/components/heading';
 import { useState } from 'react';
