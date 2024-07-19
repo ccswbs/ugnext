14fedc45037044e9c650d891ff7249e4cb962640
Raza Mahmood <razammahmood@gmail.com>
2024-07-19 16:57:04 -0400

    Refactor admissions requirements form and routing logic

--- pages/admission/undergraduate/requirements/index.js
+++ pages/admission/undergraduate/requirements/index.js
@@ -3,12 +3,13 @@ import { Container } from '@/components/container';
 import { getLocations, getPrograms, getStudentTypes } from '@/data/sqlite/admission/undergraduate/requirements';
 import { Select } from '@/components/select';
 import { Heading } from '@/components/heading';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Button } from '@/components/button';
 import { Section } from '@/components/section';
 import { List, ListItem } from '@/components/list';
 import { Link } from '@/components/link';
 import { useRouter } from 'next/router';
+import { slugify } from '@/lib/string-utils';
 
 export async function getStaticProps(context) {
 	return {
@@ -24,14 +25,42 @@ export default function UndergraduateAdmissionRequirementsHome({ locations, stud
 	const [studentType, setStudentType] = useState(null);
 	const [location, setLocation] = useState(null);
 	const [program, setProgram] = useState(null);
+
 	const [showInternational, setShowInternational] = useState(false);
 	const [showCurriculums, setShowCurriculums] = useState(false);
-	const complete = (studentType && location && program) || (studentType === 'internal' && program);
+
+	const isLocationRequirementMet = (studentType?.location_dependent && location) || !studentType?.location_dependent;
+	const isProgramRequirementMet = (studentType?.program_dependent && program) || !studentType?.program_dependent;
+	const isComplete = studentType && isLocationRequirementMet && isProgramRequirementMet;
+
+	const locationDependentStudentTypes = useMemo(
+		() => new Set(studentTypes.filter((type) => type.location_dependent).map((type) => type.id)),
+		[studentTypes],
+	);
+
+	const programDependentStudentTypes = useMemo(
+		() => new Set(studentTypes.filter((type) => type.program_dependent).map((type) => type.id)),
+		[studentTypes],
+	);
+
 	const router = useRouter();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(studentType, location, program);
+
+		if (!isComplete) return;
+
+		let slug = `${studentType.id}`;
+
+		if (studentType.program_dependent) {
+			slug += `/${slugify(program.title)}`;
+		}
+
+		if (studentType.location_dependent) {
+			slug += `/${slugify(location.name)}`;
+		}
+
+		router.push(`/admission/undergraduate/requirements/${slug}`).catch((e) => console.error(e));
 	};
 
 	return (
@@ -51,13 +80,15 @@ export default function UndergraduateAdmissionRequirementsHome({ locations, stud
 									}
 									options={studentTypes.map((type) => ({
 										label: type.name,
-										value: type.id,
+										value: type,
+										key: type.id,
 									}))}
 									onChange={(selection) => {
 										setStudentType(selection.value);
 									}}
 								/>
-								{studentType !== 'internal' && (
+
+								{locationDependentStudentTypes.has(studentType?.id) && (
 									<>
 										<Select
 											label={
@@ -68,15 +99,18 @@ export default function UndergraduateAdmissionRequirementsHome({ locations, stud
 											options={[
 												...locations.domestic.map((location) => ({
 													label: location.name,
-													value: location.name,
+													value: location,
+													key: location.name,
 												})),
 												{
 													label: 'Outside of Canada',
 													value: 'international',
+													key: 'international',
 												},
 												{
 													label: 'Other Curriculum',
 													value: 'curriculum',
+													key: 'curriculum',
 												},
 											]}
 											onChange={(selection) => {
@@ -108,7 +142,8 @@ export default function UndergraduateAdmissionRequirementsHome({ locations, stud
 												}
 												options={locations.international.map((location) => ({
 													label: location.name,
-													value: location.name,
+													value: location,
+													key: location.name,
 												}))}
 												onChange={(selection) => {
 													setLocation(selection.value);
@@ -125,7 +160,8 @@ export default function UndergraduateAdmissionRequirementsHome({ locations, stud
 												}
 												options={locations.curriculums.map((location) => ({
 													label: location.name,
-													value: location.name,
+													value: location,
+													key: location.name,
 												}))}
 												onChange={(selection) => {
 													setLocation(selection.value);
@@ -135,22 +171,30 @@ export default function UndergraduateAdmissionRequirementsHome({ locations, stud
 									</>
 								)}
 
-								<Select
-									label={
-										<Heading level={5} as="h2" className="mb-1 mt-0">
-											I am interested in studying
-										</Heading>
-									}
-									options={programs?.map((program) => ({
-										label: program.title,
-										value: program.title,
-										key: program.title,
-									}))}
-									onChange={(selection) => {
-										setProgram(selection.value);
-									}}
-								/>
-								<Button className="w-full sm:w-fit" color="red" type="submit" disabled={!complete} outlined={!complete}>
+								{programDependentStudentTypes.has(studentType?.id) && (
+									<Select
+										label={
+											<Heading level={5} as="h2" className="mb-1 mt-0">
+												I am interested in studying
+											</Heading>
+										}
+										options={programs?.map((program) => ({
+											label: program.title,
+											value: program,
+											key: program.title,
+										}))}
+										onChange={(selection) => {
+											setProgram(selection.value);
+										}}
+									/>
+								)}
+								<Button
+									className="w-full sm:w-fit"
+									color="red"
+									type="submit"
+									disabled={!isComplete}
+									outlined={!isComplete}
+								>
 									View Requirements
 								</Button>
 							</form>
