324cf4a7cb5b239295aacb07af78149e8434d82a
Raza Mahmood <razammahmood@gmail.com>
2024-07-19 16:55:46 -0400

    Update slug.js for better error handling

--- pages/admission/undergraduate/requirements/[...slug].js
+++ pages/admission/undergraduate/requirements/[...slug].js
@@ -1,9 +1,14 @@
 export async function getStaticPaths() {
-
+	return {
+		paths: [],
+		fallback: true,
+	}
 }
 
 export async function getStaticProps(context) {
-
+	return {
+		notFound: true,
+	}
 }
 
 export default function UndergraduateAdmissionRequirements(props) {
