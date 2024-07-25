0c6d10c5460febda794c0dc6b5d61d7e7d7c87f4
Raza Mahmood <razammahmood@gmail.com>
2024-07-25 16:02:54 -0400

    Validate slugs in undergraduate requirements page.

--- pages/admission/undergraduate/requirements/[...slug].js
+++ pages/admission/undergraduate/requirements/[...slug].js
@@ -1,16 +1,24 @@
+import { validateRequirementsSlug } from '@/data/sqlite/admission/undergraduate/requirements';
+
 export async function getStaticPaths() {
 	return {
 		paths: [],
 		fallback: true,
-	}
+	};
 }
 
 export async function getStaticProps(context) {
-	return {
-		notFound: true,
+	const isValid = await validateRequirementsSlug(context.params.slug);
+
+	if (!isValid) {
+		return {
+			notFound: true,
+		};
 	}
+
+	return {
+		props: {},
+	};
 }
 
-export default function UndergraduateAdmissionRequirements(props) {
-
-}
+export default function UndergraduateAdmissionRequirements(props) {}
