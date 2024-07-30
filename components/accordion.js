f87149c4905898a745384bbc0e9f8ce69bb68780
Raza Mahmood <razammahmood@gmail.com>
2024-07-30 15:26:48 -0400

    Add PropTypes to Accordion and adjust styling.

--- components/accordion.js
+++ components/accordion.js
@@ -3,14 +3,18 @@ import AnimateHeight from 'react-animate-height';
 import { twJoin } from 'tailwind-merge';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleMinus, faCirclePlus } from '@awesome.me/kit-7993323d0c/icons/classic/regular';
+import PropTypes from 'prop-types';
 
+/**
+ * An accordion component allowing users to expand and collapse sections of content.
+ */
 export const Accordion = ({ title, children }) => (
 	<Disclosure>
 		{({ open }) => (
 			<div className="my-2">
 				<DisclosureButton
 					className={twJoin(
-						'mb-1 inline-flex w-full cursor-pointer items-center justify-between p-2.5 px-5 text-lg transition-colors hover:bg-blue hover:text-white focus:bg-blue focus:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-blue focus-visible:ring-offset-2',
+						'mb-1 inline-flex text-left gap-2 w-full cursor-pointer items-center justify-between p-2.5 px-5 text-xl transition-colors hover:bg-blue hover:text-white focus:bg-blue focus:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-blue focus-visible:ring-offset-2',
 						open ? 'bg-blue text-white' : 'bg-grey-50 text-blue',
 					)}
 				>
@@ -21,7 +25,7 @@ export const Accordion = ({ title, children }) => (
 					/>
 				</DisclosureButton>
 				<AnimateHeight height={open ? 'auto' : 0} duration={200} easing={'ease-in-out'}>
-					<DisclosurePanel static className="border-l-4 border-yellow py-2 pl-3">
+					<DisclosurePanel static className="border-l-4 border-yellow py-3 pl-5">
 						{children}
 					</DisclosurePanel>
 				</AnimateHeight>
@@ -29,3 +33,8 @@ export const Accordion = ({ title, children }) => (
 		)}
 	</Disclosure>
 );
+
+Accordion.propTypes = {
+	title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+	children: PropTypes.node,
+};
