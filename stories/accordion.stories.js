2d1a4667b49d689afec5fbbaf08442622174f5f6
Raza Mahmood <razammahmood@gmail.com>
2024-07-30 15:35:49 -0400

    Add accordion stories to Storybook

---
+++ stories/accordion.stories.js
@@ -0,0 +1,31 @@
+import { Accordion } from '@/components/accordion';
+
+const config = {
+	title: 'Components/Accordion',
+	component: Accordion,
+	parameters: {
+		layout: 'padded',
+	},
+	tags: ['autodocs'],
+	argTypes: {
+		children: { control: false },
+	},
+};
+
+export default config;
+
+export const Default = {
+	args: {
+		title: 'Example Title',
+		children: (
+			<div>
+				Iusto possimus possimus delectus et. Et aspernatur culpa quis sint at nam voluptatibus. Occaecati perspiciatis
+				ea eius dolorem aliquid. Ad ducimus aut aspernatur. Cumque enim repellat reprehenderit. Similique perspiciatis
+				alias doloremque reprehenderit eum. A laboriosam dolore. Facere possimus qui. Voluptas quas dicta hic adipisci
+				voluptatibus impedit consectetur quae veniam. Totam amet magni. Vero voluptas dolorum itaque praesentium sint
+				quasi accusamus. Culpa consequuntur doloribus sint. Pariatur sequi consequuntur quidem enim exercitationem.
+				Culpa repellendus eveniet fugit cum. Sapiente doloribus recusandae ex autem.
+			</div>
+		),
+	},
+};
