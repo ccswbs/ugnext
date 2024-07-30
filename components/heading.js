006ca256a97902bbd57fbbb9b98c96349207207d
Raza Mahmood <razammahmood@gmail.com>
2024-07-30 15:27:35 -0400

    Add 'as' prop to Heading component and update font sizes

--- components/heading.js
+++ components/heading.js
@@ -1,20 +1,20 @@
 import { clamp } from '@/lib/math-utils';
 import { twJoin, twMerge } from 'tailwind-merge';
 
-export const Heading = ({ level, children, className, ...rest }) => {
+export const Heading = ({ level, children, className, as, ...rest }) => {
 	const headingLevel = clamp(level || 1, 1, 6);
-	const Tag = `h${headingLevel}`;
+	const Tag = as ?? `h${headingLevel}`;
 
 	return (
 		<Tag
 			{...rest}
 			className={twMerge(
 				twJoin(
-					'mb-7 mt-7 font-condensed font-bold leading-tight',
-					headingLevel === 1 && 'text-5xl text-red',
-					headingLevel === 2 && 'text-4xl',
-					headingLevel === 3 && 'text-3xl',
-					headingLevel === 4 && 'text-2xl',
+					'mb-7 mt-7 font-bold leading-tight',
+					headingLevel === 1 && 'text-4xl text-red',
+					headingLevel === 2 && 'text-3xl text-red',
+					headingLevel === 3 && 'text-2xl',
+					headingLevel === 4 && 'text-xl',
 					(headingLevel === 5 || headingLevel === 6) && 'text-xl',
 				),
 				className,
