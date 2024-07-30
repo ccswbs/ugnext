77d9b7604fd625249f49f26d02e6b0a9bbc087e2
Raza Mahmood <razammahmood@gmail.com>
2024-07-30 15:36:01 -0400

    Add storybook stories for Author component

---
+++ stories/author.stories.js
@@ -0,0 +1,28 @@
+import { Author } from '@/components/author';
+
+const config = {
+	title: 'Components/Author',
+	component: Author,
+	parameters: {
+		layout: 'padded',
+	},
+	tags: ['autodocs'],
+};
+
+export default config;
+
+export const Red = {
+	args: {
+		name: 'Example Name',
+		info: 'Enter a quote or other information about the author here',
+		color: 'red',
+	},
+};
+
+export const Yellow = {
+	args: {
+		name: 'Example Name',
+		info: 'Enter a quote or other information about the author here',
+		color: 'yellow',
+	},
+};
