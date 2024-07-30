f26c08cf39df23042340d117ca22a54b0989ee86
Raza Mahmood <razammahmood@gmail.com>
2024-07-30 15:28:26 -0400

    Refactor story component to use content and footer props

--- components/home/story.js
+++ components/home/story.js
@@ -2,6 +2,9 @@ import Image from 'next/image';
 import foreground from '@/img/meal-care-students.png';
 import background from '@/img/change-happens-banner.jpg';
 import { Story } from '@/components/story';
+import { Button } from '@/components/button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft, faQuoteRight } from '@awesome.me/kit-7993323d0c/icons/sharp/solid';
 
 export const HomeStory = () => (
 	<>
@@ -17,9 +20,26 @@ export const HomeStory = () => (
 					sizes="100vw"
 				/>
 			}
-			quote="Change happens at the community level. Small groups put in small efforts and those efforts add up to something big."
-			ctaText="Learn more about how the University of Guelph is working to"
-			ctaButton="Improve Life"
+			content={
+				<blockquote className="block w-full text-center text-3xl font-thin italic text-white sm:text-4xl lg:text-3xl xl:text-4xl">
+					<FontAwesomeIcon icon={faQuoteLeft} className="mr-[0.2em] inline-block h-[1em] text-yellow" />
+					<span>
+						Change happens at the community level. Small groups put in small efforts and those efforts add up to
+						something big.
+					</span>
+					<FontAwesomeIcon icon={faQuoteRight} className="ml-[0.2em] inline-block h-[1em] text-yellow" />
+				</blockquote>
+			}
+			footer={
+				<>
+					<span className="mr-1 font-bold leading-tight">
+						Learn more about how the University of Guelph is working to
+					</span>
+					<Button color="red" className="p-2">
+						Improve Life
+					</Button>
+				</>
+			}
 		/>
 
 		<div className="w-full p-2"></div>
--- components/story.js
+++ components/story.js
@@ -1,29 +1,16 @@
-import { Button } from '@/components/button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuoteLeft, faQuoteRight } from '@awesome.me/kit-7993323d0c/icons/sharp/solid';
-
-export const Story = ({ backgroundImage, foregroundImage, quote, ctaText, ctaButton }) => (
+export const Story = ({ backgroundImage, foregroundImage, content, footer }) => (
 	<div className="flex w-full flex-col">
 		<div className="relative flex w-full items-center justify-center overflow-hidden">
 			<div className="absolute z-0 h-full max-h-full w-full">{backgroundImage}</div>
 
-			<div className="container max-w-max-content z-10 flex w-full flex-col items-center gap-6 px-6 pt-6 lg:flex-row">
-				<blockquote className="my-auto block h-fit w-5/6 text-center text-3xl font-thin italic text-white sm:text-4xl lg:w-1/2 lg:text-3xl xl:text-4xl">
-					<FontAwesomeIcon icon={faQuoteLeft} className="mr-[0.2em] inline-block h-[1em] text-yellow" />
-					{quote}
-					<FontAwesomeIcon icon={faQuoteRight} className="ml-[0.2em] inline-block h-[1em] text-yellow" />
-				</blockquote>
-				<div className="w-full lg:w-1/2">{foregroundImage}</div>
+			<div className="container z-10 flex w-full max-w-max-content flex-col items-center gap-6 px-6 pt-6 lg:flex-row">
+				<div className="flex w-full justify-center lg:w-1/2">{content}</div>
+				<div className="flex w-full justify-center lg:w-1/2 lg:mt-auto">{foregroundImage}</div>
 			</div>
 		</div>
 
 		<div className="flex w-full items-center justify-center bg-black p-4 text-white">
-			<div className="text-xl">
-				<span className="mr-1 font-bold leading-tight">{ctaText}</span>
-				<Button color="red" className="p-2">
-					{ctaButton}
-				</Button>
-			</div>
+			<div className="text-xl">{footer}</div>
 		</div>
 	</div>
 );
