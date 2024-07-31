3ac07c7f534ff54e9001c9e7336c97c15e2e00aa
Raza Mahmood <razammahmood@gmail.com>
2024-07-30 23:23:53 -0400

    Add JSDoc comment in Divider and create story for it

--- components/divider.js
+++ components/divider.js
@@ -1,1 +1,4 @@
+/**
+ * Separates content on a page visually
+ */
 export const Divider = () => <hr className="mx-auto my-7 h-1 w-3/4 border-0 bg-divider" />;
---
+++ stories/divider.stories.js
@@ -0,0 +1,18 @@
+import { Divider } from '@/components/divider';
+
+const config = {
+	title: 'Components/Divider',
+	component: Divider,
+	parameters: {
+		layout: 'padded',
+		docs: {
+			toc: true,
+		},
+	},
+	tags: ['autodocs'],
+};
+
+export default config;
+
+export const Default = {
+};
