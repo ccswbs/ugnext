9c8d7ea687ded51e6ba4b037078cec7e4157b118
Raza Mahmood <razammahmood@gmail.com>
2024-07-31 14:04:01 -0400

    Add documentation source display for WithCustomInstructions

--- stories/html-parser.stories.js
+++ stories/html-parser.stories.js
@@ -20,18 +20,36 @@ export default config;
 
 export const Default = {
 	args: {
-		html: "<strong>Lorem Ipsum</strong>",
+		html: '<strong>Lorem Ipsum</strong>',
 	},
 };
 
 export const WithCustomInstructions = {
 	args: {
-		html: "<strong>Lorem Ipsum</strong>",
+		html: '<strong>Lorem Ipsum</strong>',
 		instructions: [
 			{
 				shouldProcessNode: (node) => node.tagName === 'strong',
 				processNode: (node, children) => <span className="font-black text-red">{children}</span>,
-			}
-		]
+			},
+		],
+	},
+	parameters: {
+		docs: {
+			source: {
+				code:
+					'<HtmlParser\n' +
+					'  html="<strong>Lorem Ipsum</strong>"\n' +
+					'  instructions={[\n' +
+					'    {\n' +
+					'      shouldProcessNode: (node) => node.tagName === "strong",\n' +
+					'      processNode: (node, children) => (\n' +
+					'        <span className="font-black text-red">{children}</span>\n' +
+					'      )\n' +
+					'    }\n' +
+					'  ]}\n' +
+					'/>',
+			},
+		},
 	},
 };
