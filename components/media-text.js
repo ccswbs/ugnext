6ad0c0a0d7f5f39b152110a73dc43e607b8ef4f3
Tanya Qureshi <tqureshi@uoguelph.ca>
2024-08-01 15:55:05 -0400

    Use Button Section

--- components/media-text.js
+++ components/media-text.js
@@ -4,124 +4,86 @@ import Image from 'next/image';
 import { Heading } from '@/components/heading';
 import { Video } from '@/components/video';
 import { HtmlParser } from '@/components/html-parser';
-import { Button } from '@/components/button';
+import { ButtonSection } from '@/components/widgets/button-section';
 
 export const MediaText = ({ data }) => {
-	const region = data.sectionColumn.name;
-	const mediaTitle = data?.heading;
-	const mediaDescription = data?.description?.processed;
-	const mediaBgColor = data?.background?.name;
-	
-	const mediaSize = data?.mediaImageSize;
-	const imageURL = data?.media?.image?.url;
-	const imageAlt = data?.media?.image?.alt;
-	const imageWidth = data?.media?.image?.width;
-	const imageHeight = data?.media?.image?.height;
-	const mediaAlignment = data?.mediaAlignment;
-    
-    const videoTitle = data?.media?.name;
-    const videoTranscript = data?.media?.transcript;
-    const videoURL = data?.media?.url;
-    const videoHeight = data?.media?.height;
-    const videoWidth = data?.media?.width;
-    const videoType = (videoURL?.includes("youtube") || videoURL?.includes("youtu.be") ? `youtube` : `vimeo`);
-    const videoID = (videoType === `youtube` ? extractVideoID(videoURL) : videoURL?.substring(18));
+  const region = data.sectionColumn.name;
+  const mediaTitle = data?.heading;
+  const mediaDescription = data?.description?.processed;
+  const mediaBgColor = data?.background?.name;
 
-	const mediaButtons = data.buttonSection;
-	let textOrButtons = mediaDescription || mediaButtons ? true : false;
-    //const mediaRelationships = data.widgetData?.relationships.field_media_text_media?.relationships;
+  const mediaSize = data?.mediaImageSize;
+  const imageURL = data?.media?.image?.url;
+  const imageAlt = data?.media?.image?.alt;
+  const imageWidth = data?.media?.image?.width;
+  const imageHeight = data?.media?.image?.height;
+  const mediaAlignment = data?.mediaAlignment;
 
-	const computeLayoutData = {
-		region : region,
-		mediaDescription : mediaDescription,
-		mediaBgColor : mediaBgColor,
-		mediaSize : mediaSize,
-		imageURL : imageURL,
-		videoURL : videoURL,
-		mediaButtons : mediaButtons,
-		mediaAlignment : mediaAlignment
-	}
-	const {
-		textColBg,
-		headingClass,
-		mediaCol,
-		textCol,
-		textColPadding,
-		textColHeight,
-		wrapperCol,
-		leftDivClasses,
-		rightDivClasses,
-		headingColor
-	} = computeLayoutMediaText(computeLayoutData);
+  const videoTitle = data?.media?.name;
+  const videoTranscript = data?.media?.transcript;
+  const videoURL = data?.media?.url;
+  const videoHeight = data?.media?.height;
+  const videoWidth = data?.media?.width;
+  const videoType = videoURL?.includes('youtube') || videoURL?.includes('youtu.be') ? `youtube` : `vimeo`;
+  const videoID = videoType === `youtube` ? extractVideoID(videoURL) : videoURL?.substring(18);
 
-	const videoData = {
-					videoTitle : videoTitle,
-					videoTranscript : videoTranscript,
-					videoHeight : videoHeight,
-					videoWidth : videoWidth,
-					videoType : videoType,
-					videoID : videoID,
-					}
-	
-	return (
-		
-		<div className={twJoin(
-            'mx-auto',
-            'mt-4',
-            'md:flex',
-            textColBg,
-            headingColor,
-			headingClass
-        	)}
-			data-title="media"
-			>
+  const mediaButtons = data.buttonSection;
+  let textOrButtons = mediaDescription || mediaButtons ? true : false;
+  //const mediaRelationships = data.widgetData?.relationships.field_media_text_media?.relationships;
 
-			<div className={twJoin("",mediaCol, leftDivClasses)}>
-				{videoURL &&					
-					<Video
-						className={twJoin('w-full')}
-						videoData={videoData}		
-					/>
-				}
+  const computeLayoutData = {
+    region: region,
+    mediaDescription: mediaDescription,
+    mediaBgColor: mediaBgColor,
+    mediaSize: mediaSize,
+    imageURL: imageURL,
+    videoURL: videoURL,
+    mediaButtons: mediaButtons,
+    mediaAlignment: mediaAlignment,
+  };
+  const {
+    textColBg,
+    headingClass,
+    mediaCol,
+    textCol,
+    textColPadding,
+    textColHeight,
+    wrapperCol,
+    leftDivClasses,
+    rightDivClasses,
+    headingColor,
+  } = computeLayoutMediaText(computeLayoutData);
 
-				{imageURL &&
-					<Image 
-						className={twJoin('w-full')}
-						src={imageURL}
-						alt={imageAlt}
-						width={imageWidth}
-						height={imageHeight}
-					/>
-					
-				}
-			</div>
+  const videoData = {
+    videoTitle: videoTitle,
+    videoTranscript: videoTranscript,
+    videoHeight: videoHeight,
+    videoWidth: videoWidth,
+    videoType: videoType,
+    videoID: videoID,
+  };
 
-			{textOrButtons && 
-				<div className={twJoin(textCol, rightDivClasses, 'p-5')} >
-					{mediaTitle && 
-						<Heading 
-							level={2}
-							className={twJoin("font-bold text-3xl",headingColor,headingClass)}							
-						>
-							{mediaTitle}
-						</Heading>
-					}
-					{mediaDescription &&
-						<HtmlParser html={mediaDescription} />
-					}
-					{mediaButtons && mediaButtons.buttons.map((buttonData, index) => (						
-						<Button 
-							key={index}
-							href={buttonData?.link?.url}
-							children={buttonData?.link?.title}
-							className= {'w-1/3 py-4 text-xl'}
-							color={buttonStyle(buttonData?.style?.name)}
-						/>
-					))}
-				</div>
-			}
-		
-		</div>
-		
-	)
+  return (
+    <div className={twJoin('mx-auto', 'mt-4', 'md:flex', textColBg, headingColor, headingClass)} data-title="media">
+      <div className={twJoin('', mediaCol, leftDivClasses)}>
+        {videoURL && <Video className={twJoin('w-full')} videoData={videoData} />}
+
+        {imageURL && (
+          <Image className={twJoin('w-full')} src={imageURL} alt={imageAlt} width={imageWidth} height={imageHeight} />
+        )}
+      </div>
+
+      {textOrButtons && (
+        <div className={twJoin(textCol, rightDivClasses, 'p-5')}>
+          {mediaTitle && (
+            <Heading level={2} className={twJoin('text-3xl font-bold', headingColor, headingClass)}>
+              {mediaTitle}
+            </Heading>
+          )}
+          {mediaDescription && <HtmlParser html={mediaDescription} />}
+          {mediaButtons && <ButtonSection data={mediaButtons} />}
+        </div>
+      )}
+    </div>
+  );
 };
