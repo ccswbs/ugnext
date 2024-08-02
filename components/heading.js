178e73c4ca6609a039f1082f2058fb14a39c5c6d
Raza Mahmood <razammahmood@gmail.com>
2024-08-02 01:15:53 -0400

    Refactor heading margins for consistency and readability

--- components/heading.js
+++ components/heading.js
@@ -11,12 +11,12 @@ export const Heading = ({ level, children, className, as, ...rest }) => {
 			{...rest}
 			className={twMerge(
 				twJoin(
-					'mb-7 mt-7 font-bold leading-tight',
-					headingLevel === 1 && 'text-4xl text-red',
-					headingLevel === 2 && 'text-3xl text-red',
-					headingLevel === 3 && 'text-2xl',
-					headingLevel === 4 && 'text-xl',
-					(headingLevel === 5 || headingLevel === 6) && 'text-xl',
+					'font-bold leading-tight',
+					headingLevel === 1 && 'my-7 text-4xl text-red',
+					headingLevel === 2 && 'my-3 text-3xl text-red',
+					headingLevel === 3 && 'my-3 text-2xl',
+					headingLevel === 4 && 'my-3 text-xl',
+					(headingLevel === 5 || headingLevel === 6) && 'my-3 text-xl',
 				),
 				className,
 			)}
