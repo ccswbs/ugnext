d22f8a541312c665fc48caf09759b075d996d3dd
Raza Mahmood <razammahmood@gmail.com>
2024-08-13 23:25:20 -0400

    add yaml version of sqlite data

---
+++ data/yaml/admission/undergraduate/requirements/index.js
@@ -0,0 +1,60 @@
+import studentTypes from './student-types.yml';
+import locations from './locations.yml';
+import programs from '@/data/yaml/programs/undergraduate.yml';
+
+export const getStudentTypes = async () => {
+	return studentTypes;
+};
+
+export const getLocations = async () => {
+	return locations.reduce((acc, location) => {
+		acc[location.type] ??= [];
+		acc[location.type].push(location);
+
+		return acc;
+	}, {});
+};
+
+export const getPrograms = async () => {
+	return programs.filter((program) => program.types.includes('major'));
+};
+
+export const requirementToSlug = (studentType, program, location) => {
+	let slug = '';
+
+	console.log(studentType, program, location);
+
+	if (studentType) {
+		slug += `/${studentType}`;
+	}
+
+	if (program) {
+		slug += `/${program}`;
+	}
+
+	if (location) {
+		slug += `/${location}`;
+	}
+
+	return slug;
+};
+
+export const slugToRequirement = (slug) => {
+	return {
+		studentType: slug[0],
+		program: slug.length !== 1 ? slug[1] : null,
+		location: slug.length === 3 ? slug[2] : null,
+	};
+};
+
+export const isValidRequirement = async (studentType, program, location) => {
+	return true;
+};
+
+export const getRequirementTitle = async (studentType, program, location) => {
+	return '';
+};
+
+export const getRequirementContent = async (studentType, program, location) => {
+	return '';
+};
