e9fa7ae415d1902de65c0f1b0f080ce98dbf557c
Raza Mahmood <razammahmood@gmail.com>
2024-08-14 00:39:27 -0400

    Refactor requirement checks and improve title generation functions

--- data/yaml/admission/undergraduate/requirements/index.js
+++ data/yaml/admission/undergraduate/requirements/index.js
@@ -1,6 +1,8 @@
 import studentTypes from './student-types.yml';
 import locations from './locations.yml';
-import programs from '@/data/yaml/programs/undergraduate.yml';
+import allPrograms from '@/data/yaml/programs/undergraduate.yml';
+
+const programs = allPrograms.filter((program) => program.types.includes('major'));
 
 export const getStudentTypes = async () => {
 	return studentTypes;
@@ -16,7 +18,7 @@ export const getLocations = async () => {
 };
 
 export const getPrograms = async () => {
-	return programs.filter((program) => program.types.includes('major'));
+	return programs;
 };
 
 export const slugToRequirement = (slug) => {
@@ -27,11 +29,56 @@ export const slugToRequirement = (slug) => {
 	};
 };
 
-export const isValidRequirement = async (studentType, program, location) => {
+export const isValidRequirement = async (studentTypeID, programID, locationID) => {
+	const studentType = studentTypes.find(({ id }) => id === studentTypeID);
+
+	// Check if there exists a student type with the id (retrieved from the slug) in the list
+	if (!studentType) {
+		return false;
+	}
+
+	// if the student type is location independent, then the slug shouldn't contain a program id
+	if (!studentType.program_dependent && programID) {
+		return false;
+	}
+
+	// if the student type is location independent, then the slug shouldn't contain a location id
+	if (!studentType.location_dependent && locationID) {
+		return false;
+	}
+
+	if (studentType.program_dependent && !programs.some(({ id }) => id === programID)) {
+		return false;
+	}
+
+	if (studentType.location_dependent && !locations.some(({ id }) => id === locationID)) {
+		return false;
+	}
+
 	return true;
 };
 
-export const getRequirementTitle = async (studentType, program, location) => {
+export const getRequirementTitle = async (studentTypeID, programID, locationID) => {
+	const studentTypeName = studentTypes
+		.find(({ id, name }) => id === studentTypeID)
+		?.name?.replace('Student', 'Students')
+		?.replace('Graduate', 'Graduates');
+
+	if (!programID && !locationID) {
+		return `Undergraduate Admission Requirements for ${studentTypeName}`;
+	}
+
+	const programName = programs.find(({ id, name }) => id === programID)?.name;
+	const locationName = locations.find(({ id, name }) => id === locationID)?.name;
+
+	if (programName && !locationName) {
+		return `${programName} Admission Requirements for ${studentTypeName}`;
+	}
+
+	if (programName && locationName) {
+		return `${programName} Admission Requirements for ${studentTypeName} in ${locationName}`;
+	}
+
 	return '';
 };
 
