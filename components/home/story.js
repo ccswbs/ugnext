32fa8b9a4ea6fb765b1ad4e79b1842195d3a3781
An Duong <aduong01@uoguelph.ca>
2024-08-20 15:20:05 -0400

    fix merge conflict

--- components/home/story.js
+++ components/home/story.js
@@ -37,29 +37,6 @@ export const HomeStory = () => (
 					</Button>
 				</div>
 			}
-<<<<<<< HEAD
-=======
-			content={
-				<blockquote className="block w-full text-center text-3xl font-thin italic text-white sm:text-4xl lg:text-3xl xl:text-4xl">
-					<FontAwesomeIcon icon={faQuoteLeft} className="mr-[0.2em] inline-block h-[1em] text-yellow" />
-					<span>
-						Change happens at the community level. Small groups put in small efforts and those efforts add up to
-						something big.
-					</span>
-					<FontAwesomeIcon icon={faQuoteRight} className="ml-[0.2em] inline-block h-[1em] text-yellow" />
-				</blockquote>
-			}
-			footer={
-				<>
-					<span className="mr-1 font-bold leading-tight">
-						Learn more about how the University of Guelph is working to
-					</span>
-					<Button color="red" className="p-2">
-						Improve Life
-					</Button>
-				</>
-			}
->>>>>>> text_media_widget
 		/>
 
 		<div className="w-full p-2"></div>
