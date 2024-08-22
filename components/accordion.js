1033a52e04dd6a18c90632635a4cdca890939dc9
Raza Mahmood <razammahmood@gmail.com>
2024-08-22 16:56:42 -0400

    no margin on last paragraph in accordion

--- components/accordion.js
+++ components/accordion.js
@@ -11,7 +11,7 @@ import PropTypes from 'prop-types';
 export const Accordion = ({ title, children }) => (
   <Disclosure>
     {({ open }) => (
-      <div className="my-2">
+      <div className="my-2 [&_p:last-child]:mb-0">
         <DisclosureButton
           className={twJoin(
             'mb-1 inline-flex w-full cursor-pointer items-center justify-between gap-2 p-2.5 px-5 text-left text-xl transition-colors hover:bg-blue hover:text-white focus:bg-blue focus:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-blue focus-visible:ring-offset-2',
