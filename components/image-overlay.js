4f28b4257dc620422cd6c965850bf77ece0f50ce
Raza Mahmood <razammahmood@gmail.com>
2024-08-23 15:49:47 -0400

    refactor image overlay and extract into own component

---
+++ components/image-overlay.js
@@ -0,0 +1,59 @@
+import Image from 'next/image';
+import { twJoin, twMerge } from 'tailwind-merge';
+import PropTypes from 'prop-types';
+import { Container } from '@/components/container';
+
+export const ImageOverlay = ({ image, children, alignment, overlay = 'none', blurred = false }) => {
+  return (
+    <div className="flex flex-col relative w-full h-fit lg:min-h-[40rem]">
+      <div className="absolute w-full h-full z-10">
+        <Image
+          className={twMerge('w-full h-full object-cover', blurred && 'backdrop-blur', image.className)}
+          src={image.src}
+          width={image.width}
+          height={image.height}
+          blurDataURL={image.blurred}
+          alt={image.alt}
+          sizes="100vw"
+        />
+
+        {(overlay === 'dark' || overlay === 'light') && (
+          <div
+            className={twJoin(
+              'absolute top-0 left-0 w-full h-full',
+              overlay === 'dark' && 'bg-black/60',
+              overlay === 'light' && 'bg-white/60',
+            )}
+          ></div>
+        )}
+      </div>
+
+      <Container
+        centered
+        className={twJoin(
+          'relative py-20 flex flex-col z-20 flex-1',
+          alignment === 'center' && 'justify-center items-center',
+          alignment === 'left' && 'justify-center items-start',
+          alignment === 'bottom' && 'justify-end items-center',
+        )}
+      >
+        {children}
+      </Container>
+    </div>
+  );
+};
+
+ImageOverlay.propTypes = {
+  image: PropTypes.shape({
+    width: PropTypes.number,
+    height: PropTypes.number,
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+    blurred: PropTypes.string,
+    className: PropTypes.string,
+  }).isRequired,
+  children: PropTypes.node,
+  alignment: PropTypes.oneOf(['left', 'center', 'bottom']),
+  overlay: PropTypes.oneOf(['dark', 'light', 'none']),
+  blurred: PropTypes.bool,
+};
--- components/widgets/image-overlay.js
+++ components/widgets/image-overlay.js
@@ -1,114 +1,91 @@
 import red from '@/img/red-background.webp';
 import yellow from '@/img/yellow-background.webp';
-import Image from 'next/image';
-import { Container } from '@/components/container';
 import { twJoin } from 'tailwind-merge';
 import { HtmlParser } from '@/components/html-parser';
 import { Profile } from '@/components/profile';
 import { Blockquote } from '@/components/blockquote';
 import { ButtonSection } from '@/components/widgets/button-section';
-
-const getImage = (data) => {
-  switch (data.imageOverlayStyle?.name?.toLowerCase()) {
-    case 'yellow background':
-      return yellow;
-    case 'red background':
-      return red;
-    default:
-      return data?.backgroundImage?.image ? data.backgroundImage.image : yellow;
-  }
-};
-
-const Content = ({ data, style }) => {
-  switch (data?.__typename) {
-    case 'ParagraphGeneralText':
-      return (
-        <div className="[&_*]:text-inherit">
-          <HtmlParser html={data.body.processed} />
-        </div>
-      );
-    case 'ParagraphStoryQuote':
-      const image = data.image?.image;
-
-      return (
-        <Profile
-          image={
-            image
-              ? {
-                  src: image.url,
-                  height: image.height,
-                  width: image.width,
-                  alt: image.alt,
-                }
-              : undefined
-          }
-          body={
-            <Blockquote
-              color={style === 'yellow background' ? 'red' : 'yellow'}
-              className="text-inherit text-left xl:text-4xl"
-            >
-              {data?.quoteContent}
-            </Blockquote>
-          }
-          footer={
-            <>
-              {data?.quoteSource && <span className="text-xl">{data.quoteSource}</span>}
-              {data?.quoteDescription && <span className="text-xl">{data.quoteDescription}</span>}
-            </>
-          }
-          color={style === 'yellow background' ? 'red' : 'yellow'}
-        />
-      );
-    case 'ParagraphSectionButton':
-      return <ButtonSection data={data} />;
-    default:
-      return <></>;
-  }
-};
+import { ImageOverlay as ImageOverlayComponent } from '@/components/image-overlay';
 
 export const ImageOverlay = ({ data }) => {
-  console.log(data);
-  const background = getImage(data);
-  const style = data.imageOverlayStyle?.name?.toLowerCase();
-  const alignment = data?.contentAlignment?.name?.toLowerCase();
-  const content = data.imageOverlayContent;
+  const images = {
+    'Yellow background': yellow,
+    'Red background': red,
+  };
+
+  const overlays = {
+    'Dark overlay': 'dark',
+    'Light overlay': 'light',
+  };
+
+  const alignments = {
+    'Centre middle': 'center',
+    'Left middle': 'left',
+    'Bottom middle': 'bottom',
+  };
+
+  const style = data.imageOverlayStyle?.name;
+  const image = images[style] ?? data?.backgroundImage?.image;
+  const overlay = overlays[style] ?? 'none';
+  const alignment = alignments[data?.contentAlignment?.name] ?? 'center';
 
   return (
-    <div className="flex flex-col relative w-full h-fit lg:min-h-[60rem]">
-      <div className="absolute w-full h-full z-10">
-        <Image
-          className="w-full h-full object-cover"
-          src={background.src ?? background.url}
-          width={background.width}
-          height={background.height}
-          alt={background.alt || ''}
-        />
+    <ImageOverlayComponent
+      alignment={alignment}
+      overlay={overlay}
+      image={{
+        src: image.src ?? image.url,
+        width: image.width,
+        height: image.height,
+        alt: image.alt,
+      }}
+    >
+      <div className={twJoin((style === 'Dark overlay' || style === 'Red background') && 'text-white')}>
+        {data.imageOverlayContent?.map((data, index) => {
+          switch (data?.__typename) {
+            case 'ParagraphGeneralText':
+              return (
+                <div className="[&_*]:text-inherit">
+                  <HtmlParser html={data.body.processed} />
+                </div>
+              );
+            case 'ParagraphStoryQuote':
+              const image = data.image?.image
+                ? {
+                    src: data.image?.image.url,
+                    height: data.image?.image.height,
+                    width: data.image?.image.width,
+                    alt: data.image?.image.alt,
+                  }
+                : null;
 
-        {(style === 'dark overlay' || style === 'light overlay') && (
-          <div
-            className={twJoin(
-              'absolute top-0 left-0 w-full h-full',
-              style === 'dark overlay' && 'bg-black/60',
-              style === 'light overlay' && 'bg-white/60',
-            )}
-          ></div>
-        )}
+              return (
+                <Profile
+                  image={image}
+                  body={
+                    <Blockquote
+                      color={style === 'Yellow background' ? 'red' : 'yellow'}
+                      className="text-inherit text-left xl:text-4xl"
+                    >
+                      {data?.quoteContent}
+                    </Blockquote>
+                  }
+                  footer={
+                    <>
+                      {data?.quoteSource && <span className="text-xl">{data.quoteSource}</span>}
+                      {data?.quoteDescription && <span className="text-xl">{data.quoteDescription}</span>}
+                    </>
+                  }
+                  color={style === 'Yellow background' ? 'red' : 'yellow'}
+                />
+              );
+            case 'ParagraphSectionButton':
+              return <ButtonSection data={data} />;
+            default:
+              return <></>;
+          }
+        })}
       </div>
-
-      <Container
-        className={twJoin(
-          'relative py-20 flex flex-col z-20 flex-1',
-          (style === 'dark overlay' || style === 'red background') && 'text-white',
-          alignment === 'centre middle' && 'justify-center items-center',
-          alignment === 'left middle' && 'justify-center items-start',
-          alignment === 'centre bottom' && 'justify-end items-center',
-        )}
-        centered
-      >
-        {content?.map((data, index) => (
-          <Content key={data.id ?? index} data={data} style={style} />
-        ))}
-      </Container>
-    </div>
+    </ImageOverlayComponent>
   );
 };
