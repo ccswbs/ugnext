e59e2841fe9c82d159b6dff5f2151d7568ba3b11
Raza Mahmood <razammahmood@gmail.com>
2024-08-27 14:04:16 -0400

    remove api endpoints

--- pages/api/alert/activate.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(request, response) {
-  console.log('activating alert', response.status);
-
-  return response.status(200).json({});
-}
--- pages/api/alert/deactivate.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(request, response) {
-  console.log('deactivating alert', response.status);
-
-  return response.status(200).json({});
-}
