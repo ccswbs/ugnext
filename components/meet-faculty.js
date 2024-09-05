2c20122554eedffcd18b4fd6612e1a15f687c1a4
An Duong <aduong01@uoguelph.ca>
2024-09-05 09:56:47 -0400

    add text color, red, for the Meet the Faculty heading

--- components/meet-faculty.js
+++ components/meet-faculty.js
@@ -5,7 +5,7 @@ import { Testimonials } from '@/components/testimonial';
 export const MeetFaculty  = ({data}) => {
 	return (
 		<div className='ug-testimonial text-center bg-grey-light py-10' >
-			<Heading className='' level={data.heading.level}>{data.heading.title}</Heading>
+			<Heading className='text-red' level={data.heading.level}>{data.heading.title}</Heading>
 			<Testimonials 
 				testimonialData={data.testimonialData} 
 				slideNum = {data.setting.slideNum}
