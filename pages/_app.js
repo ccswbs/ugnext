71d2430ac26ea3d343ba8df24407f7de519bec4f
Miranda Mafe <miranda.mafe@gmail.com>
2024-09-16 16:03:57 -0400

    Add GoogleTagManager component to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '@/styles/globals.css';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 function Application({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />;
+			{process.env.NODE_ENV === 'production' && <GoogleTagManager gtmId={process.env.GTM_ID} />}
+		</>
+	)
 }
 
 export default Application;
