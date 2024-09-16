1a4b1367cae4e174f69e27fd2ceaab8494e8300a
Raza Mahmood <razammahmood@gmail.com>
2024-09-16 16:28:07 -0400

    add check for preview mode (disabled for now)

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '@/styles/globals.css';
 import { GoogleTagManager } from '@next/third-parties/google';
+import { useRouter } from 'next/router';
 
 function Application({ Component, pageProps }) {
+	const { isPreview } = useRouter();
+
 	return (
 		<>
 			<Component {...pageProps} />;
 			<GoogleTagManager gtmId={process.env.GTM_ID} />
-			{/* {process.env.NODE_ENV === 'production' && <GoogleTagManager gtmId={process.env.GTM_ID} />} */}
+			{/* {process.env.NODE_ENV === 'production' && !isPreview && <GoogleTagManager gtmId={process.env.GTM_ID} />} */}
 		</>
-	)
+	);
 }
 
 export default Application;
