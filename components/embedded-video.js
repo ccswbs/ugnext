cec0cc6c8e9b36e19b721b872c8f8efa07f1a274
Raza Mahmood <razammahmood@gmail.com>
2024-09-17 15:25:12 -0400

    Add PropTypes and storybook for EmbeddedVideo component and add url parsing

--- components/embedded-video.js
+++ components/embedded-video.js
@@ -1,29 +1,63 @@
 import React from 'react';
 import { twMerge } from 'tailwind-merge';
 import { Button } from '@/components/button';
+import PropTypes from 'prop-types';
 import { YouTubeEmbed } from '@next/third-parties/google';
 
-export function EmbeddedVideo({ type, id, title, transcript, height, width, className }) {
-	const ratio = Number.parse((width / height).toFixed(2));
+const getVideoInfo = (url) => {
+	if (!url) {
+		return { type: null, id: null };
+	}
+
+	const regex =
+		/^((?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|v\/|embed\/|user\/[A-Za-z0-9]+#p\/u\/\d\/)|youtu\.be\/)|(?:https?:\/\/)?(?:www\.)?vimeo\.com\/)([A-Za-z0-9\-_]+)/;
+
+	const match = url.match(regex);
+
+	if (match) {
+		const type = match[1].includes('youtube') || match[1].includes('youtu.be') ? 'youtube' : 'vimeo';
+		const id = match[2];
+
+		return { type: type, id: id };
+	}
+
+	return { type: null, id: null };
+};
+
+export function EmbeddedVideo({ url, title, transcript, className }) {
+	const { id, type } = getVideoInfo(url);
 
 	return (
 		<div className={twMerge('flex flex-col', className)}>
-			{type === 'youtube' && <YouTubeEmbed id={id} height={height} width={width} playlabel={title} />}
+			{type === 'youtube' && id && (
+				<YouTubeEmbed
+					style="width: 100%; max-width: unset;"
+					videoid={id}
+					playlabel={title ?? 'Youtube Embedded Video Player'}
+				/>
+			)}
 
-			{type === 'vimeo' && (
+			{type === 'vimeo' && id && (
 				<iframe
-					className="w-full h-full absolute top-0 left-0"
+					className="w-full h-full"
 					allowFullScreen
 					src={`https://player.vimeo.com/video/${id}`}
-					title={title ?? 'Vimeo EmbeddedVideo Player'}
+					title={title ?? `Vimeo Embedded Video Player`}
 				/>
 			)}
 
 			{transcript && (
-				<Button color="red" className="w-full" href={transcript} download>
+				<Button color="red" className="p-3 w-full" href={transcript} download={true}>
 					Download Transcript
 				</Button>
 			)}
 		</div>
 	);
 }
+
+EmbeddedVideo.propTypes = {
+	url: PropTypes.string.isRequired,
+	title: PropTypes.string,
+	transcript: PropTypes.string,
+	className: PropTypes.string,
+};
