d9648c2412171d64ab8f5cf4056b13c11c723603
Raza Mahmood <razammahmood@gmail.com>
2024-09-19 14:15:56 -0400

    prettier

--- data/yaml/admission/undergraduate/requirements/index.js
+++ data/yaml/admission/undergraduate/requirements/index.js
@@ -1,112 +1,112 @@
-import studentTypes from './student-types.yml';
-import locations from './locations.yml';
-import programs from '@/data/yaml/programs/undergraduate.yml';
-import fragments from './fragments.yml';
+import studentTypes from "./student-types.yml";
+import locations from "./locations.yml";
+import programs from "@/data/yaml/programs/undergraduate.yml";
+import fragments from "./fragments.yml";
 
 export const getAllStudentTypes = async () => {
-	return studentTypes;
+  return studentTypes;
 };
 
 export const getAllLocations = async () => {
-	return locations.reduce((acc, location) => {
-		acc[location.type] ??= [];
-		acc[location.type].push(location);
+  return locations.reduce((acc, location) => {
+    acc[location.type] ??= [];
+    acc[location.type].push(location);
 
-		return acc;
-	}, {});
+    return acc;
+  }, {});
 };
 
 export const getAllPrograms = async () => {
-	return programs.filter((program) => program.types.includes('major'));
+  return programs.filter((program) => program.types.includes("major"));
 };
 
 export const getStudentType = async (id) => {
-	return studentTypes.find((type) => type.id === id);
+  return studentTypes.find((type) => type.id === id);
 };
 
 export const getProgram = async (id) => {
-	return programs.find((program) => program.id === id);
+  return programs.find((program) => program.id === id);
 };
 
 export const getLocation = async (id) => {
-	return locations.find((location) => location.id === id);
+  return locations.find((location) => location.id === id);
 };
 
 export const slugToRequirement = async (slug) => {
-	const ids = {
-		studentType: slug[0],
-		program: slug.length !== 1 ? slug[1] : null,
-		location: slug.length === 3 ? slug[2] : null,
-	};
+  const ids = {
+    studentType: slug[0],
+    program: slug.length !== 1 ? slug[1] : null,
+    location: slug.length === 3 ? slug[2] : null,
+  };
 
-	const requirement = {
-		studentType: await getStudentType(ids.studentType),
-		program: await getProgram(ids.program),
-		location: await getLocation(ids.location),
-	};
+  const requirement = {
+    studentType: await getStudentType(ids.studentType),
+    program: await getProgram(ids.program),
+    location: await getLocation(ids.location),
+  };
 
-	// Determine if the slug was able to be correctly mapped to a valid requirement object
-	if (
-		// No student type
-		!requirement.studentType ||
-		// Student type is not program-dependent so program id shouldn't be in the slug
-		(!requirement.studentType.isProgramDependent && ids.program) ||
-		// Student type is not location-dependent so location id shouldn't be in the slug
-		(!requirement.studentType.isLocationDependent && ids.location) ||
-		// Student type is program-dependent so need to have retrieved a program object
-		(requirement.studentType.isProgramDependent && !requirement.program) ||
-		// Student type is location-dependent so need to have retrieved a location object
-		(requirement.studentType.isLocationDependent && !requirement.location)
-	) {
-		return null;
-	}
+  // Determine if the slug was able to be correctly mapped to a valid requirement object
+  if (
+    // No student type
+    !requirement.studentType ||
+    // Student type is not program-dependent so program id shouldn't be in the slug
+    (!requirement.studentType.isProgramDependent && ids.program) ||
+    // Student type is not location-dependent so location id shouldn't be in the slug
+    (!requirement.studentType.isLocationDependent && ids.location) ||
+    // Student type is program-dependent so need to have retrieved a program object
+    (requirement.studentType.isProgramDependent && !requirement.program) ||
+    // Student type is location-dependent so need to have retrieved a location object
+    (requirement.studentType.isLocationDependent && !requirement.location)
+  ) {
+    return null;
+  }
 
-	return requirement;
+  return requirement;
 };
 
 export const getRequirementTitle = ({ studentType, program, location }) => {
-	const studentTypeName = studentType?.name?.replace('Student', 'Students')?.replace('Graduate', 'Graduates');
+  const studentTypeName = studentType?.name?.replace("Student", "Students")?.replace("Graduate", "Graduates");
 
-	if (!program && !location) {
-		return `Undergraduate Admission Requirements for ${studentTypeName}`;
-	}
+  if (!program && !location) {
+    return `Undergraduate Admission Requirements for ${studentTypeName}`;
+  }
 
-	if (program && !location) {
-		return `${program.name} Admission Requirements for ${studentTypeName}`;
-	}
+  if (program && !location) {
+    return `${program.name} Admission Requirements for ${studentTypeName}`;
+  }
 
-	if (!program && location) {
-		return location.type === 'curriculum'
-			? `Undergraduate Admission Requirements for ${location.name} Students/Graduates`
-			: `Undergraduate Admission Requirements for ${studentTypeName} in ${location.name}`;
-	}
+  if (!program && location) {
+    return location.type === "curriculum"
+      ? `Undergraduate Admission Requirements for ${location.name} Students/Graduates`
+      : `Undergraduate Admission Requirements for ${studentTypeName} in ${location.name}`;
+  }
 
-	if (program && location) {
-		return location.type === 'curriculum'
-			? `${program.name} Admission Requirements for ${location.name} Students/Graduates`
-			: `${program.name} Admission Requirements for ${studentTypeName} in ${location.name}`;
-	}
+  if (program && location) {
+    return location.type === "curriculum"
+      ? `${program.name} Admission Requirements for ${location.name} Students/Graduates`
+      : `${program.name} Admission Requirements for ${studentTypeName} in ${location.name}`;
+  }
 };
 
 export const getRequirementContent = async (requirement) => {
-	return fragments
-		.filter((fragment) => {
-			const validate = (name) => {
-				const fragmentID = fragment[name];
-				const requirementID = requirement[name]?.id;
+  return fragments
+    .filter((fragment) => {
+      const validate = (name) => {
+        const fragmentID = fragment[name];
+        const requirementID = requirement[name]?.id;
 
-				return (
-					fragmentID === null ||
-					fragmentID === requirementID ||
-					(Array.isArray(fragmentID) && fragmentID.includes(requirementID))
-				);
-			};
+        return (
+          fragmentID === null ||
+          fragmentID === requirementID ||
+          (Array.isArray(fragmentID) && fragmentID.includes(requirementID))
+        );
+      };
 
-			return validate('studentType') && validate('program') && validate('location');
-		})
-		.sort((a, b) => {
-			return a.rank - b.rank;
-		})
-		.map((fragment) => fragment.content ?? '')
-		.join('');
+      return validate("studentType") && validate("program") && validate("location");
+    })
+    .sort((a, b) => {
+      return a.rank - b.rank;
+    })
+    .map((fragment) => fragment.content ?? "")
+    .join("");
 };
--- pages/admission/undergraduate/requirements/[...slug].js
+++ pages/admission/undergraduate/requirements/[...slug].js
@@ -1,55 +1,55 @@
 import {
-	getRequirementTitle,
-	slugToRequirement,
-	getRequirementContent,
-} from '@/data/yaml/admission/undergraduate/requirements';
-import { Layout } from '@/components/layout';
-import { Container } from '@/components/container';
-import { Heading } from '@/components/heading';
-import { HtmlParser } from '@/components/html-parser';
-import { Button } from '@/components/button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeftToBracket } from '@awesome.me/kit-7993323d0c/icons/sharp/solid';
+  getRequirementTitle,
+  slugToRequirement,
+  getRequirementContent,
+} from "@/data/yaml/admission/undergraduate/requirements";
+import { Layout } from "@/components/layout";
+import { Container } from "@/components/container";
+import { Heading } from "@/components/heading";
+import { HtmlParser } from "@/components/html-parser";
+import { Button } from "@/components/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftToBracket } from "@awesome.me/kit-7993323d0c/icons/sharp/solid";
 
 export async function getStaticPaths() {
-	return {
-		paths: [],
-		fallback: true,
-	};
+  return {
+    paths: [],
+    fallback: true,
+  };
 }
 
 export async function getStaticProps(context) {
-	const requirement = await slugToRequirement(context.params.slug);
+  const requirement = await slugToRequirement(context.params.slug);
 
-	if (!requirement) {
-		return {
-			notFound: true,
-		};
-	}
+  if (!requirement) {
+    return {
+      notFound: true,
+    };
+  }
 
-	return {
-		props: {
-			title: getRequirementTitle(requirement),
-			content: await getRequirementContent(requirement),
-		},
-	};
+  return {
+    props: {
+      title: getRequirementTitle(requirement),
+      content: await getRequirementContent(requirement),
+    },
+  };
 }
 
 export default function UndergraduateAdmissionRequirements({ title, content }) {
-	return (
-		<Layout title={title || 'Undergraduate Admission Requirements'}>
-			<Container centered>
-				<Heading level={1}>{title || 'Undergraduate Admission Requirements'}</Heading>
+  return (
+    <Layout title={title || "Undergraduate Admission Requirements"}>
+      <Container centered>
+        <Heading level={1}>{title || "Undergraduate Admission Requirements"}</Heading>
 
-				<Button className="flex gap-2 w-fit" color="red" href="/admission/undergraduate/requirements/">
-					<FontAwesomeIcon icon={faArrowLeftToBracket} />
-					<span>View Other Requirements</span>
-				</Button>
+        <Button className="flex gap-2 w-fit" color="red" href="/admission/undergraduate/requirements/">
+          <FontAwesomeIcon icon={faArrowLeftToBracket} />
+          <span>View Other Requirements</span>
+        </Button>
 
-				<div className="flex flex-col [&_p]:py-2 py-6">
-					<HtmlParser html={content ?? ''} />
-				</div>
-			</Container>
-		</Layout>
-	);
+        <div className="flex flex-col [&_p]:py-2 py-6">
+          <HtmlParser html={content ?? ""} />
+        </div>
+      </Container>
+    </Layout>
+  );
 }
