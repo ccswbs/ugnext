1d1f19bfddde7a672b8dd669d6b4c3956e3a4ba4
Raza Mahmood <razammahmood@gmail.com>
2024-09-19 14:14:05 -0400

    prettier

--- components/notification.js
+++ components/notification.js
@@ -1,55 +1,55 @@
-import React, { useMemo } from 'react';
-import PropTypes from 'prop-types';
-import { twMerge } from 'tailwind-merge';
-import objectHash from 'object-hash';
-import { useDismissible } from '@/lib/use-dismissible';
-import { UnstyledLink } from '@/components/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTriangleExclamation, faCircleXmark } from '@awesome.me/kit-7993323d0c/icons/classic/regular';
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+import { twMerge } from "tailwind-merge";
+import objectHash from "object-hash";
+import { useDismissible } from "@/lib/use-dismissible";
+import { UnstyledLink } from "@/components/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTriangleExclamation, faCircleXmark } from "@awesome.me/kit-7993323d0c/icons/classic/regular";
 
 const Notification = ({ title, url, color, className }) => {
-	const hash = useMemo(() => objectHash({ url, title }), [title, url]);
-	const { dismissed, dismiss } = useDismissible('notification-bar', hash, 'session');
+  const hash = useMemo(() => objectHash({ url, title }), [title, url]);
+  const { dismissed, dismiss } = useDismissible("notification-bar", hash, "session");
 
-	return (
-		<>
-			{!dismissed && (
-				<div
-					className={twMerge(
-						'sticky w-full flex gap-1 justify-center h-fit font-condensed items-center top-0 left-0 py-3 md:py-2 z-50 overflow-x-hidden px-[calc((100%-1320px)/2)]',
-						color === 'red' && 'bg-red text-white',
-						color === 'yellow' && 'bg-yellow text-black',
-						color === 'blue' && 'bg-light-blue text-black',
-						color === 'grey' && 'bg-grey text-black',
-						className,
-					)}
-				>
-					<UnstyledLink href={url} className="flex justify-center items-center ml-auto gap-[0.25em]">
-						<FontAwesomeIcon className="text-[1.25em]" icon={faTriangleExclamation} />
-						<span>{title}</span>
-						<span>{url?.replace(/https?:\/\//, '')}</span>
-					</UnstyledLink>
+  return (
+    <>
+      {!dismissed && (
+        <div
+          className={twMerge(
+            "sticky w-full flex gap-1 justify-center h-fit font-condensed items-center top-0 left-0 py-3 md:py-2 z-50 overflow-x-hidden px-[calc((100%-1320px)/2)]",
+            color === "red" && "bg-red text-white",
+            color === "yellow" && "bg-yellow text-black",
+            color === "blue" && "bg-light-blue text-black",
+            color === "grey" && "bg-grey text-black",
+            className
+          )}
+        >
+          <UnstyledLink href={url} className="flex justify-center items-center ml-auto gap-[0.25em]">
+            <FontAwesomeIcon className="text-[1.25em]" icon={faTriangleExclamation} />
+            <span>{title}</span>
+            <span>{url?.replace(/https?:\/\//, "")}</span>
+          </UnstyledLink>
 
-					<button
-						className="ml-auto"
-						onClick={(e) => {
-							e.stopPropagation();
-							dismiss();
-						}}
-					>
-						<FontAwesomeIcon className="text-[1.5em]" icon={faCircleXmark} />
-						<span className="sr-only">Dismiss Notification</span>
-					</button>
-				</div>
-			)}
-		</>
-	);
+          <button
+            className="ml-auto"
+            onClick={(e) => {
+              e.stopPropagation();
+              dismiss();
+            }}
+          >
+            <FontAwesomeIcon className="text-[1.5em]" icon={faCircleXmark} />
+            <span className="sr-only">Dismiss Notification</span>
+          </button>
+        </div>
+      )}
+    </>
+  );
 };
 
 Notification.propTypes = {
-	title: PropTypes.string.isRequired,
-	url: PropTypes.string.isRequired,
-	color: PropTypes.oneOf(['red', 'yellow', 'blue', 'grey']),
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(["red", "yellow", "blue", "grey"]),
 };
 
 export default Notification;
--- stories/notification.stories.js
+++ stories/notification.stories.js
@@ -1,29 +1,29 @@
-import Notification from '@/components/notification';
+import Notification from "@/components/notification";
 
 const config = {
-	title: 'Components/Notification',
-	component: Notification,
-	parameters: {
-		layout: 'fullscreen',
-		docs: {
-			toc: true,
-		},
-	},
-	tags: ['autodocs'],
+  title: "Components/Notification",
+  component: Notification,
+  parameters: {
+    layout: "fullscreen",
+    docs: {
+      toc: true,
+    },
+  },
+  tags: ["autodocs"],
 };
 
 export default config;
 
 export const Red = {
-	args: {
-		children: 'Example Notification',
-		color: 'red',
-	},
+  args: {
+    children: "Example Notification",
+    color: "red",
+  },
 };
 
 export const Yellow = {
-	args: {
-		children: 'Example Notification',
-		color: 'yellow',
-	},
+  args: {
+    children: "Example Notification",
+    color: "yellow",
+  },
 };
