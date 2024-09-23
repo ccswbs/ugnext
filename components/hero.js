09c22ff2c5b3b625769c659d5d3f53ebb31c11a3
Raza Mahmood <razammahmood@gmail.com>
2024-09-23 10:36:13 -0400

    Update button spacing on home hero

--- components/hero.js
+++ components/hero.js
@@ -48,7 +48,7 @@ export const Hero = ({ variant = "spotlight", image, title, caption, button, ali
                   color="yellow"
                   href={button?.href}
                   className={twJoin(
-                    "w-fit text-lg",
+                    "w-fit text-lg p-3",
                     alignment === "center" && "lg:mx-auto",
                     alignment === "right" && "lg:ml-auto"
                   )}
