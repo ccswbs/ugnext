0f20a74cf88e784254a55497f0758d8af6a40fed
Raza Mahmood <razammahmood@gmail.com>
2024-09-25 13:42:56 -0400

    Remove unused timestamp query from preview API handler.

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,7 +1,5 @@
 import { Drupal } from "@/lib/drupal";
 
 export default async function handler(request, response) {
-  request.query.timestamp ??= Date.now().toString();
-
   return await Drupal.preview(request, response);
 }
