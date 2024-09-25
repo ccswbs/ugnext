dd1bd06bc661161f8bfd8ca9df4bd0f9d18761a6
Raza Mahmood <razammahmood@gmail.com>
2024-09-25 13:38:15 -0400

    Add default timestamp to preview request query.

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,5 +1,7 @@
 import { Drupal } from "@/lib/drupal";
 
 export default async function handler(request, response) {
+  request.query.timestamp ??= Date.now();
+
   return await Drupal.preview(request, response);
 }
