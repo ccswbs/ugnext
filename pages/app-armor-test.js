cc5ca4758c1b8b246369e355dcb0a8cb43c7d1b3
Raza Mahmood <razammahmood@gmail.com>
2024-09-25 14:19:40 -0400

    Remove Clear Storage button from AppArmorTest page

--- pages/app-armor-test.js
+++ pages/app-armor-test.js
@@ -1,23 +1,12 @@
 import { Layout } from "@/components/layout";
 import { Container } from "@/components/container";
 import { Heading } from "@/components/heading";
-import { Button } from "@/components/button";
 
 export default function AppArmorTest() {
   return (
     <Layout>
       <Container centered>
         <Heading level={1}>App Armor Alert Test Page</Heading>
-
-        <Button
-          color="red"
-          onClick={() => {
-            window.sessionStorage.removeItem("app-armor-alert-use-dismissible-hash");
-            location.reload();
-          }}
-        >
-          Clear Storage (Enables alert if it was dismissed)
-        </Button>
       </Container>
     </Layout>
   );
