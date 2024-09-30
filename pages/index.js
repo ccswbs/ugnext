6d85cefd24e5a8b26e8ab66f00d4763f6af73614
Raza Mahmood <razammahmood@gmail.com>
2024-09-30 16:49:30 -0400

    Adjust spacing between sections

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import { SpotlightHero } from "@/components/home/spotlight-hero";
 import { HomeStory } from "@/components/home/story";
 import { StudyHere } from "@/components/home/study-here";
 import { getSpotlightCards, getSpotlightHero } from "@/data/drupal/home";
+import { twJoin } from "tailwind-merge";
 
 export async function getStaticProps(context) {
   const status = context?.preview || process.env.NODE_ENV !== "production" ? null : true;
@@ -24,6 +25,8 @@ export async function getStaticProps(context) {
 }
 
 export default function Home({ cards, hero }) {
+  const containerClasses = twJoin("pt-6");
+
   return (
     <Layout>
       {hero && <SpotlightHero hero={hero} />}
@@ -31,28 +34,28 @@ export default function Home({ cards, hero }) {
       <Container centered>
         <TagLine />
 
-        <div className="pt-1">
+        <div className={containerClasses}>
           <Heading level={1} as={"h2"} className="font-condensed text-black">
             Our Latest News and Events
           </Heading>
           <SpotlightCards cards={cards} />
         </div>
 
-        <div className="pt-1">
+        <div className={containerClasses}>
           <Heading level={1} as={"h2"} className="font-condensed text-black">
             Study Here
           </Heading>
           <StudyHere />
         </div>
 
-        <div className="pt-1">
+        <div className={containerClasses}>
           <Heading level={1} as={"h2"} className="font-condensed text-black">
             How We Rank Among the World
           </Heading>
           <Rankings />
         </div>
 
-        <div className="pt-1">
+        <div className={containerClasses}>
           <Heading level={1} as={"h2"} className="font-condensed text-black">
             Our Three Campuses
           </Heading>
