427220220c9a01674226cc448235d87951723fe4
Raza Mahmood <razammahmood@gmail.com>
2024-10-16 11:24:04 -0400

    Set tag line header to h2

--- components/home/tag-line.js
+++ components/home/tag-line.js
@@ -4,7 +4,7 @@ import { Divider } from "@/components/divider";
 export const TagLine = () => (
   <>
     <div className="flex flex-col items-center justify-center py-4">
-      <Heading level={1} className="mb-4 text-center font-condensed text-4xl text-black">
+      <Heading level={1} className="mb-4 text-center font-condensed text-4xl text-black" as="h2">
         University of Guelph, Ontario, Canada
       </Heading>
 
