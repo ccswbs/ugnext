36e2a364c7b7e0d6fce40d818f6e16c619e580c8
An Duong <aduong01@uoguelph.ca>
2024-10-22 14:08:16 -0400

    move heading of testimonial

--- components/meet-faculty.js
+++ components/meet-faculty.js
@@ -3,13 +3,14 @@ import { Heading } from '@/components/heading';
 import { Testimonials } from '@/components/testimonial';
 
 export const MeetFaculty  = ({data}) => {
+	const displayType = data.setting.displayType;
 	return (
 		<div className='ug-testimonial text-center bg-grey-light py-10' >
-			<Heading className='text-red' level={data.heading.level}>{data.heading.title}</Heading>
+			<Heading className={`text-red mx-auto md:px-5 ${displayType === 'col' ? 'sm:text-left' : ''}`}  level={data.heading.level}>{data.heading.title}</Heading>
 			<Testimonials 
 				testimonialData={data.testimonialData} 
 				slideNum = {data.setting.slideNum}
-				displayType={data.setting.displayType}
+				displayType= {displayType}
 			/>
 		</div>
 
