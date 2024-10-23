28bfe080a436866b6479e8eb2ab9b33839c0588a
Raza Mahmood <razammahmood@gmail.com>
2024-10-23 14:07:18 -0400

    Working on getting media and text grouped in sections

--- components/widgets/media-text.js
+++ components/widgets/media-text.js
@@ -6,9 +6,73 @@ import { EmbeddedVideo } from "@/components/embedded-video";
 import { HtmlParser } from "@/components/html-parser";
 import { ButtonSection } from "@/components/widgets/button-section";
 import ConditionalWrap from "conditional-wrap";
+import { MediaCaption } from "@/components/media-caption";
+import { useContext } from "react";
+import { SectionContext } from "@/components/section";
+
+const getBackground = (data) => {
+  switch (data?.background?.name) {
+    case "Light Blue":
+      return "light-blue";
+    case "Dark Gray":
+      return "dark-grey";
+    default:
+      return "none";
+  }
+};
+
+const getMedia = (data) => {
+  switch (data?.media?.__typename) {
+    case "MediaImage":
+      return {
+        src: data?.media?.image?.url,
+        width: data?.media?.image?.width,
+        height: data?.media?.image?.height,
+        alt: data?.media?.image?.alt,
+      };
+    case "MediaRemoteVideo":
+      return {
+        src: data?.media?.url,
+        title: data?.media?.name,
+        transcript: data?.media?.transcript,
+      };
+  }
+};
+
+const getPosition = (data, column) => {
+  switch (column) {
+    case "primary":
+      switch (data?.mediaImageSize) {
+        case "small":
+        case "medium":
+          return "left";
+        default:
+          return "above";
+      }
+    case "secondary":
+      return "above";
+    default:
+      return data?.mediaAlignment?.name ?? "left";
+  }
+};
 
 export const MediaText = ({ data }) => {
-  console.log(data);
+  const context = useContext(SectionContext);
+  const background = getBackground(data);
+  const size = data?.mediaImageSize ?? "small";
+  const media = getMedia(data);
+  const position = getPosition(data, context?.column);
+  const headingLevel = data?.headingLevel ?? 3;
 
-  return <div></div>;
+  console.log(context);
+
+  return (
+    <MediaCaption media={media} background={background} size={size} position={position}>
+      {data?.heading && <Heading level={headingLevel}>{data?.heading}</Heading>}
+
+      <HtmlParser html={data?.description?.processed ?? ""} />
+
+      {data?.buttonSection && <ButtonSection data={data.buttonSection} />}
+    </MediaCaption>
+  );
 };
--- components/widgets/section-widget.js
+++ components/widgets/section-widget.js
@@ -4,30 +4,39 @@ import { WidgetSelector } from "@/components/widgets/widget-selector";
 import { getHeadingLevel } from "@/lib/string-utils";
 
 export const Section = ({ data }) => {
-  const primary = [];
-  const secondary = [];
-  const others = [];
+  const { primary, secondary, others } = data?.content?.reduce(
+    (acc, widget, index) => {
+      const columnName = widget?.buttonSectionColumn?.name ?? widget?.sectionColumn?.name;
+      const column = columnName ? columnName.toLowerCase() : "";
 
-  data?.content?.forEach((widget, index) => {
-    const columnName = widget?.buttonSectionColumn?.name ?? widget?.sectionColumn?.name;
-    const column = columnName ? columnName.toLowerCase() : '';
-    switch (column) {
-      case "primary":
-        primary.push(<WidgetSelector key={index} data={widget} />);
-        break;
-      case "secondary":
-        secondary.push(<WidgetSelector key={index} data={widget} />);
-        break;
-      default:
-        others.push(<WidgetSelector key={index} data={widget} />);
-        break;
+      switch (column) {
+        case "primary":
+          // Media and Text widgets need to be grouped together when in a primary column, so we will process primary widgets later.
+          acc.primary.push(<WidgetSelector key={index} data={widget} />);
+          break;
+        case "secondary":
+          acc.secondary.push(<WidgetSelector key={index} data={widget} />);
+          break;
+        default:
+          acc.others.push(<WidgetSelector key={index} data={widget} />);
+          break;
+      }
+
+      return acc;
+    },
+    {
+      primary: [],
+      secondary: [],
+      others: [],
     }
-  });
+  );
 
   return (
     <>
       {data.heading && <Heading level={getHeadingLevel(data.headingLevel) ?? 1}>{data.heading}</Heading>}
+
       {others.length > 0 && <div className="w-full text-center">{...others}</div>}
+
       {(primary.length > 0 || secondary.length > 0) && <SectionComponent primary={primary} secondary={secondary} />}
     </>
   );
