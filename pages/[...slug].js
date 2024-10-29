a6718a36e6f11fcd6cfb43b1ded3460307938cb2
Raza Mahmood <razammahmood@gmail.com>
2024-10-29 13:43:11 -0400

    Add ability to have hero widgets modal video appear on basic pages

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -62,6 +62,13 @@ export default function Page({ content }) {
               width: content.image.width,
               alt: content.image.alt,
             }}
+            video={
+              content?.heroWidgets?.video && {
+                src: content.heroWidgets.video.url,
+                title: content.heroWidgets.video.name,
+                transcript: content.heroWidgets.video.transcript?.url,
+              }
+            }
             title={content.title}
           />
 
