d3e275823aab8c127f2ef57c1cc8da03d4474407
Raza Mahmood <razammahmood@gmail.com>
2024-11-11 12:44:19 -0500

    Add close button to modal component

--- components/modal.js
+++ components/modal.js
@@ -1,6 +1,8 @@
-import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
+import { CloseButton, Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 import { twJoin } from "tailwind-merge";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@awesome.me/kit-7993323d0c/icons/classic/solid";
 
 export const Modal = ({ open, onClose = () => {}, role = "dialog", labelledBy, centered, children }) => (
   <Dialog
@@ -16,8 +18,18 @@ export const Modal = ({ open, onClose = () => {}, role = "dialog", labelledBy, c
       className="fixed inset-0 bg-black/40 transition duration-300 ease-out data-[closed]:opacity-0"
     />
 
-    <div className="fixed inset-0 flex w-screen justify-center p-4">
-      <DialogPanel className={twJoin("h-fit w-fit", centered && "my-auto")}>{children}</DialogPanel>
+    <div className="fixed inset-0 flex w-screen justify-center p-4 pt-6">
+      <div className="relative">
+        <DialogPanel className={twJoin("h-fit w-fit", centered && "my-auto")}>{children}</DialogPanel>
+
+        <CloseButton
+          onClick={onClose}
+          className="text-xl text-white bg-grey-950 w-9 h-9 flex items-center justify-center rounded-full absolute top-0 right-0 md:-top-4 md:-right-4 hover:bg-red transition-colors"
+        >
+          <span className="sr-only">Close</span>
+          <FontAwesomeIcon icon={faTimes} />
+        </CloseButton>
+      </div>
     </div>
   </Dialog>
 );
