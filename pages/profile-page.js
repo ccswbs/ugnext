2407b543e980888bed68ce16f6f8ab66d1ea71ee
Tanya Qureshi <tqureshi@uoguelph.ca>
2024-12-13 11:08:13 -0500

    Switch to uniweb-utils

--- pages/profile-page.js
+++ pages/profile-page.js
@@ -5,16 +5,19 @@ import { useEffect, useState } from "react";
 import { Container } from "@/components/container";
 import { Heading } from "@/components/heading";
 import React from 'react';
-import UniWebProfile from '../components/uniweb-profile';
+import { UniWebProfile, UniWebUnitMembers } from '../components/uniweb-utils';
 
 export default function ProfilePage() {
   const userId = 353; // Replace with the desired ID
 
   return (
-    <Layout metadata={{ title: "Profile Test" }}>
+    <Layout metadata={{ title: "UniWEB Test" }}>
       <Container>
-        <h1>Data Display</h1>
+        <h1 className="font-bold my-7 text-4xl mb-0">UniWEB Test</h1>
+        <h2 className="font-bold my-3 text-3xl text-dark">Profile Data</h2>
         <UniWebProfile id={userId} />
+        <h2 className="font-bold my-3 text-3xl text-dark">Member List</h2>
+        <UniWebUnitMembers unitName="pathobiology" />
       </Container>
     </Layout>
   );
