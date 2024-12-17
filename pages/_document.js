6b9a9ba8ed983fe4316b35236f585ecc89bd7ab0
Raza Mahmood <razammahmood@gmail.com>
2024-12-17 14:52:12 -0500

    Replace useHeaderHeight hook with CSS variable logic

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,22 @@ export default function Document() {
           strategy="beforeInteractive"
         />
         <Script src="https://kit.fontawesome.com/7993323d0c.js" crossorigin="anonymous" strategy="lazyOnload" />
+
+        <Script id="header-height-css-variable" strategy="afterInteractive">
+          {`
+            const updateVariable = () => {
+              const header = document.querySelector('uofg-header');              
+              document.body.style.setProperty('--header-height', header.clientHeight + 'px');
+            }
+            
+            window.addEventListener('resize', updateVariable, { passive: true });
+            
+            window.addEventListener('load', () => {
+              const header = document.querySelector('uofg-header');              
+              document.body.style.setProperty('--header-height', header.clientHeight + 'px');
+            });
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
