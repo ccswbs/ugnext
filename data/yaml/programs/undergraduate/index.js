f3972949a4178613d74f593f499f0e8614e0c7b4
Raza Mahmood <razammahmood@gmail.com>
2025-01-06 11:30:32 -0500

    Add back functions to fetch and process admission requirements

--- data/yaml/programs/undergraduate/index.js
+++ data/yaml/programs/undergraduate/index.js
@@ -10,6 +10,10 @@ import {
 
 const directory = path.join(process.cwd(), "data", "yaml", "programs", "undergraduate");
 
+export async function getUndergraduateStudentTypes() {
+  return await getStudentTypes(directory);
+}
+
 export async function getUndergraduateDegreeTypes() {
   return await getDegreeTypes(directory);
 }
@@ -32,4 +36,49 @@ export async function getUndergraduatePrograms() {
   return [...programs, ...degrees.map((degree) => ({ ...degree, types: [degree.type] }))].sort((a, b) =>
     a.name.localeCompare(b.name)
   );
+}
+
+export async function slugToUndergraduateRequirements(slug) {
+  const programs = await getPrograms(directory);
+  const degrees = await getDegrees(directory);
+
+  const studentType = (await getStudentTypes(directory)).find((studentType) => studentType.id === slug[0]);
+
+  if (!studentType) {
+    return null;
+  }
+
+  const location = (await getAdmissionLocations()).find((location) => location.id === slug[1]);
+
+  if (!location) {
+    return null;
+  }
+
+  const program = programs.find((program) => program.id === slug[2]) ?? degrees.find((degree) => degree.id === slug[2]);
+
+  if (!program) {
+    return null;
+  }
+
+  return { studentType, location, program };
+}
+
+export async function getUndergraduateRequirements(studentType, location, program) {
+  const requirements =
+    program.requirements
+      ?.filter((requirement) => {
+        const matchesStudentType = requirement.studentType === studentType.id;
+        const matchesLocation =
+          (Array.isArray(requirement.location) && requirement.location.includes(location.id)) ||
+          requirement.location === location.id;
+
+        return matchesStudentType && matchesLocation;
+      })
+      ?.map((requirement) => requirement.content)
+      ?.join("") ?? "";
+
+  return {
+    title: `${program.name} Admission Requirements for ${studentType.name.replace("Student", "Students").replace("Graduate", "Graduates")} in ${location.name}`,
+    content: requirements,
+  };
 }
