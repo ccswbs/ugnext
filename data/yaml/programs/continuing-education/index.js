d4177574ae8cba6ddf8c5969998e86e3f99f557f
Raza Mahmood <razammahmood@gmail.com>
2025-01-15 19:34:10 -0500

    trying another fix

--- data/yaml/programs/continuing-education/index.js
+++ data/yaml/programs/continuing-education/index.js
@@ -2,7 +2,7 @@ import path from "path";
 import { z } from "zod";
 import { getYamlData } from "@/lib/file-utils";
 
-const directory = path.join("data", "yaml", "programs", "continuing-education");
+const directory = path.join(process.cwd(), "data", "yaml", "programs", "continuing-education");
 
 export async function getContinuingEducationProgramTypes() {
   return await getYamlData({
