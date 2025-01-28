55f442bd9d9b469651c8e849ebf5e162feb311dc
An Duong <aduong01@uoguelph.ca>
2025-01-28 09:54:36 -0500

    update test page

--- pages/brand-refresh-style-test.js
+++ pages/brand-refresh-style-test.js
@@ -1,13 +1,14 @@
 import { Layout } from "@/components/layout";
 import { Container } from "@/components/container";
 import { Heading } from "@/components/heading";
+import { Divider } from "@/components/divider";
 
 export default function StyledTest() {
   return (
     <Layout metadata={{ title: "uog and uog Styles Test Page" }}>
       <Container centered>
         <Heading level={1}>uog and uog Styles Test Page</Heading>
-        <hr/>
+        <Divider />
         <div>
             <Heading level={1}>This is H1</Heading>
             <Heading level={2}  className="text-red">This is H2</Heading>
@@ -17,46 +18,46 @@ export default function StyledTest() {
             <Heading level={6}>This is H6</Heading>
             <p>This is p tag: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
-        <hr/>
+        <Divider />
         
         <div>
             <Heading level={2}>Use CSS variable styles</Heading>
             <div style={{ color: 'var(--bitter-font)' }}>
-               <h2 style={{ color: 'var(--red)' }}>This is the uog style, font Bitter.</h2>
-               <p style={{ color: 'var(--red)' }}>
-                <span style={{ color: 'var(--red)',backgroundColor:'var(--red)', marginRight:'1rem' }}>Span</span>
+               <h2 style={{ color: 'var(--uog-color-red)' }}>This is the uog style, font Bitter.</h2>
+               <p style={{ color: 'var(--uog-color-red)' }}>
+                <span style={{ color: 'var(--red)',backgroundColor:'var(--uog-color-red)', marginRight:'1rem' }}>Span</span>
                 This is red.
                </p>
-               <p style={{ color: 'var(--black)' }}>
-                <span style={{ color: 'var(--black)',backgroundColor:'var(--black)', marginRight:'1rem' }}>Span</span>
+               <p style={{ color: 'var(--uog-color-black)' }}>
+                <span style={{ color: 'var(--uog-color-black)',backgroundColor:'var(--uog-color-black)', marginRight:'1rem' }}>Span</span>
                  This is black.
                </p>
-               <p style={{ color: 'var(--yellow)' }}>
-                <span style={{ color: 'var(--yellow)',backgroundColor:'var(--yellow)', marginRight:'1rem' }}>Span</span>
+               <p style={{ color: 'var(--uog-color-yellow)' }}>
+                <span style={{ color: 'var(--uog-color-yellow)',backgroundColor:'var(--uog-color-yellow)', marginRight:'1rem' }}>Span</span>
                 This is yellow.
                 </p>
             </div>
             <div style={{ color: 'var(--dmSans-font)' }}>
-              <h2 style={{ color: 'var(--blue)' }}>This is the uog style, font DM Sans.</h2>
-              <p style={{ color: 'var(--blue)' }}>
-                <span style={{ color: 'var(--blue)',backgroundColor:'var(--blue)', marginRight:'1rem' }}>Span</span>
+              <h2 style={{ color: 'var(--uog-color-blue)' }}>This is the uog style, font DM Sans.</h2>
+              <p style={{ color: 'var(--uog-color-blue)' }}>
+                <span style={{ color: 'var(--uog-color-blue)',backgroundColor:'var(--uog-color-blue)', marginRight:'1rem' }}>Span</span>
                 This is blue
               </p>
-              <p style={{ color: 'var(--green)' }}>
-                <span style={{ color: 'var(--green)',backgroundColor:'var(--green)', marginRight:'1rem' }}>Span</span>
+              <p style={{ color: 'var(--uog-color-green)' }}>
+                <span style={{ color: 'var(--uog-color-green)',backgroundColor:'var(--uog-color-green)', marginRight:'1rem' }}>Span</span>
                This is green
               </p>
-              <p style={{ color: 'var(--light-gray)' }}>
-                <span style={{ color: 'var(--light-gray)',backgroundColor:'var(--light-gray)', marginRight:'1rem' }}>Span</span>
+              <p style={{ color: 'var(--uog-color-light-gray)' }}>
+                <span style={{ color: 'var(--uog-color-light-gray)',backgroundColor:'var(--uog-color-light-gray)', marginRight:'1rem' }}>Span</span>
                 This is light gray.
               </p>
-              <p style={{ color: 'var(--dark-gray)' }}>
-                <span style={{ color: 'var(--dark-gray)',backgroundColor:'var(--dark-gray)', marginRight:'1rem' }}>Span</span>
+              <p style={{ color: 'var(--uog-color-dark-gray)' }}>
+                <span style={{ color: 'var(--uog-color-dark-gray)',backgroundColor:'var(--uog-color-dark-gray)', marginRight:'1rem' }}>Span</span>
                 This is dark gray.
               </p>
             </div>
         </div>
-        <hr/>
+        <Divider />
           <div>
             <Heading level={2}>Use Tailwind CSS styles</Heading>
             <div className="font-bitter">
