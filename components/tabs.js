49ad624b50b38cd89fac0890092acbbd94674f55
Tanya Qureshi <tqureshi@uoguelph.ca>
2025-02-27 18:20:50 -0500

    Colour class updates

--- components/tabs.js
+++ components/tabs.js
@@ -10,7 +10,7 @@ export const Tabs = ({ fullWidth = true, tabs }) => (
         <Tab
           key={index}
           className={twJoin(
-            "mb-1 rounded-t-sm bg-gray-200 px-4 py-3 text-lg font-bold text-black transition-colors hover:bg-gray-300 focus:bg-gray-300 focus:outline-none ui-selected:mb-0 ui-selected:border-2 ui-selected:border-yellow ui-selected:bg-uog-yellow",
+            "mb-1 rounded-t-sm bg-uog-grey-light-bg px-4 py-3 text-lg font-bold text-black transition-colors hover:bg-gray-300 focus:bg-gray-300 focus:outline-none ui-selected:mb-0 ui-selected:border-2 ui-selected:border-uog-yellow ui-selected:bg-uog-yellow",
             fullWidth && "flex-1"
           )}
         >
