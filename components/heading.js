94047984f71a1b14ffa2dc9000d8da1ab5b8e7df
Tanya Qureshi <tqureshi@uoguelph.ca>
2025-02-28 11:55:35 -0500

    Font size, line height and margin changes

--- components/heading.js
+++ components/heading.js
@@ -12,11 +12,11 @@ export const Heading = ({ level, children, className, as, ...rest }) => {
       className={twMerge(
         twJoin(
           "font-bold leading-tight",
-          headingLevel === 1 && "font-serif my-7 text-4xl",
-          headingLevel === 2 && "my-3 text-3xl text-uog-red",
-          headingLevel === 3 && "my-3 text-2xl",
-          headingLevel === 4 && "my-3 text-xl",
-          (headingLevel === 5 || headingLevel === 6) && "my-3 text-xl"
+          headingLevel === 1 && "font-serif mb-0 mt-[2rem] text-[4rem]",
+          headingLevel === 2 && "mb-[1.5rem] mt-[3rem] text-[3rem] text-uog-red",
+          headingLevel === 3 && "mb-[1.5rem] mt-[3rem] text-[2.5rem]",
+          headingLevel === 4 && "mb-[1.5rem] mt-[3rem] text-[2rem]",
+          (headingLevel === 5 || headingLevel === 6) && "mb-[1.5rem] mt-[3rem] text-[2rem]"
         ),
         className
       )}
