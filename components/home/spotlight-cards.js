a3e9867fe4b3cd45d6f547d71aec84528d9975b3
Raza Mahmood <razammahmood@gmail.com>
2025-03-07 14:28:01 -0500

    Remove sizes attribute from spotlight cards component

--- components/home/spotlight-cards.js
+++ components/home/spotlight-cards.js
@@ -21,7 +21,6 @@ export const SpotlightCards = ({ cards }) => (
             card.thumbnailImageCrop === "left" && "object-left",
             card.thumbnailImageCrop === "center" && "object-center"
           ),
-          sizes: "(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw",
         }}
       />
     ))}
