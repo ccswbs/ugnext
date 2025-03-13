e6cb1897fa720e617da05b6bbd001181b8c8aca4
Scott Schau <sschau@uoguelph.ca>
2025-03-13 14:11:33 -0400

    more help

--- components/custom-footer.js
+++ components/custom-footer.js
@@ -1,27 +1,28 @@
 import { Container } from "@/components/container";
-
+import { HtmlParser } from "@/components/html-parser";
 
 export const CustomFooter = ({custfoot}) => {
 
     console.log (custfoot.customFooterByTag.results)
 
     return (
-    <>
-            {custfoot?.customFooterByTag.results.map((customFoot) => (
+    <div>
+            {custfoot?.customFooterByTag.results.map((customFoot) => 
                 customFoot.id 
         // <HtmlPaser html={customFoot.body.processed}/＞
-            ))}
+            )}
 
 
 
         <Container className="">
 
     <div className="bg-gray-50">
-            This is it
+
+            <HtmlParser html={custfoot.customFooterByTag.results[1].body.processed} />
     </div>
         </Container>
 
-    </>
+    </div>
 );
 
 };
