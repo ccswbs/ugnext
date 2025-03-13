058f4ef53e87485e696940080d6818703f51329a
Tanya Qureshi <tqureshi@uoguelph.ca>
2025-03-13 16:42:44 -0400

    Adjust heading margins

--- components/home/tag-line.js
+++ components/home/tag-line.js
@@ -2,8 +2,8 @@ import { Heading } from "@/components/heading";
 
 export const TagLine = () => (
   <>
-    <div className="flex flex-col items-center justify-center py-4">
-      <Heading level={1} className="mt-[4rem]">
+    <div className="flex flex-col py-4">
+      <Heading level={1} className="mt-[4rem] text-center">
         University of Guelph, Ontario, Canada
       </Heading>
       <span className="mt-2 text-center text-2xl">Improve Life</span>
