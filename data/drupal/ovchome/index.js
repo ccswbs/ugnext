08f4e953b21cc136640454b5f062b6ae2a9866e9
Scott Schau <sschau@uoguelph.ca>
2025-03-17 13:38:16 -0400

    error - at least one "query" or "queryId"

---
+++ data/drupal/ovchome/index.js
@@ -0,0 +1,9 @@
+import { graphql } from "@/lib/drupal";
+import { getFeaturedLegacyNewsQuery } from "./get-featured-legacy-news.graphql";
+
+export const getFeaturedLegacyNews = async () => {
+    const { data } = await graphql(getFeaturedLegacyNewsQuery, {
+
+    })
+    return data?.nodeArticles;
+};
