83e721e4d3cf33e8687fe67d2a67c36c8e854020
Scott Schau <sschau@uoguelph.ca>
2025-03-20 12:45:12 -0400

    events and news front page

--- data/drupal/ovchome/index.js
+++ data/drupal/ovchome/index.js
@@ -1,9 +1,16 @@
 import { graphql } from "@/lib/drupal";
-import { getFeaturedLegacyNewsQuery } from "./get-featured-legacy-news.graphql";
+import getFeaturedLegacyNewsQuery from "./get-featured-legacy-news.graphql";
+import getFeaturedLegacyEventsQuery from "./get-featured-legacy-events.graphql";
 
 export const getFeaturedLegacyNews = async () => {
     const { data } = await graphql(getFeaturedLegacyNewsQuery, {
 
     })
-    return data?.nodeArticles;
+    return data?.featuredNews?.results;
+};
+export const getFeaturedLegacyEvents = async () => {
+    const { data } = await graphql(getFeaturedLegacyEventsQuery, {
+
+    })
+    return data?.featuredEvents?.results;
 };
