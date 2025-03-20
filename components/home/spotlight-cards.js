4d6f5e7a55249cd669193dc1befafcf356d1d594
Miranda Mafe <miranda.mafe@gmail.com>
2025-03-19 21:24:52 -0400

    Increase quality to 100

--- components/home/spotlight-cards.js
+++ components/home/spotlight-cards.js
@@ -22,6 +22,7 @@ export const SpotlightCards = ({ cards }) => (
             card.thumbnailImageCrop === "center" && "object-center"
           ),
           sizes: "(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw",
+          quality: 100,
         }}
       />
     ))}
