4e5516f7a49ed661931d2bdd2187baca660e9385
Raza Mahmood <razammahmood@gmail.com>
2025-04-11 12:31:12 -0400

    Refactor SpotlightCards and ThreeCampuses components to use tailwind-variants and @uoguelph/react-component

--- components/home/spotlight-cards.js
+++ components/home/spotlight-cards.js
@@ -1,24 +1,39 @@
 import { Card, CardContent, CardImage, CardTitle } from "@uoguelph/react-components/card";
 import { UnstyledLink } from "@/components/link";
 import Image from "next/image";
+import { tv } from "tailwind-variants";
 
-export const SpotlightCards = ({ cards }) => (
-  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-    {cards.map((card, index) => (
-      <Card key={card.id} as={UnstyledLink} href={card.url.url} className="h-full" centered>
-        <CardImage
-          src={card.image.url}
-          alt={card.image.alt}
-          width={card.image.width}
-          height={card.image.height}
-          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
-          className="aspect-3/2 w-full"
-          as={Image}
-        />
-        <CardContent className="flex-1">
-          <CardTitle>{card.title}</CardTitle>
-        </CardContent>
-      </Card>
-    ))}
-  </div>
-);
+export const SpotlightCards = ({ cards }) => {
+  const classes = tv({
+    slots: {
+      container: "grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4",
+      card: "h-full",
+      image: "aspect-3/2 w-full",
+      content: "flex-1",
+      title: "text-[2.25rem]! font-bold text-center",
+    },
+  });
+
+  const { container, card, image, content, title } = classes();
+
+  return (
+    <div className={container()}>
+      {cards.map((data, index) => (
+        <Card key={data.id} as={UnstyledLink} href={data.url.url} className={card()} centered>
+          <CardImage
+            src={data.image.url}
+            alt={data.image.alt}
+            width={data.image.width}
+            height={data.image.height}
+            sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
+            className={image()}
+            as={Image}
+          />
+          <CardContent className={content()}>
+            <CardTitle className={title()}>{data.title}</CardTitle>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
