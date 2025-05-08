d6aaae41c3cf13648574830d3fe930ab95012ee9
Raza Mahmood <razammahmood@gmail.com>
2025-05-08 16:59:44 -0400

    Enable preview mode cookie scoping and patch next-drupal to stop navigation from exiting preview mode

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -4,9 +4,15 @@ export default async function handler(request, response) {
   // If the request has our secret key, enable preview mode, typically this will not be from Drupal,
   // because Drupal will send an encrypted token, but we can use this for testing.
   if (request.query.secret === process.env.DRUPAL_PREVIEW_SECRET) {
-    response.setPreviewData({});
-    response.writeHead(302, { Location: '/' });
-    response.end('Preview mode enabled');
+    response.setPreviewData(
+      {},
+      {
+        maxAge: 60 * 60,
+        path: "/",
+      }
+    );
+    response.writeHead(302, { Location: "/" });
+    response.end("Preview mode enabled");
     return;
   }
 
