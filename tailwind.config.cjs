21218b7a4f218d8bf06bdc8e29d7afa6c864f6a0
Raza Mahmood <razammahmood@gmail.com>
2025-05-26 10:23:02 -0400

    Clean tailwind config for migration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,84 +6,59 @@ const colors = {
   "uog-color-white-focus": "#ddd",
   "uog-color-white-on-dark": "#fff",
   "uog-color-white-contrast": "#000",
-  
+
   "uog-color-black": "#000",
   "uog-color-black-focus": "#444",
   "uog-color-black-on-light": "#000",
   "uog-color-black-contrast": "#fff",
-  
+
   "uog-color-red": "#e51937",
   "uog-color-red-focus": "#b3142c",
   "uog-color-red-on-light": "#dc1836",
   "uog-color-red-contrast": "#fff",
-  
+
   "uog-color-yellow": "#ffc429",
   "uog-color-yellow-focus": "#ffe299",
   "uog-color-yellow-on-dark": "#ffc429",
   "uog-color-yellow-contrast": "#000",
-  
+
   "uog-color-blue": "#187bb4",
   "uog-color-blue-focus": "#135f8b",
   "uog-color-blue-on-light": "#1775ab",
   "uog-color-blue-contrast": "#fff",
-  
+
   "uog-color-green": "#318738",
   "uog-color-green-focus": "#27682c",
   "uog-color-green-on-light": "#2e7f35",
   "uog-color-green-contrast": "#fff",
-  
+
   "uog-color-grey-light": "#d8d8d8",
   "uog-color-grey-light-focus": "#bfbfbf",
   "uog-color-grey-light-contrast": "#000",
   "uog-color-grey-light-bg": "#f5f5f5",
-  
+
   "uog-color-grey-dark": "#747676",
   "uog-color-grey-dark-focus": "#63625f",
   "uog-color-grey-dark-contrast": "#fff",
   "uog-color-grey-dark-bg": "#222",
-  
+
   "uog-color-body-copy": "#555",
   "uog-color-body-copy-on-light": "#555",
   "uog-color-body-copy-on-dark": "#ccc",
-  
+
   "uog-color-body-copy-bold": "#333",
   "uog-color-body-copy-bold-on-light": "#333",
   "uog-color-body-copy-bold-on-dark": "#e2e2e2",
-  
+
   "uog-color-body-copy-link": "#187bb4",
   "uog-color-body-copy-link-on-light": "#166ea2",
   "uog-color-body-copy-link-on-dark": "#1d93d7",
-  
+
   "facebook-color-blue": "#0866ff",
   "instagram-color-pink": "#d62976",
   "linkedin-color-blue": "#0a66c2",
-  "youtube-color-red": "#ff0000"
+  "youtube-color-red": "#ff0000",
 };
-
-const scaled = (input, scale) => {
-  if (input == null) {
-    return input;
-  }
-
-  switch (typeof input) {
-    case "object":
-      if (Array.isArray(input)) {
-        return input.map((val) => scaled(val, scale));
-      } else {
-        const ret = {};
-        for (const key in input) {
-          ret[key] = scaled(input[key], scale);
-        }
-        return ret;
-      }
-    case "string":
-      return input.replace(/(\d*\.?\d+)rem$/, (_, val) => parseFloat((parseFloat(val) * scale).toFixed(4)) + "rem");
-    default:
-      return input;
-  }
-};
-
-const scale = 1 / 0.625;
 
 module.exports = {
   content: [
@@ -94,17 +69,6 @@ module.exports = {
   ],
   darkMode: "selector",
   theme: {
-    borderRadius: scaled(defaultTheme.borderRadius, scale),
-    columns: scaled(defaultTheme.columns, scale),
-    fontSize: scaled(defaultTheme.fontSize, scale),
-    lineHeight: scaled(defaultTheme.lineHeight, scale),
-    maxWidth: ({ theme, breakpoints }) => ({
-      ...scaled(defaultTheme.maxWidth({ theme, breakpoints }), scale),
-    }),
-    maxHeight: ({ theme, breakpoints }) => ({
-      ...scaled(defaultTheme.maxHeight({ theme, breakpoints }), scale),
-    }),
-    spacing: scaled(defaultTheme.spacing, scale),
     extend: {
       fontFamily: {
         condensed: ["DM Sans", "sans-serif"],
@@ -120,7 +84,7 @@ module.exports = {
         "chevron-right": '"\\f054"',
       },
       backgroundImage: {
-        divider: `linear-gradient(90deg, ${colors['uog-color-red']} 0%, ${colors['uog-color-red']} 33.33%, ${colors['uog-color-yellow']} 33.33%, ${colors['uog-color-yellow']} 66.66%, ${colors['uog-color-black']} 66.66%, ${colors['uog-color-black']} 100%)`,
+        divider: `linear-gradient(90deg, ${colors["uog-color-red"]} 0%, ${colors["uog-color-red"]} 33.33%, ${colors["uog-color-yellow"]} 33.33%, ${colors["uog-color-yellow"]} 66.66%, ${colors["uog-color-black"]} 66.66%, ${colors["uog-color-black"]} 100%)`,
       },
       aria: {
         "page-current": 'current="page"',
