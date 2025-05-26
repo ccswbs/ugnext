8b29c1628477fd924cb98ee3f5461a6661092f67
Raza Mahmood <razammahmood@gmail.com>
2025-05-26 13:45:24 -0400

    Migrate 404 page to use @uoguelph/react-components

--- pages/404.js
+++ pages/404.js
@@ -1,56 +1,74 @@
 import Head from "next/head";
-import { Layout } from "@/components/layout";
+import { Layout, LayoutContent } from "@uoguelph/react-components/layout";
 import { useEffect, useState } from "react";
-import { Container } from "@/components/container";
-import { Heading } from "@/components/heading";
-import { Link } from "@/components/link";
-import { List, ListItem } from "@/components/list";
+import { Typography } from "@uoguelph/react-components/typography";
+import { Link } from "@uoguelph/react-components/link";
+import { List, ListItem } from "@uoguelph/react-components/list";
+import { Header } from "@uoguelph/react-components/header";
+import { Footer } from "@uoguelph/react-components/footer";
 
-export default function Home() {
+export default function NotFound() {
   const [searchLink, setSearchLink] = useState("https://www.uoguelph.ca/search");
 
   useEffect(() => {
-    setSearchLink("https://www.uoguelph.ca/search/#gsc.tab=0&gsc.q=" + window.location.pathname + "&gsc.sort=");
+    setSearchLink("https://www.uoguelph.ca/search/#gsc.tab=0&gsc.q=" + window.location.pathname);
   }, []);
 
   return (
-    <Layout metadata={{ title: "404: Not Found" }}>
-      <Container centered>
-        <Heading level={1}>HTTP 404 — File not found</Heading>
-        <Heading level={2}>Possible reasons for this error:</Heading>
+    <>
+      <Head>
+        <title>HTTP 404 — File not found</title>
+      </Head>
 
-        <List variant="ordered">
-          <ListItem>
-            You have clicked on an out-of-date bookmark. Once you find the correct page, please update your bookmark to
-            avoid this error in the future.
-          </ListItem>
+      <Header></Header>
 
-          <ListItem>
-            You have mis-typed the web address into the URL bar. Please check your spelling of the URL.
-          </ListItem>
+      <Layout>
+        <LayoutContent>
+          <Typography type="h1" as="h1" className="block!">
+            HTTP 404 — File not found
+          </Typography>
 
-          <ListItem>
-            The search engine has an out-of-date listing for this page -{" "}
-            <Link href="mailto:ithelp@uoguelph.ca">please let us know!</Link>
-          </ListItem>
+          <Typography type="h2" as="h2" className="block!">
+            Possible reasons for this error:
+          </Typography>
 
-          <ListItem>
-            The university has removed this page (either on purpose or by mistake) -{" "}
-            <Link href="mailto:ithelp@uoguelph.ca">please let us know!</Link>
-          </ListItem>
-        </List>
+          <List as="ol">
+            <ListItem>
+              You have clicked on an out-of-date bookmark. Once you find the correct page, please update your bookmark
+              to avoid this error in the future.
+            </ListItem>
 
-        <Heading level={2}>Try one of these links instead:</Heading>
+            <ListItem>
+              You have mis-typed the web address into the URL bar. Please check your spelling of the URL.
+            </ListItem>
 
-        <List>
-          <ListItem>
-            <Link href="https://www.uoguelph.ca/">Go to the University of Guelph Home Page</Link>
-          </ListItem>
-          <ListItem>
-            <Link href={searchLink}>Search on the University of Guelph</Link>
-          </ListItem>
-        </List>
-      </Container>
-    </Layout>
+            <ListItem>
+              The search engine has an out-of-date listing for this page -{" "}
+              <Link href="mailto:ithelp@uoguelph.ca">please let us know!</Link>
+            </ListItem>
+
+            <ListItem>
+              The university has removed this page (either on purpose or by mistake) -{" "}
+              <Link href="mailto:ithelp@uoguelph.ca">please let us know!</Link>
+            </ListItem>
+          </List>
+
+          <Typography type="h2" as="h2" className="block!">
+            Try one of these links instead:
+          </Typography>
+
+          <List as="ul">
+            <ListItem>
+              <Link href="https://www.uoguelph.ca/">Go to the University of Guelph Home Page</Link>
+            </ListItem>
+            <ListItem>
+              <Link href={searchLink}>Search on the University of Guelph</Link>
+            </ListItem>
+          </List>
+        </LayoutContent>
+      </Layout>
+
+      <Footer></Footer>
+    </>
   );
 }
