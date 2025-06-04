f37cd997b4f93cc21a3633ccbde2ae218b3d0939
Raza Mahmood <razammahmood@gmail.com>
2025-06-03 22:50:30 -0400

    Refactor `BlockWidget` to handle invalid block types gracefully and remove unused `YamlBlock` component.

--- components/widgets/block.js
+++ components/widgets/block.js
@@ -9,22 +9,17 @@ const WidgetBlock = ({ data }) => {
   return <WidgetSelector data={data.content} />;
 };
 
-const YamlBlock = ({ data }) => {
-  return (
-    <>
-      <div>YAML BLOCK: {JSON.stringify(data)}</div>
-    </>
-  );
-};
-
 export const BlockWidget = ({ data }) => {
   const map = {
     BlockContentBasic: BasicBlock,
     BlockContentWidgetBlock: WidgetBlock,
-    BlockContentYamlBlock: YamlBlock,
   };
 
-  const Widget = map[data.block.__typename];
+  const Widget = map?.[data.block.__typename];
+
+  if (!Widget) {
+    return <>Invalid block type: {data.block.__typename}</>;
+  }
 
   return <Widget data={data.block} />;
 };
