59e3df7c7236d36ca0a18201f1187d6e274fc2ad
Raza Mahmood <razammahmood@gmail.com>
2025-06-05 16:16:24 -0400

    Refactor undergraduate programs and degrees fetching
    New strategy, rather than fetching ids and getting latest revision, we fetch all nodes, and only get latest revision if node is unpublished and we are not in draft mode

--- data/drupal/programs/undergraduate/index.js
+++ data/drupal/programs/undergraduate/index.js
@@ -1,11 +1,10 @@
 import { graphql } from "@/lib/drupal";
 import getDegreeTypesQuery from "./get-degree-types.graphql";
 import getProgramTypesQuery from "./get-program-types.graphql";
-import getDegreeIdsQuery from "./get-degree-ids.graphql";
-import getDegreeLatestRevisionQuery from "./get-degree-latest-revision.graphql";
-import getProgramIdsQuery from "./get-program-ids.graphql";
-import getProgramLatestRevisionQuery from "./get-program-latest-revision.graphql";
-import getRequirementIdsQuery from "@/data/drupal/programs/undergraduate/requirements/get-requirement-ids.graphql";
+import getDegreesQuery from "./get-degrees.graphql";
+import getDegreeLatestPublishedRevisionQuery from "./get-degree-latest-published-revision.graphql";
+import getProgramsQuery from "./get-programs.graphql";
+import getProgramLatestRevisionPublishedQuery from "./get-program-latest-published-revision.graphql";
 
 export async function getDegreeTypes() {
   const { data } = await graphql(getDegreeTypesQuery);
@@ -18,81 +17,86 @@ export async function getProgramTypes() {
 }
 
 export async function getDegrees(draft = false) {
-  // Get all the degree ids.
+  function process(degree) {
+    return {
+      ...degree,
+      __typename: "undergraduate-degree",
+      url: degree.url?.url
+        ? degree.url.url?.startsWith("http")
+          ? degree.url.url
+          : `https://uoguelph.ca${degree.url.url}`
+        : "",
+      type: degree.type?.name ?? null,
+      tags: degree.tags?.map((tag) => tag.name) ?? [],
+    };
+  }
+  // Get all the degrees.
   // MOTE: we are making the assumption that they're no more than 100 degrees (a fair assumption since no university has 100 different degrees), otherwise this query will need to be refactored to use pagination/cursors.
-  const { data } = await graphql(getDegreeIdsQuery);
+  const { data } = await graphql(getDegreesQuery);
+  const degrees = [];
+  const unpublished = [];
 
-  // Destructure the nodes to get the ids.
-  const ids = data.nodeUndergraduateDegrees.nodes.map((node) => node.id);
+  // Partition the degrees into ones that are published vs unpublished, if we are in draft mode, then will just stick everything in the published array as we don't care about hiding unpublished content.
+  for (const degree of data.nodeUndergraduateDegrees.nodes) {
+    !draft || degree.status ? degrees.push(degree) : unpublished.push(degree);
+  }
 
-  // Now that we have the ids, we can retrieve the content.
-  const degrees = ids.map(async (id) => {
-    const { data } = await graphql(getDegreeLatestRevisionQuery, {
+  // We are not in draft mode, so we want to update the content to the latest published revision
+  for (const { id } of unpublished) {
+    const { data } = await graphql(getDegreeLatestPublishedRevisionQuery, {
       id: id,
-      status: draft ? undefined : true,
     });
 
-    const result = data.latestContentRevision.results[0];
+    degrees.push(data.latestContentRevision.results[0]);
+  }
 
-    return {
-      ...result,
-      __typename: "undergraduate-degree",
-      url: result.url?.url
-        ? result.url.url?.startsWith("http")
-          ? result.url.url
-          : `https://uoguelph.ca${result.url.url}`
-        : "",
-      type: result.type?.name ?? null,
-      tags: result.tags?.map((tag) => tag.name) ?? [],
-    };
-  });
-
-  return Promise.all(degrees);
+  return degrees.map(process).sort((a, b) => a.name.localeCompare(b.name));
 }
 
 export async function getPrograms(draft = false) {
+  function process(program) {
+    return {
+      ...program,
+      __typename: "undergraduate-program",
+      url: program.url?.url
+        ? program.url.url?.startsWith("http")
+          ? program.url.url
+          : `https://uoguelph.ca${program.url.url}`
+        : "",
+      type: program.type?.map((type) => type.name) ?? [],
+      degree: program?.degree?.name ?? null,
+      tags: program.tags?.map((tag) => tag.name) ?? [],
+    };
+  }
+
   // Similar idea to getDegrees but our assumption of less than 100 fails for programs, so we need to use pagination/cursors.
   let hasNextPage = true;
   let cursor = "";
-  let ids = [];
+  const programs = [];
+  const unpublished = [];
 
-  // We keep looping a while as long as there is an another page
+  // We keep looping a while as long as there is another page
   while (hasNextPage) {
-    const { data } = await graphql(getProgramIdsQuery, {
+    const { data } = await graphql(getProgramsQuery, {
       after: cursor,
     });
 
-    const results = data.nodeUndergraduatePrograms.nodes.map((node) => node.id);
+    for (const program of data.nodeUndergraduatePrograms.nodes) {
+      !draft || program.status ? programs.push(program) : unpublished.push(program);
+    }
 
-    ids = [...ids, ...results];
     hasNextPage = data.nodeUndergraduatePrograms.pageInfo.hasNextPage;
     cursor = data.nodeUndergraduatePrograms.pageInfo.endCursor;
   }
 
-  // Get the latest revisions content for each ID we got from before.
-  const programs = [];
-
-  for (const id of ids) {
-    const { data } = await graphql(getProgramLatestRevisionQuery, {
+  for (const { id } of unpublished) {
+    const { data } = await graphql(getProgramLatestRevisionPublishedQuery, {
       id: id,
       status: draft ? undefined : true,
     });
 
-    const result = data.latestContentRevision.results[0];
-
-    programs.push({
-      ...result,
-      __typename: "undergraduate-program",
-      url: result.url?.url
-        ? result.url.url?.startsWith("http")
-          ? result.url.url
-          : `https://uoguelph.ca${result.url.url}`
-        : "",
-      type: result.type?.map((type) => type.name) ?? [],
-      degree: result?.degree?.name ?? null,
-      tags: result.tags?.map((tag) => tag.name) ?? [],
-    });
+    programs.push(data.latestContentRevision.results[0]);
   }
 
-  return programs;
+  return programs.map(process).sort((a, b) => a.name.localeCompare(b.name));
 }
