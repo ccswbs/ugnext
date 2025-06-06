7b09669904e169a7b1c0461bc507e1804ac8361e
Raza Mahmood <razammahmood@gmail.com>
2025-06-05 23:44:27 -0400

    Update Playwright config: add more devices to projects and update webServer command to `npm run start`.

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,10 +32,26 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+    {
+      name: "firefox",
+      use: devices["Desktop Firefox"],
+    },
+    {
+      name: "webkit",
+      use: devices["Desktop Safari"],
+    },
+    {
+      name: "Mobile Chrome",
+      use: devices["Pixel 5"],
+    },
+    {
+      name: "Mobile Safari",
+      use: devices["iPhone 12"],
+    },
   ],
   // Run your local dev server before starting the tests.
   webServer: {
-    command: "npm run dev",
+    command: "npm run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
