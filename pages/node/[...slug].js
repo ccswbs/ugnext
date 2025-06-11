b1d76ef1dd5da8c629fff4adbe82d1bb733a72e5
Scott Schau <sschau@uoguelph.ca>
2025-06-11 11:00:01 -0400

    destination: "/ovc/news/node/{ID}"

--- pages/node/[...slug].js
+++ pages/node/[...slug].js
@@ -41,7 +41,7 @@ export async function getStaticProps(context) {
     case "NodeArticle":
       return {
         redirect: {
-          destination: "/ovc/news/[...slug].js",
+          destination: "/ovc/news/node/{ID}",
           permanent: true,
         },
       };
