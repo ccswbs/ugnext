37c0c0389094c0ad740a194b0047bdcae0ef5b0b
Raza Mahmood <razammahmood@gmail.com>
2025-06-23 13:26:31 -0400

    Refactor OVC page to use updated `@uoguelph/react-components` for layout, typography, and hero components; improve structure and maintain consistency.

--- pages/ovc.js
+++ pages/ovc.js
@@ -1,26 +1,23 @@
-import { Layout } from "@/components/layout";
-import { Container } from "@/components/container";
-import { Heading } from "@/components/heading";
-import { Hero } from "@/components/hero";
+import { Layout, LayoutContent } from "@uoguelph/react-components/layout";
+import { Container } from "@uoguelph/react-components/container";
+import { Hero, HeroTitle, HeroVideo } from "@uoguelph/react-components/hero";
+import { Divider } from "@uoguelph/react-components/divider";
+import { Button } from "@uoguelph/react-components/button";
+import { Typography } from "@uoguelph/react-components/typography";
+import { Link } from "@uoguelph/react-components/link";
+import { OVCFooter } from "@/components/ovc/ovc-footer";
+import { UnstyledLink } from "@/components/unstyled-link";
+import { OVCCards } from "@/components/ovchome/ovc-cards";
+import { WidgetSelector } from "@/components/widgets/widget-selector";
 import { getPageContent, getPageID, getPageMenu } from "@/data/drupal/basic-pages";
 import { getFeaturedLegacyNews } from "@/data/drupal/ovc-news";
-import { WidgetSelector } from "@/components/widgets/widget-selector";
 import Image from "next/image";
-
-import { Divider } from "@/components/divider";
-import {
-  FormatDateFull,
-  // FormatEventDate,
-  // FormatEventDay,
-  // FormatEventMonth,
-  // FormatEventWeekday,
-} from "@/lib/date-utils";
+import { FormatDateFull } from "@/lib/date-utils";
 import { twMerge } from "tailwind-merge";
-import { OVCCards } from "@/components/ovchome/ovc-cards";
-import { Link } from "@/components/link";
-import { OVCFooter } from "@/components/ovc/ovc-footer";
-import { Button } from "@/components/button";
 import defaultImage from "@/img/ovc/OVC_front_entrance.jpeg";
+import { Header } from "@/components/header";
+import { Meta } from "@/components/meta";
+import { Footer } from "@uoguelph/react-components/footer";
 
 export async function getStaticProps(context) {
   const status = context?.preview || process.env.NODE_ENV !== "production" ? null : true;
@@ -73,35 +70,34 @@ function renderFeaturedNewsItem(newsItem, index) {
     <div
       key={index}
       className={twMerge(
-        "tile overflow-hidden shadow-lg ",
+        "tile overflow-hidden shadow-lg",
         index === 0 ? "row-span-2" : "grid grid-cols-1 lg:grid-cols-2 gap-4"
       )}
     >
       {/* Image Section */}
       <div className="relative">
-        <Image 
+        <Image
           src={newsItem?.heroImage?.image?.url || defaultImage.src}
           width={newsItem?.heroImage?.image?.width || defaultImage.width}
-          height={newsItem?.heroImage?.image?.height|| defaultImage.height}
+          height={newsItem?.heroImage?.image?.height || defaultImage.height}
           blurred={newsItem?.heroImage?.image?.blurDataURL || defaultImage.blurDataURL}
-          alt={newsItem?.heroImage?.image?.alt  || "OVC Front Entrance"}
-          className="object-cover "
-          style={{ aspectRatio: "9 / 4" }}
+          alt={newsItem?.heroImage?.image?.alt || "OVC Front Entrance"}
+          className="object-cover md:aspect-[9/4] h-full w-full"
         />
       </div>
 
       {/* Text Section */}
       <div className="p-4">
-        <Heading
-          level={index === 0 ? 3 : 5}
+        <Typography
+          type={"h5"}
           as={"h3"}
-          className={twMerge("font-condensed mt-0 text-lg", index === 0 ? "text-dark" : "")}
+          className={twMerge("font-condensed! mt-0! text-lg", index === 0 ? "text-dark" : "")}
         >
           <Link href={newsItem?.path} className="hover:underline">
             {newsItem?.title}
           </Link>
-        </Heading>
-        <p className="text-sm text-gray-600">{newsItem?.articleDate}</p>
+        </Typography>
+        <p className="text-sm text-grey-dark">{newsItem?.articleDate}</p>
       </div>
     </div>
   );
@@ -109,60 +105,68 @@ function renderFeaturedNewsItem(newsItem, index) {
 
 export default function Page({ content }) {
   return (
-    <Layout metadata={{ title: content?.title }} header={content?.menu}>
-      {content?.image ? (
-        <>
-          <Hero
-            variant="content-hub"
-            image={{
-              src: content.image.url,
-              height: content.image.height,
-              width: content.image.width,
-              alt: content.image.alt,
-            }}
-            video={
-              content?.heroWidgets?.video && {
-                src: content.heroWidgets.video.url,
-                title: content.heroWidgets.video.name,
-                transcript: content.heroWidgets.video.transcript?.url,
-              }
-            }
-            title={content.title}
-          />
-        </>
-      ) : (
-        <>
-          <Container centered>
-            <Heading level={1} className="mb-0">
-              {content?.title}
-            </Heading>
-          </Container>
-        </>
-      )}
+    <Layout>
+      <Meta title={content?.title + " | University of Guelph"} />
+      <Header title={content?.menu?.topic?.title} url={content?.menu?.topic?.url} menu={content?.menu?.navigation} />
 
-      {content?.widgets?.map((widget, index) => (
-        <WidgetSelector key={index} data={widget} />
-      ))}
+      <LayoutContent container={false}>
+        {content?.image ? (
+          <>
+            <Hero
+              alt={content.image.alt}
+              height={content.image.height}
+              width={content.image.width}
+              src={content.image.url}
+              variant="basic"
+              priority
+              as={Image}
+            >
+              <HeroTitle>{content.title}</HeroTitle>
+              {content?.heroWidgets?.video && (
+                <HeroVideo
+                  src={content.heroWidgets.video.url}
+                  title={content.heroWidgets.video.name}
+                  transcript={content.heroWidgets.video.transcript?.url}
+                />
+              )}
+            </Hero>
+          </>
+        ) : (
+          <>
+            <Container>
+              <Typography type="h1" as="h1" className="mb-0">
+                {content?.title}
+              </Typography>
+            </Container>
+          </>
+        )}
 
-      <Container centered>
-        <Heading level={2} as={"h2"} className="font-condensed">
-          Featured OVC News
-          <Divider />
-        </Heading>
-        <div className="container m-auto grid grid-cols-1 grid-rows-2 gap-4 md:grid-cols-1 lg:grid-cols-2">
-          {content?.featuredLegacyNews.map((newsItem, index) => renderFeaturedNewsItem(newsItem, index))}
-        </div>
-        <div className="pt-4">
-          <Button href="/ovc/news/" color="red" className="py-2 px-4 mx-[.25em] text-2xl">
-            OVC News Hub
-          </Button>
-          <Divider />
-        </div>
-        <div className="pt-6">
-          <OVCCards />
-        </div>
-      </Container>
+        {content?.widgets?.map((widget, index) => (
+          <WidgetSelector key={index} data={widget} />
+        ))}
+
+        <Container>
+          <Typography type="h2" as="h2" className="font-condensed">
+            Featured OVC News
+            <Divider />
+          </Typography>
+          <div className="m-auto grid grid-cols-1 grid-rows-2 gap-4 md:grid-cols-1 lg:grid-cols-2">
+            {content?.featuredLegacyNews.map((newsItem, index) => renderFeaturedNewsItem(newsItem, index))}
+          </div>
+          <div className="pt-4">
+            <Button as={UnstyledLink} href="/ovc/news/" color="red" className="py-2 px-4 mx-[.25em] text-2xl">
+              OVC News Hub
+            </Button>
+            <Divider />
+          </div>
+          <div className="pt-6">
+            <OVCCards />
+          </div>
+        </Container>
+      </LayoutContent>
+
       <OVCFooter />
+      <Footer />
     </Layout>
   );
 }
