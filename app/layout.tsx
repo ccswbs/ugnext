46178a7ce9c086a3defdf199b1d89274a17c95c4
Raza Mahmood <razammahmood@gmail.com>
2025-07-07 14:26:26 -0400

    Enable draft mode UI with preview banner and exit button. Update imports to include `@uoguelph/react-components/button`.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import Script from "next/script";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { Metadata } from "next";
 import AppArmor from "@/components/client/app-armor";
+import { draftMode } from "next/headers";
 
 // Stylesheets
 import "@/styles/globals.css";
 import "@uoguelph/web-components/style";
+import { Button } from "@uoguelph/react-components/button";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://uoguelph.ca"),
@@ -25,6 +27,7 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const gtmId =
     process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_GTM_ID_DEV : process.env.NEXT_PUBLIC_GTM_ID;
+  const { isEnabled: isDraftMode } = await draftMode();
 
   return (
     <html lang="en" className="h-full">
@@ -46,6 +49,16 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <Script src="https://kit.fontawesome.com/7993323d0c.js" crossOrigin="anonymous" strategy="lazyOnload" />
 
         <AppArmor />
+
+        {isDraftMode && (
+          <div className="sticky left-0 top-0 z-20 flex h-fit w-full items-center justify-center gap-2 bg-uog-color-red p-2 text-center text-base font-bold text-white">
+            <span>You are currently in Preview Mode.</span>
+
+            <Button color="yellow" className="p-2" href="/api/exit-preview" as="a">
+              Exit Preview Mode
+            </Button>
+          </div>
+        )}
 
         {children}
       </body>
