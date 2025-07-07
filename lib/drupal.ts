eac518aa12fa9f72ca1d8b70388427f3c802ad71
Raza Mahmood <razammahmood@gmail.com>
2025-07-07 14:19:51 -0400

    Add API routes for revalidation, draft mode, and update Drupal client integration

---
+++ lib/drupal.ts
@@ -0,0 +1,14 @@
+import { NextDrupal } from "next-drupal";
+
+const baseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL as string;
+const clientId = process.env.DRUPAL_CLIENT_ID as string;
+const clientSecret = process.env.DRUPAL_CLIENT_SECRET as string;
+
+export const drupal = new NextDrupal(baseUrl, {
+  auth: {
+    clientId,
+    clientSecret,
+  },
+  withAuth: true,
+  debug: process.env.NODE_ENV === "development",
+});
