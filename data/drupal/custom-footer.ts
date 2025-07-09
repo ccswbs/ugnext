30704865a60613224ae5a7b9ebdcc0b078b287a8
Raza Mahmood <razammahmood@gmail.com>
2025-07-09 15:08:06 -0400

    Add utility functions for fetching `CustomFooter` by tags, units, or ID with unpublished content support

--- data/drupal/custom-footer.ts
+++ data/drupal/custom-footer.ts
@@ -1,8 +1,11 @@
 import { gql } from "@/lib/graphql";
+import { showUnpublishedContent } from "@/lib/show-unpublished-content";
+import { query } from "@/lib/apollo";
 
 export const CUSTOM_FOOTER_FRAGMENT = gql(/* gql */ `
   fragment CustomFooter on NodeCustomFooter {
     __typename
+    status
     title
     footerLogo {
       ...Image
@@ -16,3 +19,79 @@ export const CUSTOM_FOOTER_FRAGMENT = gql(/* gql */ `
     }
   }
 `);
+
+async function getCustomFooterID(tags: string[], units: string[]) {
+  const showUnpublished = await showUnpublishedContent();
+
+  const { data } = await query({
+    query: gql(/* gql */ `
+      query GetCustomFooterID($tags: [String], $units: [String], $status: Boolean) {
+        customFooterByUnitOrTag(filter: { tag: $tags, unit: $units, status: $status }) {
+          results {
+            ... on NodeCustomFooter {
+              id
+            }
+          }
+        }
+      }
+    `),
+    variables: {
+      tags: tags ?? null,
+      units: units ?? null,
+      status: showUnpublished ? null : true,
+    },
+  });
+
+  if (!data?.customFooterByUnitOrTag?.results) {
+    return null;
+  }
+
+  if (data.customFooterByUnitOrTag.results.length === 0) {
+    return null;
+  }
+
+  if (data.customFooterByUnitOrTag.results[0].__typename !== "NodeCustomFooter") {
+    return null;
+  }
+
+  return data.customFooterByUnitOrTag.results[0].id;
+}
+
+export async function getCustomFooterByID(id: string) {
+  const showUnpublished = await showUnpublishedContent();
+
+  const { data } = await query({
+    query: gql(/* gql */ `
+      query CustomFooterContent($id: ID!, $revision: ID = "current") {
+        nodeCustomFooter(id: $id, revision: $revision) {
+          ...CustomFooter
+        }
+      }
+    `),
+    variables: {
+      id: id,
+      revision: showUnpublished ? "latest" : "current",
+    },
+  });
+
+  if (!data?.nodeCustomFooter) {
+    return null;
+  }
+
+  if (data.nodeCustomFooter.status === false && !showUnpublished) {
+    return null;
+  }
+
+  return data.nodeCustomFooter;
+}
+
+export async function getCustomFooterByTagsOrUnits(tags: string[], units: string[]) {
+  const showUnpublished = await showUnpublishedContent();
+  const id = await getCustomFooterID(tags, units);
+
+  if (!id) {
+    return null;
+  }
+
+  return await getCustomFooterByID(id);
+}
