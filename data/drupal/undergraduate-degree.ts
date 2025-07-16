0351e88c35e7738fefeaa8594f68e9c80e41e13d
Raza Mahmood <razammahmood@gmail.com>
2025-07-15 20:12:10 -0400

    Add support for fetching draft undergraduate degrees when unpublished content is enabled

--- data/drupal/undergraduate-degree.ts
+++ data/drupal/undergraduate-degree.ts
@@ -1,6 +1,7 @@
 import { query } from "@/lib/apollo";
 import { gql } from "@/lib/graphql";
 import { UndergraduateDegreesQuery } from "@/lib/graphql/types";
+import { showUnpublishedContent } from "@/lib/show-unpublished-content";
 
 export async function getUndergraduateDegreeTypes() {
   const { data } = await query({
@@ -37,7 +38,49 @@ export const UNDERGRADUATE_DEGREE = gql(/* gql */ `
   }
 `);
 
-export async function getUndergraduateDegrees() {
+async function getDraftUndergraduateDegrees() {
+  const degreeQuery = gql(/* gql */ `
+    query DraftUndergraduateDegrees($pageSize: Int = 100, $page: Int = 0) {
+      latestContentRevisions(filter: { type: "undergraduate_degree" }, pageSize: $pageSize, page: $page) {
+        pageInfo {
+          total
+        }
+        results {
+          __typename
+          ...UndergraduateDegree
+        }
+      }
+    }
+  `);
+
+  const degrees: UndergraduateDegreesQuery["nodeUndergraduateDegrees"]["nodes"] = [];
+  const pageSize = 100;
+  let page = 0;
+  let total = 1;
+
+  while (page < total) {
+    const { data } = await query({
+      query: degreeQuery,
+      variables: {
+        pageSize,
+        page,
+      },
+    });
+
+    for (const degree of data?.latestContentRevisions?.results ?? []) {
+      if (degree.__typename === "NodeUndergraduateDegree") {
+        degrees.push(degree);
+      }
+    }
+
+    total = Math.ceil((data?.latestContentRevisions?.pageInfo?.total ?? 0) / pageSize);
+    page++;
+  }
+
+  return degrees;
+}
+
+async function getPublishedUndergraduateDegrees() {
   const degreesQuery = gql(/* gql */ `
     query UndergraduateDegrees($after: Cursor = "") {
       nodeUndergraduateDegrees(after: $after, first: 100) {
@@ -54,7 +97,7 @@ export async function getUndergraduateDegrees() {
 
   let cursor = "";
   let hasNextPage = true;
-  const degrees: UndergraduateDegreesQuery['nodeUndergraduateDegrees']['nodes'] = [];
+  const degrees: UndergraduateDegreesQuery["nodeUndergraduateDegrees"]["nodes"] = [];
 
   while (hasNextPage) {
     const { data } = await query({
@@ -69,5 +112,13 @@ export async function getUndergraduateDegrees() {
     hasNextPage = data.nodeUndergraduateDegrees.pageInfo.hasNextPage;
   }
 
-  return degrees;
+  return degrees.filter((degree) => degree.status);
+}
+
+export async function getUndergraduateDegrees() {
+  if (await showUnpublishedContent()) {
+    return await getDraftUndergraduateDegrees();
+  }
+
+  return await getPublishedUndergraduateDegrees();
 }
