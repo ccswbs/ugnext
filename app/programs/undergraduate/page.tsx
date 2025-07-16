8f8cba1727f7b0c8145f45570c067fce43cf5182
Raza Mahmood <razammahmood@gmail.com>
2025-07-16 12:58:08 -0400

    Working on program search

--- app/programs/undergraduate/page.tsx
+++ app/programs/undergraduate/page.tsx
@@ -5,13 +5,43 @@ import { LayoutContent } from "@uoguelph/react-components/layout";
 import { Typography } from "@uoguelph/react-components/typography";
 import { Footer } from "@uoguelph/react-components/footer";
 import { ProgramSearch } from "@/components/client/programs/program-search";
-import {
-  getUndergraduateDegrees,
-  getUndergraduateDegreeTypes,
-  getUndergraduatePrograms,
-  getUndergraduateProgramTypes,
-} from "@/data/yaml/programs/undergraduate";
+import { getUndergraduateDegrees, getUndergraduateDegreeTypes } from "@/data/drupal/undergraduate-degree";
+import { getUndergraduatePrograms, getUndergraduateProgramTypes } from "@/data/drupal/undergraduate-program";
 import { Metadata } from "next";
+/*
+import { Card, CardContent, CardFooter, CardTitle } from "@uoguelph/react-components/card";
+import { UndergraduateDegreeFragment, UndergraduateProgramFragment } from "@/lib/graphql/types";
+
+type CombinedUndergraduateProgram = UndergraduateProgramFragment | UndergraduateDegreeFragment
+
+const ProgramCard = ({ program }: { program: CombinedUndergraduateProgram }) => {
+  if(!program.url.url) {
+
+  }
+
+  return (
+    <Card as="a" key={program.id} href={program.url.url} className="h-full hover:scale-105 transition-transform">
+      <CardContent className="flex-1 flex justify-center">
+        <CardTitle>{program.name}</CardTitle>
+
+        {program.degree && <span className="text-black/65">{program.degree.name}</span>}
+
+        {program.degrees && (
+          <span className="text-black/65">
+            {program.degrees?.map((degree) => degree.acronym ?? degree.name).join(", ")}
+          </span>
+        )}
+      </CardContent>
+
+      <CardFooter>
+        <span className="overflow-hidden text-ellipsis whitespace-nowrap">
+          {program?.types?.map((type) => type.name).join(", ")}
+        </span>
+      </CardFooter>
+    </Card>
+  );
+};
+*/
 
 export const metadata: Metadata = {
   title: "Undergraduate Programs | University of Guelph",
@@ -19,18 +49,11 @@ export const metadata: Metadata = {
 
 export default async function ProgramsUndergraduate() {
   const degreeTypes = await getUndergraduateDegreeTypes();
-  const degrees = (await getUndergraduateDegrees()).map((degree: { type: any }) => ({
-    ...degree,
-    types: [degree.type],
-  }));
+  const degrees = await getUndergraduateDegrees();
   const programTypes = await getUndergraduateProgramTypes();
   const programs = await getUndergraduatePrograms();
 
-  const combined = [...programs, ...degrees]
-    .map((program) => {
-      return { ...program, requirements: null, "alternative-offers": null };
-    })
-    .sort((a, b) => a.name.localeCompare(b.name));
+  const combined = [...programs, ...degrees].sort((a, b) => a.title.localeCompare(b.title));
   const combinedTypes = [...programTypes, ...degreeTypes];
 
   return (
@@ -44,7 +67,7 @@ export default async function ProgramsUndergraduate() {
           </Typography>
         </Container>
 
-        <ProgramSearch programs={combined} types={combinedTypes} degreeTypes={undefined} />
+        {JSON.stringify(combined)}
       </LayoutContent>
 
       <Footer></Footer>
