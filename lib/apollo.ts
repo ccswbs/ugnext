4bff9f8b6e810d3d421057ce202dd9be9bf743dd
Raza Mahmood <razammahmood@gmail.com>
2025-07-15 20:18:25 -0400

    Switch Apollo Client link to `BatchHttpLink` for improved request batching

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,13 +1,14 @@
 import { HttpLink } from "@apollo/client";
 import { registerApolloClient, ApolloClient, InMemoryCache } from "@apollo/client-integration-nextjs";
 import { showUnpublishedContent } from "@/lib/show-unpublished-content";
+import { BatchHttpLink } from "@apollo/client/link/batch-http";
 
 const DRUPAL_BASE_URL = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL ?? "https://api.liveugconthub.uoguelph.dev";
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
+    link: new BatchHttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
       uri: `${DRUPAL_BASE_URL}/graphql`,
       headers: {
