e53223af04e2fa6c92bcb8c4d4ea6b9c4d372e7e
Raza Mahmood <razammahmood@gmail.com>
2025-07-22 20:54:16 -0400

    Refactor `ProgramGrid` to use `Grid` component from `@uoguelph/react-components` for improved layout and styling consistency.

--- components/client/programs/program-grid.js
+++ components/client/programs/program-grid.js
@@ -1,11 +1,27 @@
 import { ProgramCard } from "./program-card";
+import { Grid } from "@uoguelph/react-components/grid";
 
 export const ProgramGrid = ({ programs, condensedDegrees = false }) => {
   return (
-    <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+    <Grid
+      template={{
+        base: ["minmax(0, 1fr)"],
+        md: Array(2).fill("minmax(0, 1fr)"),
+        lg: Array(3).fill("minmax(0, 1fr)"),
+        xl: Array(4).fill("minmax(0, 1fr)"),
+      }}
+      gap={{
+        x: 14,
+        y: 14,
+      }}
+      alignment={{
+        x: "stretch",
+        y: "stretch",
+      }}
+    >
       {programs?.map((program) => (
         <ProgramCard key={program.id} program={program} condensedDegrees={condensedDegrees} />
       ))}
-    </div>
+    </Grid>
   );
 };
