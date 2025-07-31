22efe64b3a6097a32c0e7c440b468826af53609a
Raza Mahmood <razammahmood@gmail.com>
2025-07-31 12:55:25 -0400

    Adjust form layout to use `w-full` for improved spacing in admission requirements form.

--- components/client/programs/undergraduate/admission-requirements-form.tsx
+++ components/client/programs/undergraduate/admission-requirements-form.tsx
@@ -86,7 +86,7 @@ export default function AdmissionRequirementsForm({
 
   return (
     <form
-      className="w-3/4 flex flex-col"
+      className="w-full flex flex-col"
       onSubmit={() => {
         if (url) {
           router.push(url);
