6693836000228a41575563f83b9441f56c50c229
Raza Mahmood <razammahmood@gmail.com>
2025-08-01 00:30:30 -0400

    Refactor Drupal client initialization to use `api-key` header and simplify base URL handling

--- lib/drupal.ts
+++ lib/drupal.ts
@@ -1,14 +1,8 @@
 import { NextDrupal } from "next-drupal";
 
-const baseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL as string;
-const clientId = process.env.DRUPAL_CLIENT_ID as string;
-const clientSecret = process.env.DRUPAL_CLIENT_SECRET as string;
-
-export const drupal = new NextDrupal(baseUrl, {
-  auth: {
-    clientId,
-    clientSecret,
+export const drupal = new NextDrupal((process.env.NEXT_PUBLIC_DRUPAL_BASE_URL as string).replace(/\/$/, ""), {
+  headers: {
+    "api-key": process.env.DRUPAL_API_KEY ?? "",
   },
-  withAuth: true,
   debug: process.env.NODE_ENV === "development",
 });
