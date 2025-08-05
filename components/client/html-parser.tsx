61f1a52117556e111d707e760fe52da8e37da39f
Raza Mahmood <razammahmood@gmail.com>
2025-08-05 16:24:51 -0400

    Refactor `HtmlParser` for improved configurability and simplify node parsing logic with custom instructions.

--- components/client/html-parser.tsx
+++ components/client/html-parser.tsx
@@ -1,29 +1,17 @@
 "use client";
 
 import parse, { HTMLReactParserOptions, Element, attributesToProps, domToReact, type DOMNode } from "html-react-parser";
-import { useMemo } from "react";
+import React, { useMemo } from "react";
 import { nanoid } from "nanoid";
 import { Typography } from "@uoguelph/react-components/typography";
 import { twMerge } from "tailwind-merge";
-import { Divider } from "@uoguelph/react-components/divider";
 import Script from "next/script";
 
-const selfClosingTags = new Set([
-  "area",
-  "base",
-  "br",
-  "col",
-  "embed",
-  "hr",
-  "img",
-  "input",
-  "link",
-  "meta",
-  "param",
-  "source",
-  "track",
-  "wbr",
-]);
+type ParserNodeProps = ReturnType<typeof attributesToProps>;
+type ParserInstruction = {
+  shouldParse: (node: Element, props: ParserNodeProps, index: number) => boolean;
+  parse: (node: Element, props: ParserNodeProps, index: number, options: HTMLReactParserOptions) => React.JSX.Element;
+};
 
 function isElement(domNode: DOMNode): domNode is Element {
   const isTag = ["tag", "script"].includes(domNode.type);
@@ -32,83 +20,80 @@ function isElement(domNode: DOMNode): domNode is Element {
   return isTag && hasAttributes;
 }
 
-type ParsedProps = ReturnType<typeof attributesToProps>;
+const defaultInstructions: ParserInstruction[] = [
+  {
+    shouldParse: (node) =>
+      node.tagName === "h1" ||
+      node.tagName === "h2" ||
+      node.tagName === "h3" ||
+      node.tagName === "h4" ||
+      node.tagName === "h5" ||
+      node.tagName === "h6",
+    parse: (node, props, index, options) => {
+      const level = node.tagName as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+      const className = typeof props.className === "string" ? props.className : "";
 
-function parseHeading(node: Element, props: ParsedProps, index: number) {
-  const level = node.tagName as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-  const className = typeof props.className === "string" ? props.className : "";
-
-  return (
-    <Typography {...props} key={nanoid()} type={level} as={level} className={twMerge(index === 0 && "mt-0", className)}>
-      {domToReact(node.children as DOMNode[], options)}
-    </Typography>
-  );
-}
-
-function parseParagraph(node: Element, props: ParsedProps, index: number) {
-  return (
-    <Typography {...props} key={nanoid()} type="body" as="p">
-      {domToReact(node.children as DOMNode[], options)}
-    </Typography>
-  );
-}
-
-function parseScript(node: Element, props: ParsedProps, index: number) {
-  if (typeof props.src !== "string" || props.src === "") {
-    return <></>;
-  }
-
-  if (typeof props.type !== "string") {
-    return <></>;
-  }
-
-  return <Script src={props.src} type={props.type} strategy="lazyOnload" />;
-}
-
-const options: HTMLReactParserOptions = {
-  replace: (node, index) => {
-    if (!isElement(node)) {
-      return;
-    }
-
-    const props = attributesToProps(node.attribs);
-
-    // Remove bad props
-    delete props.style;
-    delete props.key;
-    delete props.dangerouslySetInnerHTML;
-
-    // Custom renderers
-    switch (node.name) {
-      case "h1":
-      case "h2":
-      case "h3":
-      case "h4":
-      case "h5":
-      case "h6":
-        return parseHeading(node, props, index);
-      case "p":
-        return parseParagraph(node, props, index);
-      case "hr":
-        return <Divider key={nanoid()} />;
-      case "script":
-        return parseScript(node, props, index);
-    }
-
-    // Fallback renderer for nodes that are self-closing and as a result cannot have children passed to them
-    if (selfClosingTags.has(node.name)) {
-      return <node.name key={nanoid()} {...props} />;
-    }
-
-    // Fallback renderer for nodes that can have children passed to them
-    // @ts-ignore
-    return <node.name key={nanoid()}>{domToReact(node.children as DOMNode[], options)}</node.name>;
+      return (
+        <Typography
+          {...props}
+          key={nanoid()}
+          type={level}
+          as={level}
+          className={twMerge(index === 0 && "mt-0", className)}
+        >
+          {domToReact(node.children as DOMNode[], options)}
+        </Typography>
+      );
+    },
   },
-  trim: true,
-};
+  {
+    shouldParse: (node) => node.tagName === "p",
+    parse: (node, props, index, options) => (
+      <Typography {...props} key={nanoid()} type="body" as="p">
+        {domToReact(node.children as DOMNode[], options)}
+      </Typography>
+    ),
+  },
+  {
+    shouldParse: (node) => node.tagName === "script",
+    parse: (node, props, index, options) => {
+      if (typeof props.src !== "string" || props.src === "") {
+        return <></>;
+      }
 
-export function HtmlParser({ html }: { html: string }) {
-  const content = useMemo(() => parse(html, options), [html]);
+      if (typeof props.type !== "string") {
+        return <></>;
+      }
+
+      return <Script src={props.src} type={props.type} strategy="lazyOnload" />;
+    },
+  },
+];
+
+export function HtmlParser({ html, instructions = [] }: { html: string; instructions: ParserInstruction[] }) {
+  const options: HTMLReactParserOptions = useMemo(() => {
+    return {
+      replace: (node, index) => {
+        if (!isElement(node)) {
+          return;
+        }
+
+        const props = attributesToProps(node.attribs);
+
+        // Remove bad props
+        delete props.style;
+        delete props.key;
+        delete props.dangerouslySetInnerHTML;
+
+        for (const instruction of [...instructions, ...defaultInstructions]) {
+          if (instruction.shouldParse(node, props, index)) return instruction.parse(node, props, index, options);
+        }
+      },
+      trim: true,
+    };
+  }, [instructions]);
+
+  const content = useMemo(() => parse(html, options), [html, options]);
 
   return <>{content}</>;
 }
