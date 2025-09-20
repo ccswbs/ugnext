f88f8ee9056f5c408deedfe310bd8d91a2161aa7
Miranda Mafe <miranda.mafe@gmail.com>
2025-09-19 22:23:00 -0400

    Update inline input elements

--- components/client/html-parser.tsx
+++ components/client/html-parser.tsx
@@ -6,6 +6,7 @@ import { nanoid } from "nanoid";
 import { Typography } from "@uoguelph/react-components/typography";
 import { twMerge } from "tailwind-merge";
 import Script from "next/script";
+import { Input } from '@headlessui/react';
 import { Info } from "@uoguelph/react-components/info";
 import { Button, type ButtonProps } from "@uoguelph/react-components/button";
 import { Link } from "@uoguelph/react-components/link";
@@ -385,6 +386,27 @@ const defaultInstructions: ParserInstruction[] = [
         </figure>
       );
     },
+  },
+  // Input
+  {
+    shouldProcessNode: (node, props) => node.tagName === "input",
+    processNode: (node, props) => {
+      let inputWrapperClasses = "uofg-text-input-wrapper text-input flex rounded-md border border-grey-light bg-white px-4 py-2 transition-colors focus-within:border-blue focus:outline-none";
+      if(props.type === "submit"){
+        return <Button
+            {...props}
+            key={nanoid()}
+            color={"red"}
+            as={Input} />
+      }
+
+        // @todo - consider using TextInput in future(currently gives warning due to div being in a paragraph)
+        return (
+          <span className={twMerge(inputWrapperClasses, "mb-4")}>
+            <Input key={nanoid()} className={"uofg-text-input flex-1 bg-white focus:outline-none"} {...props} />
+          </span>
+        )
+    }
   },
   // Containers
   {
