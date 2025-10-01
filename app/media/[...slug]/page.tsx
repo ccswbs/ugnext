ea5c60290a5a858ba3945aa8cecb3215210dd0ac
Raza Mahmood <razammahmood@gmail.com>
2025-10-01 11:20:45 -0400

    Update media slug parameter to support arrays in `MediaPage` and `getMediaPathById`.

--- app/media/[...slug]/page.tsx
+++ app/media/[...slug]/page.tsx
@@ -2,12 +2,12 @@ import { notFound, permanentRedirect } from "next/navigation";
 import { getMediaPathById } from "@/data/drupal/media";
 
 type Props = {
-  params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string[] }>;
 };
 
 export default async function MediaPage({ params }: Props) {
   const { slug } = await params;
-  const path = await getMediaPathById(slug);
+  const path = await getMediaPathById(slug[0]);
 
   if (!path) {
     notFound();
