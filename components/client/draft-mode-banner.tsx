39b7bfb6f196235a91dbd902ded25fa4975062ac
Raza Mahmood <razammahmood@gmail.com>
2025-10-08 16:38:57 -0400

    Add "Share Page" button to `DraftModeBanner` with generated shareable link

--- components/client/draft-mode-banner.tsx
+++ components/client/draft-mode-banner.tsx
@@ -5,6 +5,9 @@ import { usePathname } from "next/navigation";
 
 export function DraftModeBanner() {
   const pathname = usePathname();
+  const searchParams = new URLSearchParams(window.location.search);
+  const shareableLink = `${window.location.origin}${pathname}?${searchParams.toString()}`;
+
   return (
     <div className="sticky left-0 top-0 z-1000 flex h-fit w-full items-center justify-center gap-2 bg-red p-2 text-center text-base font-bold text-white">
       <span>You are currently in Draft Mode.</span>
@@ -15,6 +18,10 @@ export function DraftModeBanner() {
 
       <Button color="yellow" className="p-2" href={`/api/revalidate?path=${pathname}`} as="a">
         Revalidate Page
+      </Button>
+
+      <Button color="yellow" className="p-2" href={shareableLink} as="a">
+        Share Page
       </Button>
     </div>
   );
