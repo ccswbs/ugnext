51df2ec7b562ecbe6282316cb570f5d778cc6231
Raza Mahmood <razammahmood@gmail.com>
2025-10-17 15:26:16 -0400

    Refactor `ProfileTabs` to use `ProfileType` instead of `ProfileCategory` for parameter and type consistency. Removed redundant "All" navigation link.

--- components/client/profiles/profile-tabs.tsx
+++ components/client/profiles/profile-tabs.tsx
@@ -1,22 +1,18 @@
 "use client";
 
-import type { ProfileCategory } from "@/data/drupal/profile";
+import type { ProfileType } from "@/data/drupal/profile";
 import { Navigation, NavigationLink } from "@uoguelph/react-components/navigation";
 import Link from "next/link";
 import { Container } from "@uoguelph/react-components/container";
 import { usePathname } from "next/navigation";
 
-export function ProfileTabs({ types }: { types: ProfileCategory[] }) {
+export function ProfileTabs({ types }: { types: ProfileType[] }) {
   const pathname = usePathname();
   const activeType = pathname.split("/").pop();
 
   return (
     <Container className="pb-0 mb-0">
       <Navigation>
-        <NavigationLink href="/people" as={Link} active={activeType === "people" || !activeType}>
-          All
-        </NavigationLink>
-
         {types.map((type) => {
           const typeSlug = (type.path ?? "").split("/").pop();
           const href = `/people/${(type.path ?? "").split("/").pop()}`;
