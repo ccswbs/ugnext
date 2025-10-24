8175aeb315dc20298960f131cbedf2ca16414285
Raza Mahmood <razammahmood@gmail.com>
2025-10-24 14:24:03 -0400

    Remove unused packages from `bun.lock` to optimize dependencies and improve maintainability.

---
+++ scripts/prebuild.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import { cp } from "fs/promises";
+
+async function copyFiles(from: string, to: string) {
+  try {
+    await cp(from, to, { recursive: true, force: true });
+  } catch (error) {
+    throw error;
+  }
+}
+
+const cwd = process.cwd();
+const from = path.resolve(cwd, "node_modules", "@uoguelph", "web-components", "dist", "uofg-web-components");
+const to = path.resolve(cwd, "public", "@uoguelph", "web-components");
+
+console.log(`Copying web components library to Next public folder`);
+
+try {
+  await copyFiles(from, to);
+} catch (error) {
+  console.error("Failed to copy web components library to public folder:", error);
+  process.exit(1);
+}
