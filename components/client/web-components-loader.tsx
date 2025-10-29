2dc233be605404ab2b0929f67f5302e2642c4926
Raza Mahmood <razammahmood@gmail.com>
2025-10-29 13:49:35 -0400

    Refactor: Introduce `WebComponentsLoader` component for loading web components and replace inline scripts in `layout.tsx`

---
+++ components/client/web-components-loader.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Script from "next/script";
+import React from "react";
+
+export type WebComponentsLoaderProps = {
+  cdn: "jsdelivr" | "unpkg" | "local";
+  native: boolean;
+};
+
+export function WebComponentsLoader({ cdn, native }: WebComponentsLoaderProps) {
+  let base = "";
+
+  switch (cdn) {
+    case "jsdelivr":
+      base = "https://cdn.jsdelivr.net/npm/@uoguelph/web-components@2.x.x/dist/uofg-web-components";
+      break;
+    case "unpkg":
+      base = "https://unpkg.com/@uoguelph/web-components@2.x.x/dist/uofg-web-components";
+      break;
+    case "local":
+      base = "/@uoguelph/web-components";
+      break;
+  }
+
+  const Component = native ? "script" : Script;
+
+  return (
+    <>
+      <Component src={`${base}/uofg-header.esm.js`} type="module" strategy="afterInteractive" />
+      <Component src={`${base}/uofg-footer.esm.js`} type="module" strategy="afterInteractive" />
+    </>
+  );
+}
