399e8001f66effeb9f4cfbf68355bd973a495c39
Raza Mahmood <razammahmood@gmail.com>
2025-12-08 09:53:32 -0500

    Refactor `AccordionWidget` to add unique `id` using `slugify` for section titles and improve heading element handling.

--- components/client/widgets/accordions.tsx
+++ components/client/widgets/accordions.tsx
@@ -1,17 +1,22 @@
 import { Accordion, AccordionButton, AccordionContent } from "@uoguelph/react-components/accordion";
 import { HtmlParser } from "@/components/client/html-parser";
-import { getHeadingLevel } from "@/lib/string-utils";
+import { getHeadingLevel, slugify } from "@/lib/string-utils";
 import { Typography, TypographyProps } from "@uoguelph/react-components/typography";
 import type { AccordionFragment } from "@/lib/graphql/types";
 
 export function AccordionWidget({ data }: { data: AccordionFragment }) {
   const level = data.headingLevel ? (getHeadingLevel(data.headingLevel) ?? 3) : 2;
   const sectionTitle = data?.accordionSectionTitle;
+  const id = sectionTitle ? slugify(sectionTitle) : `accordions-heading-${data.uuid}`;
 
   return (
     <>
       {sectionTitle && (
-        <Typography id={`accordions-heading-${data.uuid}`} type={`h${level}` as TypographyProps<any>["type"]}>
+        <Typography
+          id={id}
+          type={`h${level}` as TypographyProps<any>["type"]}
+          as={`h${level}` as TypographyProps<any>["as"]}
+        >
           {sectionTitle}
         </Typography>
       )}
