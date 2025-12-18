6334713b1dd20ee2558abf4ef514fe635ed3d63c
abanuog <abanerji@uoguelph.ca>
2025-12-18 15:51:58 -0500

    Update GeneralTextWidget styling for better layout

--- components/client/widgets/general-text.tsx
+++ components/client/widgets/general-text.tsx
@@ -2,7 +2,7 @@ import { HtmlParser } from "@/components/client/html-parser";
 import type { GeneralTextFragment } from "@/lib/graphql/types";
 
 export const GeneralTextWidget = ({ data }: { data: GeneralTextFragment }) => (
-  <div id={`general-text-${data.uuid}`} className="contents">
+  <div id={`general-text-${data.uuid}`} className="contents [&>ul]:columns-1 [&>ul]:md:columns-3 [&>ul]:gap-[1.5rem] [&>ul>li]:break-inside-avoid">
     <HtmlParser key={data.id} html={data.body.processed} />
   </div>
 );
