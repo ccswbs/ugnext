1ecd08a2455d8708ac87f9b2e34026719ea8ef4b
RazaMM <razammahmood@gmail.com>
2026-01-30 01:26:12 -0500

    Integrate `Layout`, `Header`, `Footer`, and `Breadcrumbs` into news page and improve unit-based rendering.

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -2,6 +2,14 @@ import { notFound } from "next/navigation";
 import { getRoute } from "@/data/drupal/route";
 import { News } from "@/components/server/news";
 import { getUnitBySlugifiedName } from "@/data/drupal/taxonomy";
+import { Header } from "@/components/server/header";
+import React from "react";
+import { Layout, LayoutContent } from "@uoguelph/react-components/layout";
+import { Footer } from "@uoguelph/react-components/footer";
+import { Typography } from "@uoguelph/react-components/typography";
+import { CustomFooter } from "@/components/server/custom-footer";
+import { Breadcrumb, BreadcrumbHome, Breadcrumbs } from "@uoguelph/react-components/breadcrumbs";
+import Link from "next/link";
 
 type Props = {
   params: Promise<{ slug: string }>;
@@ -12,5 +20,26 @@ export default async function NewsArticle({ params }: Props) {
 
   const unit = await getUnitBySlugifiedName(unitName);
 
-  return <>{JSON.stringify(unit)}</>;
+  if (!unit) notFound();
+
+  return (
+    <Layout>
+      <Header></Header>
+
+      <LayoutContent>
+        <Breadcrumbs>
+          <BreadcrumbHome />
+          <Breadcrumb as={"span"}>{unit.name}</Breadcrumb>
+        </Breadcrumbs>
+
+        <Typography type="h1" as="h1">
+          {unit.name} News
+        </Typography>
+      </LayoutContent>
+
+      <CustomFooter tags={[]} units={[unit.id]} />
+
+      <Footer></Footer>
+    </Layout>
+  );
 }
