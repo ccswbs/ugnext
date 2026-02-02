bd498c540b2ba25f1b3fa49941df7c6d24af58a5
RazaMM <razammahmood@gmail.com>
2026-02-02 15:31:34 -0500

    Refactor `getRoute` to use fragments for better GraphQL query modularity, clean up code structure, and improve breadcrumb handling logic.

--- data/drupal/route.ts
+++ data/drupal/route.ts
@@ -7,142 +7,157 @@ import { Link, RouteEntityUnion } from "@/lib/graphql/graphql";
 
 export type Route = NonNullable<RouteQuery["route"]>;
 
-export async function getRoute(url: string) {
-  const showUnpublished = await showUnpublishedContent();
-  const client = getClient();
-  const routeQuery = gql(/* gql */ `
-    query Route($path: String!, $revision: ID = "current") {
-      route(path: $path, revision: $revision) {
-        __typename
-        ... on RouteInternal {
-          entity {
-            __typename
-            ... on NodeArticle {
-              uuid
-              id
-              title
-            }
-            ... on NodeCallToAction {
-              uuid
-              id
-              title
-            }
-            ... on NodeCareer {
-              uuid
-              id
-              title
-            }
-            ... on NodeCourse {
-              uuid
-              id
-              title
-            }
-            ... on NodeCustomFooter {
-              uuid
-              id
-              title
-            }
-            ... on NodeEmployer {
-              uuid
-              id
-              title
-            }
-            ... on NodeEvent {
-              uuid
-              id
-              title
-            }
-            ... on NodePage {
-              uuid
-              id
-              title
-            }
-            ... on NodeProfile {
-              uuid
-              id
-              title
-            }
-            ... on NodeSpotlight {
-              uuid
-              id
-              title
-            }
-            ... on NodeTestimonial {
-              uuid
-              id
-              title
-            }
-            ... on NodeUserDocumentation {
-              uuid
-              id
-              title
-            }
-            ... on NodeUndergraduateProgram {
-              uuid
-              id
-              title
-            }
-            ... on NodeUndergraduateDegree {
-              uuid
-              id
-              title
-            }
-            ... on TermUndergraduateStudentType {
-              uuid
-              id
-              name
-            }
-            ... on TermAdmissionLocation {
-              uuid
-              id
-              name
-              weight
-            }
-            ... on NodeUndergraduateRequirement {
-              uuid
-              id
-              title
-            }
-            ... on TermProfileType {
-              uuid
-              id
-              name
-            }
-          }
-        }
-        ... on RouteRedirect {
-          status
-          url
+const METATAGS_VALUE_FRAGMENT = gql(/* gql */ `
+  fragment MetaValue on MetaTagValue {
+    __typename
+    tag
+    attributes {
+      name
+      content
+    }
+  }
+`);
+
+const METATAGS_PROPERTY_FRAGMENT = gql(/* gql */ `
+  fragment MetaProperty on MetaTagProperty {
+    __typename
+    tag
+    attributes {
+      property
+      content
+    }
+  }
+`);
+
+const ROUTE_INTERNAL_FRAGMENT = gql(/* gql */ `
+  fragment InternalRoute on RouteInternal {
+    entity {
+      __typename
+      ... on NodeArticle {
+        uuid
+        id
+        title
+      }
+      ... on NodeCallToAction {
+        uuid
+        id
+        title
+      }
+      ... on NodeCareer {
+        uuid
+        id
+        title
+      }
+      ... on NodeCourse {
+        uuid
+        id
+        title
+      }
+      ... on NodeCustomFooter {
+        uuid
+        id
+        title
+      }
+      ... on NodeEmployer {
+        uuid
+        id
+        title
+      }
+      ... on NodeEvent {
+        uuid
+        id
+        title
+      }
+      ... on NodePage {
+        uuid
+        id
+        title
+        metatag {
+          __typename
+          ...MetaValue
+          ...MetaProperty
         }
       }
+      ... on NodeProfile {
+        uuid
+        id
+        title
+      }
+      ... on NodeSpotlight {
+        uuid
+        id
+        title
+      }
+      ... on NodeTestimonial {
+        uuid
+        id
+        title
+      }
+      ... on NodeUserDocumentation {
+        uuid
+        id
+        title
+      }
+      ... on NodeUndergraduateProgram {
+        uuid
+        id
+        title
+      }
+      ... on NodeUndergraduateDegree {
+        uuid
+        id
+        title
+      }
+      ... on TermUndergraduateStudentType {
+        uuid
+        id
+        name
+      }
+      ... on TermAdmissionLocation {
+        uuid
+        id
+        name
+        weight
+      }
+      ... on NodeUndergraduateRequirement {
+        uuid
+        id
+        title
+      }
+      ... on TermProfileType {
+        uuid
+        id
+        name
+      }
     }
-  `);
-
-  const { data, error } = await client.query({
-    query: routeQuery,
-    variables: {
-      path: url,
-      revision: showUnpublished ? "latest" : "current",
-    },
-  });
-
-  if (error) {
-    handleGraphQLError(error);
   }
+`);
 
-  if (!data) {
-    return null;
+const ROUTE_REDIRECT_FRAGMENT = gql(/* gql */ `
+  fragment RedirectRoute on RouteRedirect {
+    status
+    url
   }
+`);
 
-  if (showUnpublished && data.route?.__typename === "RouteInternal" && data.route.entity === null) {
-    // For some reason, the route is found, but the entity is null.
-    // This only happens when we are looking for the latest revision and the entity has no revisions.
-    // We need to query again with the current revision.
+export async function getRoute(url: string) {
+  const client = getClient();
+  const showUnpublished = await showUnpublishedContent();
+
+  async function _getRoute(showUnpublished: boolean) {
     const { data, error } = await client.query({
-      query: routeQuery,
+      query: gql(/* gql */ `
+        query Route($path: String!, $revision: ID = "current") {
+          route(path: $path, revision: $revision) {
+            __typename
+            ...InternalRoute
+            ...RedirectRoute
+          }
+        }
+      `),
       variables: {
         path: url,
-        revision: "current",
+        revision: showUnpublished ? "latest" : "current",
       },
     });
 
@@ -154,27 +169,37 @@ export async function getRoute(url: string) {
       return null;
     }
 
-    return data.route;
+    // For some reason, if we are looking for the latest revision and the entity has no revisions, the route is found, but the entity is null.
+    // We need to query again with the current revision.
+    if (showUnpublished && data.route?.__typename === "RouteInternal" && data.route.entity === null) {
+      return _getRoute(false);
+    }
+
+    switch (data.route?.__typename) {
+      case "RouteInternal":
+      case "RouteRedirect":
+        return data?.route;
+      default:
+        return null;
+    }
   }
 
-  switch (data.route?.__typename) {
-    case "RouteInternal":
-    case "RouteRedirect":
-      return data?.route;
-    default:
-      return null;
-  }
+  return _getRoute(showUnpublished ?? false);
 }
 
-export async function checkIsEntityInMenu (entity_id: string, entity_path: string | null | undefined, primary_navigation: string | undefined) {
-  if(primary_navigation){
+export async function checkIsEntityInMenu(
+  entity_id: string,
+  entity_path: string | null | undefined,
+  primary_navigation: string | undefined
+) {
+  if (primary_navigation) {
     const entityNodeURL = entity_id ? `node/${entity_id}` : null;
     const checkMenuForEntityID = entityNodeURL ? await getMenuLinkByURI(entityNodeURL, primary_navigation) : null;
 
     // Check both node URL and path URL
-    if(!checkMenuForEntityID || checkMenuForEntityID?.length === 0){
+    if (!checkMenuForEntityID || checkMenuForEntityID?.length === 0) {
       const checkMenuForEntityPath = entity_path ? await getMenuLinkByURI(entity_path, primary_navigation) : null;
-      if(!checkMenuForEntityPath || checkMenuForEntityPath?.length === 0){
+      if (!checkMenuForEntityPath || checkMenuForEntityPath?.length === 0) {
         return false;
       }
     }
@@ -182,7 +207,7 @@ export async function checkIsEntityInMenu (entity_id: string, entity_path: strin
   return true;
 }
 
-function filterBreadcrumbs (breadcrumbs: Link[], currentPage: {title: string}) {
+function filterBreadcrumbs(breadcrumbs: Link[], currentPage: { title: string }) {
   // Filter out elements without titles and the root from Breadcrumb Path
   let breadcrumbPath = breadcrumbs.filter((breadcrumb) => {
     if (!breadcrumb.title) {
@@ -192,21 +217,20 @@ function filterBreadcrumbs (breadcrumbs: Link[], currentPage: {title: string}) {
   });
 
   // Remove last breadcrumb item if same as current page
-  if(breadcrumbPath.length > 0){
+  if (breadcrumbPath.length > 0) {
     const lastBreadcrumbItem = breadcrumbPath[breadcrumbPath.length - 1];
-    if((currentPage.title === lastBreadcrumbItem?.title) && (lastBreadcrumbItem?.url === '')){
-      if(breadcrumbPath.length > 1){
+    if (currentPage.title === lastBreadcrumbItem?.title && lastBreadcrumbItem?.url === "") {
+      if (breadcrumbPath.length > 1) {
         // pop returns undefined if only one item
-        breadcrumbPath.pop(); 
-      }else{
+        breadcrumbPath.pop();
+      } else {
         breadcrumbPath = [];
       }
-    } 
+    }
   }
 
   return breadcrumbPath;
 }
-
 
 // Route assumes we are using Menu-generated Breadcrumbs instead of Path-based Breadcrumbs
 export async function getRouteBreadcrumbs(url: string, primary_navigation: string | undefined) {
@@ -301,13 +325,13 @@ export async function getRouteBreadcrumbs(url: string, primary_navigation: strin
       };
 
       if (!Array.isArray(data.route.breadcrumbs)) {
-        return [ currentPage ];
+        return [currentPage];
       }
 
       let breadcrumbPath = filterBreadcrumbs(data.route.breadcrumbs, currentPage);
 
       /* ---- Handle Basic Pages with Primary Navigation Homepage URL --- */
-      if(data.route.entity.__typename === "NodePage" && data.route.entity.primaryNavigation?.primaryNavigationUrl) {
+      if (data.route.entity.__typename === "NodePage" && data.route.entity.primaryNavigation?.primaryNavigationUrl) {
         const primaryNavigationHome = {
           title: data.route.entity.primaryNavigation?.primaryNavigationUrl?.title,
           url: data.route.entity.primaryNavigation?.primaryNavigationUrl?.url,
@@ -315,46 +339,36 @@ export async function getRouteBreadcrumbs(url: string, primary_navigation: strin
 
         /* ---- Handle pages that are NOT in the menu --- */
         // If page NOT in menu, return [Primary Nav Home > currentPage]
-        const isPageInMenu = await checkIsEntityInMenu(data.route.entity.id, data.route.entity.path, primary_navigation); 
-        if(!isPageInMenu){
-          return [
-            primaryNavigationHome,
-            currentPage,
-          ];
+        const isPageInMenu = await checkIsEntityInMenu(
+          data.route.entity.id,
+          data.route.entity.path,
+          primary_navigation
+        );
+        if (!isPageInMenu) {
+          return [primaryNavigationHome, currentPage];
         }
 
         // Only add Primary Nav Homepage URL if NOT already at start of breadcrumbPath
-        if (primaryNavigationHome && (breadcrumbPath[0]?.url !== primaryNavigationHome.url)){
-          
+        if (primaryNavigationHome && breadcrumbPath[0]?.url !== primaryNavigationHome.url) {
           // If Primary Nav Home and currentPage are SAME, only return [ Primary Nav Home ]
-          if(primaryNavigationHome.title === currentPage.title && breadcrumbPath.length === 0){
-            return [ primaryNavigationHome ];
+          if (primaryNavigationHome.title === currentPage.title && breadcrumbPath.length === 0) {
+            return [primaryNavigationHome];
           }
 
           /* ---- Handle pages in MULTIPLE MENUS --- */
           // Pages in multiple menus can have breadcrumb path from a different menu than Primary Navigation
           // If page in multiple menus, return [Primary Nav Home > currentPage]
-          if(data.route.entity.primaryNavigation?.menuName !== primary_navigation){  
-            return [
-              primaryNavigationHome,
-              currentPage,
-            ];
+          if (data.route.entity.primaryNavigation?.menuName !== primary_navigation) {
+            return [primaryNavigationHome, currentPage];
           }
 
           // Default return
-          return [
-            primaryNavigationHome,
-            ...breadcrumbPath,
-            currentPage,
-          ];
+          return [primaryNavigationHome, ...breadcrumbPath, currentPage];
         }
       }
 
       // Handle content without Primary Navigation
-      return [
-        ...breadcrumbPath,
-        currentPage,
-      ];
+      return [...breadcrumbPath, currentPage];
     default:
       return null;
   }
