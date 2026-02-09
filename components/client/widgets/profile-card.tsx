dfbbfdabbaa10d4d9db55a8a5583194a5bde5a76
Tanya Qureshi <tqureshi@uoguelph.ca>
2026-02-09 17:06:56 -0500

    Improve card formatting

--- components/client/widgets/profile-card.tsx
+++ components/client/widgets/profile-card.tsx
@@ -19,29 +19,50 @@ export const ProfileCard = ({ data }: { data: ProfileCardFragment }) => {
   }
 
   const sharedClassName =
-    "group block bg-grey-light-bg hover:shadow-lg transition-shadow duration-200 overflow-hidden h-full xl:w-[calc(45%-0.75rem)] xl:inline-block xl:align-top xl:mr-3 xl:mb-4";
+    `${profileInfo.path ? 'group ' : ''}block bg-grey-light-bg overflow-hidden h-full xl:w-[calc(45%-0.75rem)] xl:inline-block xl:align-top xl:mr-3 xl:mb-4`;
 
   const content = (
     <div className="flex flex-col md:flex-row h-full">
       {/* Image Section */}
       {profileInfo.profilePicture && (
         <div className="shrink-0 w-full md:w-48 lg:w-56 xl:w-1/3">
-          <Image
-            src={profileInfo.profilePicture.image.url}
-            alt={profileInfo.profilePicture.image.alt ?? ""}
-            width={400}
-            height={400}
-            sizes="(max-width: 768px) 100vw, (max-width: 1024px) 192px, (max-width: 1280px) 224px, 50vw"
-            className="aspect-square object-cover object-top w-full h-full md:h-full"
-          />
+          {profileInfo.path ? (
+            <Link href={profileInfo.path} className="block">
+              <Image
+                src={profileInfo.profilePicture.image.url}
+                alt={profileInfo.profilePicture.image.alt ?? ""}
+                width={400}
+                height={400}
+                sizes="(max-width: 768px) 100vw, (max-width: 1024px) 192px, (max-width: 1280px) 224px, 50vw"
+                className="aspect-square object-cover object-top w-full h-full md:h-full transition-transform duration-200 ease-in-out group-hover:scale-110"
+              />
+            </Link>
+          ) : (
+            <Image
+              src={profileInfo.profilePicture.image.url}
+              alt={profileInfo.profilePicture.image.alt ?? ""}
+              width={400}
+              height={400}
+              sizes="(max-width: 768px) 100vw, (max-width: 1024px) 192px, (max-width: 1280px) 224px, 50vw"
+              className="aspect-square object-cover object-top w-full h-full md:h-full"
+            />
+          )}
         </div>
       )}
 
       {/* Content Section */}
       <div className="flex-1 p-6 flex flex-col justify-center">
-        <Typography type="h3" as="p" className="m-0">
-          {profileInfo.title}
-        </Typography>
+        {profileInfo.path ? (
+          <Link href={profileInfo.path} className="block">
+            <Typography type="h3" as="p" className="m-0 hover:bg-yellow transition-colors duration-200">
+              {profileInfo.title}
+            </Typography>
+          </Link>
+        ) : (
+          <Typography type="h3" as="p" className="m-0">
+            {profileInfo.title}
+          </Typography>
+        )}
         {profileInfo.profileJobTitle && (
           <Typography type="h5" as="p" className="m-0">
             {profileInfo.profileJobTitle}
@@ -57,18 +78,6 @@ export const ProfileCard = ({ data }: { data: ProfileCardFragment }) => {
             directoryPhone={!!profileInfo.directoryPhone}
             className="mt-2"
           />
-        )}
-
-        {/* Profile Fields */}
-        {profileInfo.profileFields && profileInfo.profileFields.length > 0 && (
-          <div className="mt-2">
-            {profileInfo.profileFields.map((field, index) => (
-              <div key={index}>
-                <HtmlParser html={getDisplayText(field.label)} instructions={undefined} />
-                <HtmlParser html={getDisplayText(field.value)} instructions={undefined} />
-              </div>
-            ))}
-          </div>
         )}
 
         {/* Custom links if available */}
@@ -87,17 +96,21 @@ export const ProfileCard = ({ data }: { data: ProfileCardFragment }) => {
             )}
           </div>
         )}
+
+        {/* Profile Fields 
+        {profileInfo.profileFields && profileInfo.profileFields.length > 0 && (
+          <div className="mt-2">
+            {profileInfo.profileFields.map((field, index) => (
+              <div key={index}>
+                <HtmlParser html={getDisplayText(field.label)} instructions={undefined} />
+                <HtmlParser html={getDisplayText(field.value)} instructions={undefined} />
+              </div>
+            ))}
+          </div>
+        )}*/}
       </div>
     </div>
   );
-
-  if (profileInfo.path) {
-    return (
-      <Link href={profileInfo.path} className={sharedClassName}>
-        {content}
-      </Link>
-    );
-  }
 
   return (
     <div className="xl:after:content-[''] xl:after:display-table xl:after:clear-both">
