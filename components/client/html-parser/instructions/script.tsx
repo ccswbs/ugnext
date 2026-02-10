8485f47be5fc49f685c6679a747facfb441687e1
RazaMM <razammahmood@gmail.com>
2026-02-10 14:38:28 -0500

    Support inline script content in `ScriptInstruction` processing

--- components/client/html-parser/instructions/script.tsx
+++ components/client/html-parser/instructions/script.tsx
@@ -4,7 +4,17 @@ import { HTMLParserInstruction } from "@/components/client/html-parser";
 
 export const ScriptInstruction: HTMLParserInstruction = {
   shouldProcessNode: (node) => node.tagName === "script",
-  processNode: (node, props) => {
-    return <Script key={nanoid()} src={props.src as string} type={props.type as string} strategy="lazyOnload" />;
+  processNode: (node, props, children) => {
+    const id = nanoid();
+
+    if (!props.src && node.children[0]?.type === "text") {
+      return (
+        <Script id={id} key={id}>
+          {node.children[0].data}
+        </Script>
+      );
+    }
+
+    return <Script key={id} src={props.src as string} type={props.type as string} strategy="lazyOnload" />;
   },
 };
