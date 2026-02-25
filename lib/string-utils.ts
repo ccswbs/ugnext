41a7ad997d02fc43ba5c9add49200ef7bb361c8f
Tanya Qureshi <tqureshi@uoguelph.ca>
2026-02-24 23:20:44 -0500

    Add function for parsing tel links

--- lib/string-utils.ts
+++ lib/string-utils.ts
@@ -156,3 +156,19 @@ export const parsePhoneNumber = (phoneString: string) => {
     extension: undefined
   };
 };
+
+/**
+ * Parse a tel: URL to separate the main number from extension
+ * tel: URLs can use semicolon or 'p' to separate extensions (RFC 3966)
+ * @param telUrl - Tel URL string (e.g., "tel:+15198244120;ext=54019" or "tel:5198244120p54019")
+ * @returns Object with number and extension (if present)
+ */
+export const parseTelUrl = (telUrl: string) => {
+  const cleanUrl = telUrl.replace("tel:", "");
+  const tokens = cleanUrl.split(/[;p]/);
+  
+  return {
+    number: tokens[0] || "",
+    extension: tokens[1] || ""
+  };
+};
