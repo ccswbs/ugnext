70acc78ea7bdd6e4f2d370949d2f8b90cc9a0f0c
Raza (Ray) Mahmood <razammahmood@gmail.com>
2026-05-25 02:35:09 -0400

    Refactor `GraduateProgramSummary` component: simplify `classes` usage by directly accessing styles from `tv` object.

--- components/client/programs/graduate/graduate-program-summary.tsx
+++ components/client/programs/graduate/graduate-program-summary.tsx
@@ -133,76 +133,46 @@ function GraduateProgramDeadlines({ deadlines }: { deadlines: GraduateProgramApp
 }
 
 export function GraduateProgramSummary({ program }: { program: GraduateProgram }) {
-  const classes = tv({
-    slots: {
-      container: "p-4 w-full grid gap-4 grid-cols-1 md:grid-cols-3  bg-grey-dark-bg text-white",
-      column: "flex flex-col gap-4",
-      section: "flex flex-col gap-2",
-      sectionTitle: "text-yellow-on-dark font-bold text-xl",
-      sectionSubtitle: "font-bold pt-2",
-      sectionList: "flex flex-col [&>li]:not-first:has-[ul]:pt-2",
-    },
-  });
-
-  const { container, column, section, sectionTitle, sectionSubtitle, sectionList } = classes();
-
-  const deadlines = program.deadlines.reduce((acc, item) => {
-    const date = item.ongoing
-      ? "Ongoing"
-      : new Date(item.date?.timestamp ?? "").toLocaleDateString("en-US", {
-          month: "long",
-          day: "numeric",
-          year: item.date?.showYear ? "numeric" : undefined,
-        });
-
-    const value = `${toTitleCase(item.term)}: ${date}`;
-
-    const existing = acc.get(item.location) ?? [];
-    acc.set(item.location, [...existing, value]);
-
-    return acc;
-  }, new Map<string, string[]>());
-
   return (
-    <Container className={container()}>
-      <div className={column()}>
+    <Container className={classes.container()}>
+      <div className={classes.column()}>
         {/* Program Type Section */}
-        <div className={section()}>
-          <h2 className={sectionTitle()}>Program Type</h2>
+        <div className={classes.section()}>
+          <h2 className={classes.sectionTitle()}>Program Type</h2>
           <GraduateProgramTypes types={program.type} />
         </div>
 
         {/* Degree Section */}
-        <div className={section()}>
-          <h2 className={sectionTitle()}>Degree</h2>
+        <div className={classes.section()}>
+          <h2 className={classes.sectionTitle()}>Degree</h2>
           <GraduateDegree degree={program.degree} />
         </div>
 
         {/* Delivery Section*/}
-        <div className={section()}>
-          <h2 className={sectionTitle()}>Delivery</h2>
+        <div className={classes.section()}>
+          <h2 className={classes.sectionTitle()}>Delivery</h2>
           <GraduateProgramDelivery delivery={program.delivery} />
         </div>
       </div>
 
-      <div className={column()}>
+      <div className={classes.column()}>
         {/* Admission Average Section */}
-        <div className={section()}>
-          <h2 className={sectionTitle()}>Admission Average</h2>
+        <div className={classes.section()}>
+          <h2 className={classes.sectionTitle()}>Admission Average</h2>
           <GraduateProgramAdmissionAverage average={program.average} />
         </div>
 
         {/* Duration Section */}
-        <div className={section()}>
-          <h2 className={sectionTitle()}>Duration</h2>
+        <div className={classes.section()}>
+          <h2 className={classes.sectionTitle()}>Duration</h2>
           <GraduateProgramDuration duration={program.duration} />
         </div>
       </div>
 
-      <div className={column()}>
+      <div className={classes.column()}>
         {/* Deadlines & Entry Terms Section */}
-        <div className={section()}>
-          <h2 className={sectionTitle()}>Deadlines & Entry Terms</h2>
+        <div className={classes.section()}>
+          <h2 className={classes.sectionTitle()}>Deadlines & Entry Terms</h2>
           <GraduateProgramDeadlines deadlines={program.deadlines} />
         </div>
       </div>
