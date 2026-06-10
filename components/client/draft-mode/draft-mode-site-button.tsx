a9f9aa1a3b29e461d3913ba2c44c1e2174cd1eff
Raza (Ray) Mahmood <razammahmood@gmail.com>
2026-06-10 00:14:32 -0400

    Refactor `DraftModeSiteButton`: add toast notifications, loading state, and use button click for page revalidation

--- components/client/draft-mode/draft-mode-site-button.tsx
+++ components/client/draft-mode/draft-mode-site-button.tsx
@@ -4,6 +4,7 @@ import { createPortal } from "react-dom";
 import { Button } from "@uoguelph/react-components/button";
 import { useEffect, useState } from "react";
 import { getCacheTag } from "@/data/drupal/linked-revalidation";
+import { toast } from "@uoguelph/react-components/toaster";
 
 export function DraftModeSiteButton({
   primaryNavigation,
@@ -11,6 +12,21 @@ export function DraftModeSiteButton({
   primaryNavigation: { __typename: string; id: string; name: string };
 }) {
   const [containerExists, setContainerExists] = useState<boolean>(false);
+  const [revalidating, setRevalidating] = useState(false);
+  const tag = getCacheTag(primaryNavigation);
+
+  const revalidatePage = async () => {
+    setRevalidating(true);
+    const res = await fetch(`/api/revalidate?tags=${tag}`);
+
+    if (res.ok) {
+      toast.success(`Rebuilt all ${primaryNavigation.name} pages successfully!`);
+    } else {
+      toast.error("Failed to rebuild pages.");
+    }
+
+    setRevalidating(false);
+  };
 
   useEffect(() => {
     setContainerExists(Boolean(document.getElementById("uofg-draft-mode-banner-extra-buttons")));
@@ -20,7 +36,7 @@ export function DraftModeSiteButton({
     <>
       {containerExists &&
         createPortal(
-          <Button color="yellow" className="p-2" href={`/api/revalidate?tags=${getCacheTag(primaryNavigation)}`} as="a">
+          <Button color="yellow" className="p-2" onClick={revalidatePage} disabled={revalidating} as="button">
             Rebuild Site
           </Button>,
           // @ts-expect-error We check the element exists in the useEffect, so this will never be null but TypeScript doesn't know that
