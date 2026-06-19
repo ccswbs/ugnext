3ad79096d4e3cd486242dcf242a125f845e93d8b
Miranda Mafe <miranda.mafe@gmail.com>
2026-06-19 11:38:08 -0400

    Update entry term title and allow uppercase after certain delimiters

--- components/client/programs/graduate/graduate-program-summary.tsx
+++ components/client/programs/graduate/graduate-program-summary.tsx
@@ -223,7 +223,7 @@ export function GraduateProgramSummary({ program }: { program: GraduateProgramVa
         {/* Deadlines & Entry Terms Section */}
         {/* TO DO - if only entry term and no content, do not render anything */}
         <div className={classes.section()}>
-          <h2 className={classes.sectionTitle()}>Deadlines & Entry Terms</h2>
+          <h2 className={classes.sectionTitle()}>Entry Terms & Deadlines</h2>
           <GraduateProgramDeadlines deadlines={program.deadlines} />
         </div>
       </div>
