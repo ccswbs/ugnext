b9adf34b32e488a8651fffbb46e9eb1ad4a9a0c9
Miranda Mafe <miranda.mafe@gmail.com>
2026-06-25 15:00:46 -0400

    Revert all of app/api/revalidate

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,56 +1,44 @@
 import { revalidatePath, revalidateTag } from "next/cache";
 import type { NextRequest } from "next/server";
 import { getRoute } from "@/data/drupal/route";
-import { draftMode } from "next/headers";
-import { getPathsByEntity, getTagsToRevalidateByEntity } from "@/data/drupal/linked-revalidation";
 
 async function handler(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
+  const path = searchParams.get("path");
+  const tags = searchParams.get("tags");
   const secret = searchParams.get("secret");
-  const { isEnabled } = await draftMode();
 
-  // User must provide a valid secret or be in draft mode to revalidate.
-  // if (secret !== process.env.DRUPAL_REVALIDATE_SECRET && !isEnabled) {
-  //   return new Response("Invalid secret or you are not in draft mode.", { status: 401 });
-  // }
+  // Validate secret.
   if (secret !== process.env.DRUPAL_REVALIDATE_SECRET) {
     return new Response("Invalid secret.", { status: 401 });
   }
 
-  const pathParam = searchParams.get("path");
-  const tagsParam = searchParams.get("tags");
-
-  if (!pathParam && !tagsParam) {
+  // Either tags or path must be provided.
+  if (!path && !tags) {
     return new Response("Missing path or tags.", { status: 400 });
   }
 
-  const paths: string[] = [];
-  const tags: string[] = [];
-
-  if (pathParam) {
-    paths.push(pathParam);
-
-    const route = await getRoute(pathParam);
+  // Some node types might require some custom revalidation logic.
+  if (path) {
+    const route = await getRoute(path);
 
     if (route && route.__typename === "RouteInternal" && route.entity) {
-      tags.push(...getTagsToRevalidateByEntity(route.entity));
-      paths.push(...getPathsByEntity(route.entity));
+      switch (route.entity.__typename) {
+        case "NodeUndergraduateRequirement":
+          revalidatePath("/programs/undergraduate/requirements/[...slug]", "page");
+          break;
+      }
     }
   }
 
-  if (tagsParam) {
-    tags.push(...tagsParam.split(","));
-  }
+  try {
+    path && revalidatePath(path);
+    tags?.split(",").forEach((tag) => revalidateTag(tag, "max"));
 
-  for (const tag of tags) {
-    revalidateTag(tag, { expire: 0 });
+    return new Response("Revalidated.");
+  } catch (error) {
+    return new Response((error as Error).message, { status: 500 });
   }
-
-  for (const path of paths) {
-    revalidatePath(path, "page");
-  }
-
-  return Response.json({ tags, paths });
 }
 
 export { handler as GET, handler as POST };
