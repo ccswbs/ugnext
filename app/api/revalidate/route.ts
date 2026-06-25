433da2840fef4451b8d6b99e2e6b02ad125a7c90
Miranda Mafe <miranda.mafe@gmail.com>
2026-06-25 16:14:47 -0400

    Add commented out linked revalidation code

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,14 +1,15 @@
 import { revalidatePath, revalidateTag } from "next/cache";
 import type { NextRequest } from "next/server";
 import { getRoute } from "@/data/drupal/route";
+// import { draftMode } from "next/headers";
+// import { getPathsByEntity, getTagsToRevalidateByEntity } from "@/data/drupal/linked-revalidation";
 
 async function handler(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
+  const secret = searchParams.get("secret");
   const path = searchParams.get("path");
   const tags = searchParams.get("tags");
-  const secret = searchParams.get("secret");
 
-  // Validate secret.
   if (secret !== process.env.DRUPAL_REVALIDATE_SECRET) {
     return new Response("Invalid secret.", { status: 401 });
   }
@@ -39,6 +40,51 @@ async function handler(request: NextRequest) {
   } catch (error) {
     return new Response((error as Error).message, { status: 500 });
   }
+
+  // --- Code for LINKED REVALIDATION
+  // const searchParams = request.nextUrl.searchParams;
+  // const secret = searchParams.get("secret");
+  // const { isEnabled } = await draftMode();
+
+  // User must provide a valid secret or be in draft mode to revalidate.
+  // if (secret !== process.env.DRUPAL_REVALIDATE_SECRET && !isEnabled) {
+  //   return new Response("Invalid secret or you are not in draft mode.", { status: 401 });
+  // }
+
+  // const pathParam = searchParams.get("path");
+  // const tagsParam = searchParams.get("tags");
+
+  // if (!pathParam && !tagsParam) {
+  //   return new Response("Missing path or tags.", { status: 400 });
+  // }
+
+  // const paths: string[] = [];
+  // const tags: string[] = [];
+
+  // if (pathParam) {
+  //   paths.push(pathParam);
+
+  //   const route = await getRoute(pathParam);
+
+  //   if (route && route.__typename === "RouteInternal" && route.entity) {
+  //     tags.push(...getTagsToRevalidateByEntity(route.entity));
+  //     paths.push(...getPathsByEntity(route.entity));
+  //   }
+  // }
+
+  // if (tagsParam) {
+  //   tags.push(...tagsParam.split(","));
+  // }
+
+  // for (const tag of tags) {
+  //   revalidateTag(tag, { expire: 0 });
+  // }
+
+  // for (const path of paths) {
+  //   revalidatePath(path, "page");
+  // }
+
+  // return Response.json({ tags, paths });
 }
 
 export { handler as GET, handler as POST };
