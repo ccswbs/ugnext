b0dcc76214539d2dba30d36ee1db1ea3829ab7ad
Raza (Ray) Mahmood <razammahmood@gmail.com>
2026-07-07 13:30:52 -0400

    Add request logging for draft mode enablement in `route.ts`.

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -3,6 +3,10 @@ import { enableDraftMode } from "next-drupal/draft";
 import type { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest): Promise<Response | never> {
+  const searchParams = request.nextUrl.searchParams;
+  console.log("Attempt to enable draft mode");
+  console.log(searchParams.toString());
+
   // @ts-ignore
   return enableDraftMode(request, drupal);
 }
