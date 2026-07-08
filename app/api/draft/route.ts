51d1abd6f8f900486fa9ea811875673cdab02a93
Miranda Mafe <miranda.mafe@gmail.com>
2026-07-08 15:21:13 -0400

    Add comments in draft mode

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -7,13 +7,19 @@ import { redirect } from "next/navigation";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest): Promise<Response | never> {
+  console.log("DRAFT 1");
   const searchParams = request.nextUrl.searchParams;
+  console.log("DRAFT 2");
   const secret = searchParams.get("secret");
+  console.log("DRAFT 3");
   let path = searchParams.get("path");
+  console.log("DRAFT 4");
 
   // Manually enter draft mode if provided DRUPAL_PREVIEW_SECRET directly in the URL.
   if (secret === process.env.DRUPAL_PREVIEW_SECRET) {
+    console.log("DRAFT 5");
     if (!path) {
+      console.log("DRAFT 6");
       return NextResponse.json(
         {
           message: "Field 'path' is missing",
@@ -21,18 +27,23 @@ export async function GET(request: NextRequest): Promise<Response | never> {
         { status: 400 }
       );
     }
-
+  console.log("DRAFT 7");
     if (URL.canParse(path)) {
+      console.log("DRAFT 8");
       const url = new URL(path);
       path = url.pathname;
     }
 
+    console.log("DRAFT 9");
     const draft = await draftMode();
+    console.log("DRAFT 10");
     draft.enable();
 
+    console.log("DRAFT 11");
     redirect(path);
   }
 
+  console.log("DRAFT 12");
   // @ts-ignore
   return enableDraftMode(request, drupal);
 }
