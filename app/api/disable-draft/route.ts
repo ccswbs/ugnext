07c6e989c9163b52ff783bd093e472cc4fad64f2
Miranda Mafe <miranda.mafe@gmail.com>
2026-07-09 09:21:24 -0400

    Test draft mode

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -1,5 +1,6 @@
 import { disableDraftMode } from "next-drupal/draft";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from 'next/headers';
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
@@ -7,5 +8,17 @@ export async function GET(request: NextRequest) {
 
   await disableDraftMode();
 
+  (await cookies()).set({
+    name: "STYXKEY_draft",
+    value: "",
+    path: "/",
+    maxAge: 0,
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+
+
   return NextResponse.redirect(new URL(path, request.url));
 }
+
--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -1,35 +1,30 @@
-import { drupal } from "@/lib/drupal";
-import { enableDraftMode } from "next-drupal/draft";
-import type { NextRequest } from "next/server";
+// import { drupal } from "@/lib/drupal";
+// import { enableDraftMode } from "next-drupal/draft";
+// import type { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest): Promise<Response | never> {
-  // @ts-ignore
-  return enableDraftMode(request, drupal);
-}
+// export async function GET(request: NextRequest): Promise<Response | never> {
+//   // @ts-ignore
+//   return enableDraftMode(request, drupal);
+// }
 
-/*
+
 import { drupal } from "@/lib/drupal";
 import { enableDraftMode } from "next-drupal/draft";
 import { NextRequest, NextResponse } from "next/server";
 import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
+import { cookies } from 'next/headers';
 
 export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest): Promise<Response | never> {
-  console.log("DRAFT 1");
   const searchParams = request.nextUrl.searchParams;
-  console.log("DRAFT 2");
   const secret = searchParams.get("secret");
-  console.log("DRAFT 3");
   let path = searchParams.get("path");
-  console.log("DRAFT 4");
 
   // Manually enter draft mode if provided DRUPAL_PREVIEW_SECRET directly in the URL.
   if (secret === process.env.DRUPAL_PREVIEW_SECRET) {
-    console.log("DRAFT 5");
     if (!path) {
-      console.log("DRAFT 6");
       return NextResponse.json(
         {
           message: "Field 'path' is missing",
@@ -37,23 +32,36 @@ export async function GET(request: NextRequest): Promise<Response | never> {
         { status: 400 }
       );
     }
-  console.log("DRAFT 7");
     if (URL.canParse(path)) {
-      console.log("DRAFT 8");
       const url = new URL(path);
       path = url.pathname;
     }
 
-    console.log("DRAFT 9");
     const draft = await draftMode();
-    console.log("DRAFT 10");
     draft.enable();
 
-    console.log("DRAFT 11");
+    (await cookies()).set({
+      name: "STYXKEY_draft",
+      value: "1",
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
+
     redirect(path);
   }
 
-  console.log("DRAFT 12");
+  (await cookies()).set({
+    name: "STYXKEY_draft",
+    value: "1",
+    path: "/",
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+
   // @ts-ignore
   return enableDraftMode(request, drupal);
-}*/
+}
