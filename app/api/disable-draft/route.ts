1349be673ba28627f8db7a423b758fbb3b3e7324
Miranda Mafe <miranda.mafe@gmail.com>
2026-07-09 11:47:18 -0400

    Revert rest of disable-draft

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -5,30 +5,7 @@ import { cookies } from 'next/headers';
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  // let path = searchParams.get("path") ?? "/";
-  let path = searchParams.get("path");
-
-  console.log("-1path----->>>>" + path);
-
-  if (!path) {
-    return NextResponse.json(
-      {
-        message: "Field 'path' is missing",
-      },
-      { status: 400 }
-    );
-  }
-  if (URL.canParse(path)) {
-    const url = new URL(path);
-    path = url.pathname;
-  }
-
-  console.log("-2path------>>>>" + path);
-
-  const originalUrl = request.nextUrl.protocol + request.headers.get('host') + request.nextUrl.pathname;
-
-  console.log("-3originalURL------>>>>" + originalUrl);
-
+  const path = searchParams.get("path") ?? "/";
 
   await disableDraftMode();
 
@@ -42,10 +19,6 @@ export async function GET(request: NextRequest) {
     sameSite: "none",
   });
 
-  console.log("-4REQUEST.url ------>>>>" + request.url);
-
-  // return NextResponse.redirect(new URL(path, request.url));
-  // return NextResponse.redirect(new URL(path, originalUrl));
   return redirect(path);
 }
 
