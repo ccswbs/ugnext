235049519abb39b66112ce7aa5c09c36426db32d
Raza Mahmood <razammahmood@gmail.com>
2026-07-20 15:30:48 -0400

    Add loading state and transition handling to admission requirements form

--- components/client/programs/undergraduate/admission-requirements-form.tsx
+++ components/client/programs/undergraduate/admission-requirements-form.tsx
@@ -8,7 +8,7 @@ import type {
   UndergraduateAdmissionLocationType,
   UndergraduateAdmissionStudentType,
 } from "@/data/drupal/undergraduate-admission-requirements";
-import { useEffect, useMemo, useState } from "react";
+import { SubmitEventHandler, useEffect, useMemo, useState, useTransition } from "react";
 import { Select, SelectButton, SelectOption, SelectOptions } from "@uoguelph/react-components/select";
 import {
   Autocomplete,
@@ -26,6 +26,7 @@ import {
   UNDERGRADUATE_PROGRAMS_NODE_PATH,
 } from "@/lib/undergraduate-admission-requirements";
 import { RadioGroup, Radio } from "@uoguelph/react-components/radio-group";
+import { LoadingIndicator } from "@uoguelph/react-components/loading-indicator";
 
 type AdmissionRequirementsFormProps = {
   studentTypes: UndergraduateAdmissionStudentType[];
@@ -48,6 +49,8 @@ export default function AdmissionRequirementsForm({
 
   const [programQuery, setProgramQuery] = useState<string>("");
   const [program, setProgram] = useState<UndergraduateProgram | null>(null);
+
+  const [isPending, startTransition] = useTransition();
 
   const programSearch = useFuzzySearch({
     schema: {
@@ -85,135 +88,81 @@ export default function AdmissionRequirementsForm({
     return `/programs/undergraduate/requirements/${studentTypePath}/${locationPath}/${programPath}`;
   }, [studentType, location, program]);
 
+  const onSubmit: SubmitEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    if (!url) return;
+
+    startTransition(() => {
+      router.push(url);
+    });
+  };
+
   return (
-    <form
-      className="w-full flex flex-col"
-      onSubmit={(e) => {
-        e.preventDefault();
+    <>
+      {isPending && (
+        <div className="fixed top-0 left-0 w-screen z-10000 h-screen bg-white flex items-center justify-center">
+          <LoadingIndicator />
+        </div>
+      )}
 
-        if (url) {
-          router.push(url);
-        }
-      }}
-    >
-      <Field>
-        <Label>
-          <Typography type={"h3"} as={"h2"}>
-            I am a
-          </Typography>
-        </Label>
-
-        <Select value={studentType} multiple={false} onChange={setStudentType}>
-          <SelectButton>{studentType ? studentType.name : <span>&nbsp;</span>}</SelectButton>
-
-          <SelectOptions anchor="bottom">
-            {studentTypes
-              .sort((a, b) => a.weight - b.weight)
-              .map((type) => (
-                <SelectOption key={type.id} value={type}>
-                  {type.name}
-                </SelectOption>
-              ))}
-          </SelectOptions>
-        </Select>
-      </Field>
-
-      <Field>
-        <Label>
-          <Typography type={"h3"} as={"h2"}>
-            I attend/attended high school
-          </Typography>
-        </Label>
-
-        <Select
-          value={locationType}
-          onChange={(type) => {
-            setLocationType(type);
-            setLocation(null);
-            setLocationQuery("");
-          }}
-        >
-          <SelectButton>
-            {locationType === "domestic" && "Within Canada"}
-            {locationType === "international" && "Outside of Canada"}
-            {locationType === "curriculum" && "Under a Specific Curriculum of Study"}
-            <span>&nbsp;</span>
-          </SelectButton>
-
-          <SelectOptions anchor="bottom">
-            <SelectOption value="domestic">Within Canada</SelectOption>
-            <SelectOption value="international">Outside of Canada</SelectOption>
-          </SelectOptions>
-        </Select>
-      </Field>
-
-      {locationType === "domestic" && (
+      <form className="w-full flex flex-col" onSubmit={onSubmit}>
         <Field>
           <Label>
             <Typography type={"h3"} as={"h2"}>
-              My province/territory of study is/was
+              I am a
             </Typography>
           </Label>
 
-          <Autocomplete
-            value={location}
-            multiple={false}
-            onClose={() => setLocationQuery("")}
-            onChange={setLocation}
-            immediate
-          >
-            <AutocompleteInput
-              onChange={(event) => setLocationQuery(event.target.value.toLowerCase())}
-              displayValue={(selected: UndergraduateAdmissionLocation | null) => selected?.name ?? ""}
-            />
+          <Select value={studentType} multiple={false} onChange={setStudentType}>
+            <SelectButton>{studentType ? studentType.name : <span>&nbsp;</span>}</SelectButton>
 
-            <AutocompleteOptions anchor="bottom" className="max-h-[20rem]!">
-              {locations
-                .filter((location) => location.type === "domestic")
-                .filter((location) => location.name.toLowerCase().includes(locationQuery))
-                .map((location) => (
-                  <AutocompleteOption key={location.id} value={location} className="flex flex-col">
-                    {location.name}
-                  </AutocompleteOption>
+            <SelectOptions anchor="bottom">
+              {studentTypes
+                .sort((a, b) => a.weight - b.weight)
+                .map((type) => (
+                  <SelectOption key={type.id} value={type}>
+                    {type.name}
+                  </SelectOption>
                 ))}
-            </AutocompleteOptions>
-          </Autocomplete>
+            </SelectOptions>
+          </Select>
         </Field>
-      )}
 
-      {locationType && locationType !== "domestic" && (
-        <>
-          <Typography type={"h3"} as={"h2"}>
-            My Curriculum or Country of study is/was
-          </Typography>
+        <Field>
+          <Label>
+            <Typography type={"h3"} as={"h2"}>
+              I attend/attended high school
+            </Typography>
+          </Label>
 
+          <Select
+            value={locationType}
+            onChange={(type) => {
+              setLocationType(type);
+              setLocation(null);
+              setLocationQuery("");
+            }}
+          >
+            <SelectButton>
+              {locationType === "domestic" && "Within Canada"}
+              {locationType === "international" && "Outside of Canada"}
+              {locationType === "curriculum" && "Under a Specific Curriculum of Study"}
+              <span>&nbsp;</span>
+            </SelectButton>
+
+            <SelectOptions anchor="bottom">
+              <SelectOption value="domestic">Within Canada</SelectOption>
+              <SelectOption value="international">Outside of Canada</SelectOption>
+            </SelectOptions>
+          </Select>
+        </Field>
+
+        {locationType === "domestic" && (
           <Field>
             <Label>
-              <Typography type={"h3"} as={"h3"}>
-                Curriculum
-              </Typography>
-            </Label>
-
-            <RadioGroup onChange={setLocation} value={location?.type === "curriculum" ? location : null} by="name">
-              {locations
-                .filter((location) => location.type === "curriculum")
-                .sort((a, b) => a.name.localeCompare(b.name))
-                .map((loc) => (
-                  <Radio key={loc.id} value={loc}>
-                    {loc.name}
-                  </Radio>
-                ))}
-            </RadioGroup>
-          </Field>
-
-          <Typography type={"h3"} as={"span"}>
-            or
-          </Typography>
-
-          <Field>
-            <Label>
-              <Typography type={"h3"} as={"h3"}>
-                Country
+              <Typography type={"h3"} as={"h2"}>
+                My province/territory of study is/was
               </Typography>
             </Label>
 
@@ -226,15 +175,12 @@ export default function AdmissionRequirementsForm({
             >
               <AutocompleteInput
                 onChange={(event) => setLocationQuery(event.target.value.toLowerCase())}
-                displayValue={(selected: UndergraduateAdmissionLocation | null) =>
-                  selected?.type === "international" ? (selected?.name ?? "") : ""
-                }
+                displayValue={(selected: UndergraduateAdmissionLocation | null) => selected?.name ?? ""}
               />
 
               <AutocompleteOptions anchor="bottom" className="max-h-[20rem]!">
                 {locations
-                  .sort((a, b) => a.name.localeCompare(b.name))
-                  .filter((location) => location.type === "international")
+                  .filter((location) => location.type === "domestic")
                   .filter((location) => location.name.toLowerCase().includes(locationQuery))
                   .map((location) => (
                     <AutocompleteOption key={location.id} value={location} className="flex flex-col">
@@ -244,41 +190,107 @@ export default function AdmissionRequirementsForm({
               </AutocompleteOptions>
             </Autocomplete>
           </Field>
-        </>
-      )}
+        )}
 
-      <Field>
-        <Label>
-          <Typography type={"h3"} as={"h2"}>
-            I am interested in studying
-          </Typography>
-        </Label>
+        {locationType && locationType !== "domestic" && (
+          <>
+            <Typography type={"h3"} as={"h2"}>
+              My Curriculum or Country of study is/was
+            </Typography>
 
-        <Autocomplete
-          value={program}
-          multiple={false}
-          onClose={() => setProgramQuery("")}
-          onChange={setProgram}
-          immediate
-        >
-          <AutocompleteInput
-            onChange={(event) => setProgramQuery(event.target.value.toLowerCase())}
-            displayValue={(selected: UndergraduateProgram | null) => selected?.title ?? ""}
-          />
+            <Field>
+              <Label>
+                <Typography type={"h3"} as={"h3"}>
+                  Curriculum
+                </Typography>
+              </Label>
 
-          <AutocompleteOptions anchor="bottom" className="max-h-[20rem]!">
-            {filteredPrograms.map((program) => (
-              <AutocompleteOption key={program.id} value={program}>
-                {program.title}
-              </AutocompleteOption>
-            ))}
-          </AutocompleteOptions>
-        </Autocomplete>
-      </Field>
+              <RadioGroup onChange={setLocation} value={location?.type === "curriculum" ? location : null} by="name">
+                {locations
+                  .filter((location) => location.type === "curriculum")
+                  .sort((a, b) => a.name.localeCompare(b.name))
+                  .map((loc) => (
+                    <Radio key={loc.id} value={loc}>
+                      {loc.name}
+                    </Radio>
+                  ))}
+              </RadioGroup>
+            </Field>
 
-      <Button type="submit" disabled={!url} className="w-fit mt-8">
-        View Requirements
-      </Button>
-    </form>
+            <Typography type={"h3"} as={"span"}>
+              or
+            </Typography>
+
+            <Field>
+              <Label>
+                <Typography type={"h3"} as={"h3"}>
+                  Country
+                </Typography>
+              </Label>
+
+              <Autocomplete
+                value={location}
+                multiple={false}
+                onClose={() => setLocationQuery("")}
+                onChange={setLocation}
+                immediate
+              >
+                <AutocompleteInput
+                  onChange={(event) => setLocationQuery(event.target.value.toLowerCase())}
+                  displayValue={(selected: UndergraduateAdmissionLocation | null) =>
+                    selected?.type === "international" ? (selected?.name ?? "") : ""
+                  }
+                />
+
+                <AutocompleteOptions anchor="bottom" className="max-h-[20rem]!">
+                  {locations
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .filter((location) => location.type === "international")
+                    .filter((location) => location.name.toLowerCase().includes(locationQuery))
+                    .map((location) => (
+                      <AutocompleteOption key={location.id} value={location} className="flex flex-col">
+                        {location.name}
+                      </AutocompleteOption>
+                    ))}
+                </AutocompleteOptions>
+              </Autocomplete>
+            </Field>
+          </>
+        )}
+
+        <Field>
+          <Label>
+            <Typography type={"h3"} as={"h2"}>
+              I am interested in studying
+            </Typography>
+          </Label>
+
+          <Autocomplete
+            value={program}
+            multiple={false}
+            onClose={() => setProgramQuery("")}
+            onChange={setProgram}
+            immediate
+          >
+            <AutocompleteInput
+              onChange={(event) => setProgramQuery(event.target.value.toLowerCase())}
+              displayValue={(selected: UndergraduateProgram | null) => selected?.title ?? ""}
+            />
+
+            <AutocompleteOptions anchor="bottom" className="max-h-[20rem]!">
+              {filteredPrograms.map((program) => (
+                <AutocompleteOption key={program.id} value={program}>
+                  {program.title}
+                </AutocompleteOption>
+              ))}
+            </AutocompleteOptions>
+          </Autocomplete>
+        </Field>
+
+        <Button type="submit" disabled={!url || isPending} className="w-fit mt-8">
+          View Requirements
+        </Button>
+      </form>
+    </>
   );
 }
